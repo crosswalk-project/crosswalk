b7a48a1ba7ca442c7b5b0f473dee1f45fba337a7
Caio Marcelo de Oliveira Filho <caio.de.oliveira.filho@intel.com>
2013-07-02 18:03:45 -0300

    extensions: Extract test fixture to separate file
    Creates CameoExtensionsTestBase that adds a hook for registering
    extensions in the test. This separation will make easy to create new
    testes.

--- extensions/extensions_browsertests.gypi
+++ extensions/extensions_browsertests.gypi
@@ -1,5 +1,7 @@
 {
   'sources': [
     'test/cameo_extensions_browsertest.cc',
+    'test/cameo_extensions_test_base.cc',
+    'test/cameo_extensions_test_base.h',
   ],
 }
--- extensions/test/cameo_extensions_browsertest.cc
+++ extensions/test/cameo_extensions_browsertest.cc
@@ -2,16 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/path_service.h"
-#include "base/utf_string_conversions.h"
+#include "cameo/extensions/test/cameo_extensions_test_base.h"
+
 #include "cameo/extensions/browser/cameo_extension.h"
 #include "cameo/extensions/browser/cameo_extension_service.h"
 #include "cameo/runtime/browser/runtime.h"
 #include "cameo/test/base/cameo_test_utils.h"
-#include "cameo/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
-#include "net/base/net_util.h"
 
 using cameo::extensions::CameoExtension;
 using cameo::extensions::CameoExtensionService;
@@ -53,30 +51,10 @@ class EchoExtension : public CameoExtension {
   }
 };
 
-class CameoExtensionsTest : public InProcessBrowserTest {
+class CameoExtensionsTest : public CameoExtensionsTestBase {
  public:
-  virtual void SetUp() OVERRIDE {
-    CameoExtensionService::SetRegisterExtensionsCallbackForTesting(
-        base::Bind(&CameoExtensionsTest::RegisterExtensions,
-                   base::Unretained(this)));
-    InProcessBrowserTest::SetUp();
-  }
-
-  void RegisterExtensions(CameoExtensionService* extension_service) {
+  void RegisterExtensions(CameoExtensionService* extension_service) OVERRIDE {
     extension_service->RegisterExtension(new EchoExtension);
-  }
-
-  GURL GetExtensionsTestURL(const base::FilePath& dir,
-                            const base::FilePath& file) {
-    base::FilePath test_file;
-    PathService::Get(base::DIR_SOURCE_ROOT, &test_file);
-    test_file = test_file
-                .Append(FILE_PATH_LITERAL("cameo"))
-                .Append(FILE_PATH_LITERAL("extensions"))
-                .Append(FILE_PATH_LITERAL("test"))
-                .Append(FILE_PATH_LITERAL("data"))
-                .Append(dir).Append(file);
-    return net::FilePathToFileURL(test_file);
   }
 };
 
