d430bf54e4a6fecf607859c36f9dd71eb42e9ad5
Girish Ramakrishnan <girish.ramakrishnan@intel.com>
2013-07-17 14:26:08 -0700

    android: unset GYP_GENERATOR_FLAGS and CHROMIUM_GYP_FILE
    sdk_build_init() sets GYP_GENERATOR_FLAGS to 'All' target and
    CHROMIUM_GYP_FILE to all_android.gyp. Unset these so that xwalk.gyp
    is processed.

--- build/android/envsetup.sh
+++ build/android/envsetup.sh
@@ -7,6 +7,13 @@ SCRIPT_DIR="$(dirname "${BASH_SOURCE:-$0}")"
 
 . ${SCRIPT_DIR}/../../../build/android/envsetup.sh "$@"
 
+# sdk_build_init() sets GYP_GENERATOR_FLAGS to 'All' target and
+# CHROMIUM_GYP_FILE to all_android.gyp. Unset these so that xwalk.gyp
+# is processed.
+# TODO(girish): Remove only default_target=All from GYP_GENERATOR_FLAGS
+unset GYP_GENERATOR_FLAGS
+unset CHROMIUM_GYP_FILE
+
 xwalk_android_gyp() {
   echo "GYP_GENERATORS set to '$GYP_GENERATORS'"
   (
