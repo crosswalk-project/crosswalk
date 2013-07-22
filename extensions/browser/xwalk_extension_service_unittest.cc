eb14ac308ea0e16babde6151ab3809ff63677f40
Caio Marcelo de Oliveira Filho <caio.de.oliveira.filho@intel.com>
2013-07-22 15:16:16 -0300

    Add infrastructure to run unittests for XWalk extension module
    New unit tests inside xwalk/extensions should be added to the list in
    xwalk/extensions/extensions_unittests.gypi. This list is included when
    generating xwalk_unittest.

---
+++ extensions/browser/xwalk_extension_service_unittest.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "xwalk/extensions/browser/xwalk_extension_service.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(XWalkExtensionServiceTest, DummyTest) {
+  // Dummy test to add infrastructure for unittests in extension module.
+  EXPECT_EQ(1 + 1, 2);
+}
