6b3c262ba063fa985b88f5398636e801d02d1a3c
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-10-15 20:57:20 +0300

    [SysApps] Add Raw Sockets stub
    Just the skeleton to serve as base for the interfaces implementation. No API is
    currently exposed.

---
+++ sysapps/raw_socket/raw_socket_extension.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef XWALK_SYSAPPS_RAW_SOCKET_RAW_SOCKET_EXTENSION_H_
+#define XWALK_SYSAPPS_RAW_SOCKET_RAW_SOCKET_EXTENSION_H_
+
+#include <string>
+#include "base/values.h"
+#include "xwalk/sysapps/common/binding_object_store.h"
+
+namespace xwalk {
+namespace sysapps {
+
+using extensions::XWalkExtension;
+using extensions::XWalkExtensionInstance;
+
+class RawSocketExtension : public XWalkExtension {
+ public:
+  explicit RawSocketExtension();
+  virtual ~RawSocketExtension();
+
+  // XWalkExtension implementation.
+  virtual XWalkExtensionInstance* CreateInstance() OVERRIDE;
+};
+
+class RawSocketInstance : public XWalkExtensionInstance {
+ public:
+  RawSocketInstance();
+
+  // XWalkExtensionInstance implementation.
+  virtual void HandleMessage(scoped_ptr<base::Value> msg) OVERRIDE {}
+};
+
+}  // namespace sysapps
+}  // namespace xwalk
+
+#endif  // XWALK_SYSAPPS_RAW_SOCKET_RAW_SOCKET_EXTENSION_H_
