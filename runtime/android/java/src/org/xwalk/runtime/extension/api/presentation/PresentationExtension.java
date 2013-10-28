29dff090299604bdd486ebc16accc9f88fed5421
Hongbo Min <hongbo.min@intel.com>
2013-10-28 15:19:14 +0800

    [Android] Hotfix in Presentation API extension
    This patch is to fix 2 issues:
    o Place PresentationExtension into ..api.presentation package to follow
      the general pattern in Java. Otherwise, we will get error message when
      import xwalk_core_library into eclipse.
    o Don't create PresentationExtension if failing to load the JS binding
      code from assets/
    
    TODO:
    1. Figure out why the external extension test crash when passing a empty string to
       create a new XWalkExtension.
    2. Ensure the PresentationExtension code doesn't break Android 4.0 since
       DisplayManager is introduced since API level 17.

--- runtime/android/java/src/org/xwalk/runtime/extension/api/presentation/PresentationExtension.java
+++ runtime/android/java/src/org/xwalk/runtime/extension/api/presentation/PresentationExtension.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.xwalk.runtime.extension.api;
+package org.xwalk.runtime.extension.api.presentation;
 
 import android.content.Context;
 import android.hardware.display.DisplayManager;
