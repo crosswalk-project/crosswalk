581b5752457ed2efcbe7c7c7a2d93e78097f2ddb
Shao Changbin <changbin.shao@intel.com>
2013-11-04 13:59:35 +0800

    [Runtime Model] Implement non-persistent main document.
    The following CL is to implement
    https://crosswalk-project.org/jira/browse/XWALK-2.
    
    The CL makes ApplicationProcessManager as an Observer for Runtime
    objects. Therefore, whenever a Runtime object/Window is added or
    removed, the ApplicationProcessManager could be notified.

--- application/browser/application_process_manager.h
+++ application/browser/application_process_manager.h
@@ -15,6 +15,7 @@
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "xwalk/application/common/application.h"
+#include "xwalk/runtime/browser/runtime_registry.h"
 
 class GURL;
 
@@ -31,7 +32,7 @@ class Manifest;
 
 // This manages dynamic state of running applications. By now, it only launches
 // one application, later it will manages all event pages' lifecycle.
-class ApplicationProcessManager {
+class ApplicationProcessManager : public RuntimeRegistryObserver {
  public:
   explicit ApplicationProcessManager(xwalk::RuntimeContext* runtime_context);
   ~ApplicationProcessManager();
@@ -41,13 +42,20 @@ class ApplicationProcessManager {
 
   Runtime* GetMainDocumentRuntime() const { return main_runtime_; }
 
+  // RuntimeRegistryObserver implementation.
+  virtual void OnRuntimeAdded(Runtime* runtime) OVERRIDE;
+  virtual void OnRuntimeRemoved(Runtime* runtime) OVERRIDE;
+  virtual void OnRuntimeAppIconChanged(Runtime* runtime) OVERRIDE {}
+
  private:
   bool RunMainDocument(const Application* application);
   bool RunFromLocalPath(const Application* application);
+  void CloseMainDocument();
 
   xwalk::RuntimeContext* runtime_context_;
   xwalk::Runtime* main_runtime_;
   base::WeakPtrFactory<ApplicationProcessManager> weak_ptr_factory_;
+  std::set<Runtime*> runtimes_;
 
   DISALLOW_COPY_AND_ASSIGN(ApplicationProcessManager);
 };
