3720e347eba49cef8a6c5e842205f44c4e5c3ad0
hengzhi.wu <hengzhix.wu@intel.com>
2013-11-04 09:55:49 +0800

    [Android] Add the test case about onPause and onResume.
    This patch contains the test case about onPause and onResume on
    embedded and shared mode. It's for the runtime client.
    Invoke onPause or onResume on main thread, wait for some seconds
    to ensure that we get the title. Then wait for two hundred
    milliseconds, get the title again. Compare the two titles.
    
    For embedded client test:
    1. install XWalkRuntimeClientEmbeddedShell.apk
    2. python build/android/test_runner.py instrumentation \
    --test-apk=XWalkRuntimeClientEmbeddedTest -v -f testPauseResume
    
    For shared client test:
    1. install XWalkRuntimeLib.apk
    2. install XWalkRuntimeClientShell.apk
    3. python build/android/test_runner.py instrumentation \
    --test-apk=XWalkRuntimeClientTest -v -f testPauseResume

--- test/android/util/runtime_client/src/org/xwalk/test/util/RuntimeClientApiTestBase.java
+++ test/android/util/runtime_client/src/org/xwalk/test/util/RuntimeClientApiTestBase.java
@@ -8,17 +8,52 @@ package org.xwalk.test.util;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Test helper for Runtime client APIs.
  */
 public class RuntimeClientApiTestBase<T extends Activity> {
     private XWalkRuntimeClientTestUtilBase mTestUtil;
     private ActivityInstrumentationTestCase2<T> mTestCase;
+    private Timer mTimer = new Timer();
+    enum Relation {
+        EQUAL,
+        GREATERTHAN,
+        LESSSTHAN,
+        NONE
+    }
 
     public RuntimeClientApiTestBase(XWalkRuntimeClientTestUtilBase testUtil,
             ActivityInstrumentationTestCase2<T> testCase) {
         mTestUtil = testUtil;
         mTestCase = testCase;
+    }
+
+    public void compareTitle(String prevTitle, String title, String msg, Relation relation) {
+        switch (relation) {
+            case EQUAL:
+                mTestCase.assertTrue(msg, title.equals(prevTitle));
+                break;
+            case GREATERTHAN:
+                mTestCase.assertTrue(msg, title.compareTo(title) > 0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void compareTitleAfterTimer(final String prevTitle, int milliSeconds,
+            final String msg, final Relation relation) {
+        mTimer.schedule(new TimerTask() {
+            String title = "";
+            @Override
+            public void run() {
+                title = mTestUtil.getTestedView().getTitleForTest();
+                compareTitle(prevTitle, title, msg, relation);
+            }
+        }, milliSeconds);
     }
 
     // For loadAppFromUrl.
@@ -58,5 +93,26 @@ public class RuntimeClientApiTestBase<T extends Activity> {
         mTestUtil.loadAssetFile("echoSync.html");
         String title = mTestUtil.getTestedView().getTitleForTest();
         mTestCase.assertEquals("Pass", title);
+    }
+
+    // For onPause, onResume.
+    public void testPauseAndResume() throws Throwable {
+        String title = "";
+        String msg = "";
+
+        title = mTestUtil.loadAssetFileAndWaitForTitle("timer.html");
+        mTestCase.assertNotNull(title);
+
+        mTestUtil.onPause();
+        mTestUtil.waitForTitleUpdated(mTestUtil.WAIT_TIMEOUT_SECONDS);
+        title = mTestUtil.getTestedView().getTitleForTest();
+        msg = "The second title should be equal to the first title.";
+        compareTitleAfterTimer(title, 200, msg, Relation.EQUAL);
+
+        mTestUtil.onResume();
+        mTestUtil.waitForTitleUpdated(mTestUtil.WAIT_TIMEOUT_SECONDS);
+        title = mTestUtil.getTestedView().getTitleForTest();
+        msg = "The second title should be greater than the first title.";
+        compareTitleAfterTimer(title, 200, msg, Relation.GREATERTHAN);
     }
 }
