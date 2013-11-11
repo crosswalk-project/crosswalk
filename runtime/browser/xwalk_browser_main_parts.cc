0503c5ddeaac556e2da9ed7a446927f26ecd8cdd
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2013-11-11 17:48:08 -0200

    [Application] Move package details inside ApplicationService
    These are details for package installation that belong to
    ApplicationService object, which is responsible for installation and
    uninstallation of packages.

--- runtime/browser/xwalk_browser_main_parts.cc
+++ runtime/browser/xwalk_browser_main_parts.cc
@@ -50,7 +50,6 @@
 
 #if defined(OS_TIZEN_MOBILE)
 #include "content/browser/device_orientation/device_inertial_sensor_service.h"
-#include "xwalk/application/browser/installer/tizen/package_installer.h"
 #include "xwalk/runtime/browser/tizen/tizen_data_fetcher_shared_memory.h"
 #include "xwalk/sysapps/device_capabilities/device_capabilities_extension.h"
 #endif  // defined(OS_TIZEN_MOBILE)
@@ -323,15 +322,6 @@ void XWalkBrowserMainParts::PreMainMessageLoopRun() {
       id = std::string(args[0].begin(), args[0].end());
     if (xwalk::application::Application::IsIDValid(id)) {
       if (command_line->HasSwitch(switches::kUninstall)) {
-#if defined(OS_TIZEN_MOBILE)
-        scoped_refptr<xwalk::application::PackageInstaller> installer =
-            xwalk::application::PackageInstaller::Create(service, id,
-                runtime_context_->GetPath());
-        if (!installer || !installer->Uninstall()) {
-          LOG(ERROR) << "[ERR] An error occurred during uninstalling on Tizen.";
-          return;
-        }
-#endif
         if (!service->Uninstall(id))
           LOG(ERROR) << "[ERR] An error occurred during"
                         "uninstalling application "
@@ -351,15 +341,6 @@ void XWalkBrowserMainParts::PreMainMessageLoopRun() {
       if (base::PathExists(path)) {
         std::string id;
         if (service->Install(path, &id)) {
-#if defined(OS_TIZEN_MOBILE)
-          scoped_refptr<xwalk::application::PackageInstaller> installer =
-              xwalk::application::PackageInstaller::Create(service, id,
-                  runtime_context_->GetPath());
-          if (!installer || !installer->Install()) {
-            LOG(ERROR) << "[ERR] An error occurred during installing on Tizen.";
-            return;
-          }
-#endif  // OS_TIZEN_MOBILE
           LOG(INFO) << "[OK] Application installed: " << id;
         } else {
           LOG(ERROR) << "[ERR] Application install failure: " << path.value();
