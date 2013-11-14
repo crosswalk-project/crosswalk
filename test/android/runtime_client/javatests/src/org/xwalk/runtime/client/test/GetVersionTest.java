9c76cde837f47631281d8eb6078601b54ab756c6
hengzhi.wu <hengzhix.wu@intel.com>
2013-11-14 18:46:30 +0800

    [Android] Add the test case about getVersion.
    This patch contains the test about getVersion in shared and
    embedded mode. It's for the runtime client.
    Get the version, confirm it's validity by regular expression.

---
+++ test/android/runtime_client/javatests/src/org/xwalk/runtime/client/test/GetVersionTest.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.runtime.client.test;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import org.chromium.base.test.util.Feature;
+import org.xwalk.test.util.RuntimeClientApiTestBase;
+
+/**
+ * Test suite for getVersion().
+ */
+public class GetVersionTest extends XWalkRuntimeClientTestBase {
+
+    @SmallTest
+    @Feature({"GetVersion"})
+    public void testGetVersion() throws Throwable {
+        RuntimeClientApiTestBase<XWalkRuntimeClientTestRunnerActivity> helper =
+                new RuntimeClientApiTestBase<XWalkRuntimeClientTestRunnerActivity>(
+                        getTestUtil(), this);
+        helper.testGetVersion();
+    }
+}
