501abf1c575fe230a6ab186adfd86c0134c6d5e0
Long Xiang <xiang.long@intel.com>
2013-12-04 21:35:35 +0800

    [Runtime] Add application onLauched event browser test

---
+++ application/test/application_event_test.cc
@@ -0,0 +1,24 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/test/test_utils.h"
+#include "net/base/net_util.h"
+#include "url/gurl.h"
+#include "xwalk/application/test/application_apitest.h"
+#include "xwalk/application/test/application_testapi.h"
+
+class ApplicationEventTest : public ApplicationApiTest {
+ public:
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    ApplicationBrowserTest::SetUpCommandLine(command_line);
+    GURL url = net::FilePathToFileURL(test_data_dir_.Append(
+          FILE_PATH_LITERAL("event")));
+    command_line->AppendArg(url.spec());
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(ApplicationEventTest, EventTest) {
+  test_runner_->WaitForTestComplete();
+  EXPECT_EQ(test_runner_->GetTestsResult(), ApiTestRunner::PASS);
+}
