2274b627405656e268786d07f6bcee3d6e5640e3
Alexis Menard <alexis.menard@intel.com>
2013-12-05 17:42:07 -0300

    Protect the SysApp RawSocket and DeviceCapabilities API behind a feature flag.
    The flag is stable (on) by default.

--- runtime/browser/xwalk_browser_main_parts.cc
+++ runtime/browser/xwalk_browser_main_parts.cc
@@ -33,7 +33,7 @@
 #include "content/public/common/result_codes.h"
 #include "net/base/net_util.h"
 #include "ui/gl/gl_switches.h"
-#include "xwalk_runtime_enabled_features.h"
+#include "xwalk_runtime_enabled_features.h" // NOLINT
 
 #if defined(USE_AURA) && defined(USE_X11)
 #include "ui/base/ime/input_method_initializer.h"
@@ -228,8 +228,10 @@ void XWalkBrowserMainParts::RegisterInternalExtensionsInExtensionThreadServer(
   server->RegisterExtension(scoped_ptr<XWalkExtension>(new RuntimeExtension()));
   server->RegisterExtension(scoped_ptr<XWalkExtension>(
       new experimental::DialogExtension(runtime_registry_.get())));
-  server->RegisterExtension(scoped_ptr<XWalkExtension>(
-      new sysapps::RawSocketExtension()));
+  if (XWalkRuntimeEnabledFeatures::RawSocketEnabled()) {
+    server->RegisterExtension(scoped_ptr<XWalkExtension>(
+        new sysapps::RawSocketExtension()));
+  }
 }
 
 void XWalkBrowserMainParts::RegisterInternalExtensionsInUIThreadServer(
