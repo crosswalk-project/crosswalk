ce6255e4cc7a9252cffae698693586b27773e25d
Yangang Han <yangangx.han@intel.com>
2014-03-06 16:58:00 +0800

    Clear code of interfaces related with Android browser.
    Since existent code doesn't work naturally by our investigation, so we leave it with "TODO".
    
    Refer to below figure to check related code.
    chromedriver_server.cc:    main
                                 |
    http_handler.cc:        HttpHandler
                                 |
                       "CommandMapping commands[];"
                                             |
                                 +-----------+-----
                                 |           |
                            "InitSession"   ...
                                 |
    session_commands.cc:  ExecuteInitSession
                                 |
                          InitSessionHelper
                                 |
    xwalk_launcher.cc:       LaunchXwalk
                                 |
                                 +-------------------+
                                 |                   |
                         LaunchDesktopXwalk    LaunchAndroidXwalk
                                 |                   |
                                 +-----+             +-------------+
                                 |     |             |             |
    xwalk_finder.cc:         FindXwalk |            ...            |
                                    +--+---------+                 |
                                    |            |                 |
    xwalk_android_impl.cc:         ...          ...          XwalkAndroidImpl
                                                                   |
    device_manager.cc:                                        DeviceManager
                                                                   |
                                                                Device
                                                                   |
    adb_impl.h:                                                 AdbImpl

--- test/chromedriver/chrome/adb_impl.cc
+++ test/chromedriver/chrome/adb_impl.cc
@@ -29,26 +29,17 @@ class ResponseBuffer : public base::RefCountedThreadSafe<ResponseBuffer> {
   ResponseBuffer() : ready_(true, false) {}
 
   void OnResponse(int result, const std::string& response) {
-    response_ = response;
-    result_ = result;
-    ready_.Signal();
+    // TODO(Peter Wang): Need to implement for android xwalk.
+    (void) response;
+    (void) result;
   }
 
   Status GetResponse(
       std::string* response, const base::TimeDelta& timeout) {
-    base::TimeTicks deadline = base::TimeTicks::Now() + timeout;
-    while (!ready_.IsSignaled()) {
-      base::TimeDelta delta = deadline - base::TimeTicks::Now();
-      if (delta <= base::TimeDelta())
-        return Status(kTimeout, base::StringPrintf(
-            "Adb command timed out after %d seconds",
-            static_cast<int>(timeout.InSeconds())));
-      ready_.TimedWait(timeout);
-    }
-    if (result_ < 0)
-      return Status(kUnknownError,
-          "Failed to run adb command, is the adb server running?");
-    *response = response_;
+    // TODO(Peter Wang): Need to implement for android xwalk.
+    (void) response;
+    (void) timeout;
+
     return Status(kOk);
   }
 
@@ -64,9 +55,10 @@ class ResponseBuffer : public base::RefCountedThreadSafe<ResponseBuffer> {
 void ExecuteCommandOnIOThread(
     const std::string& command, scoped_refptr<ResponseBuffer> response_buffer,
     int port) {
-  CHECK(base::MessageLoop::current()->IsType(base::MessageLoop::TYPE_IO));
-  AdbClientSocket::AdbQuery(port, command,
-      base::Bind(&ResponseBuffer::OnResponse, response_buffer));
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) command;
+  (void) response_buffer;
+  (void) port;
 }
 
 }  // namespace
@@ -75,174 +67,119 @@ AdbImpl::AdbImpl(
     const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner,
     int port)
     : io_task_runner_(io_task_runner), port_(port) {
-  CHECK(io_task_runner_.get());
+  // TODO(Peter Wang): Need to implement for android xwalk.
 }
 
 AdbImpl::~AdbImpl() {}
 
 Status AdbImpl::GetDevices(std::vector<std::string>* devices) {
-  std::string response;
-  Status status = ExecuteCommand("host:devices", &response);
-  if (!status.IsOk())
-    return status;
-  base::StringTokenizer lines(response, "\n");
-  while (lines.GetNext()) {
-    std::vector<std::string> fields;
-    base::SplitStringAlongWhitespace(lines.token(), &fields);
-    if (fields.size() == 2 && fields[1] == "device") {
-      devices->push_back(fields[0]);
-    }
-  }
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) devices;
+
   return Status(kOk);
 }
 
 Status AdbImpl::ForwardPort(
     const std::string& device_serial, int local_port,
     const std::string& remote_abstract) {
-  std::string response;
-  Status status = ExecuteHostCommand(
-      device_serial,
-      "forward:tcp:" + base::IntToString(local_port) + ";localabstract:" +
-          remote_abstract,
-      &response);
-  if (!status.IsOk())
-    return status;
-  if (response == "OKAY")
-    return Status(kOk);
-  return Status(kUnknownError, "Failed to forward ports to device " +
-                device_serial + ": " + response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) local_port;
+  (void) remote_abstract;
+
+  return Status(kOk);
 }
 
 Status AdbImpl::SetCommandLineFile(const std::string& device_serial,
                                    const std::string& command_line_file,
                                    const std::string& exec_name,
                                    const std::string& args) {
-  std::string response;
-  std::string quoted_command =
-      base::GetDoubleQuotedJson(exec_name + " " + args);
-  Status status = ExecuteHostShellCommand(
-      device_serial,
-      base::StringPrintf("echo %s > %s; echo $?",
-                         quoted_command.c_str(),
-                         command_line_file.c_str()),
-      &response);
-  if (!status.IsOk())
-    return status;
-  if (response.find("0") == std::string::npos)
-    return Status(kUnknownError, "Failed to set command line file " +
-                  command_line_file + " on device " + device_serial);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) command_line_file;
+  (void) exec_name;
+  (void) args;
+
   return Status(kOk);
 }
 
 Status AdbImpl::CheckAppInstalled(
     const std::string& device_serial, const std::string& package) {
-  std::string response;
-  std::string command = "pm path " + package;
-  Status status = ExecuteHostShellCommand(device_serial, command, &response);
-  if (!status.IsOk())
-    return status;
-  if (response.find("package") == std::string::npos)
-    return Status(kUnknownError, package + " is not installed on device " +
-                  device_serial);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) package;
+
   return Status(kOk);
 }
 
 Status AdbImpl::ClearAppData(
     const std::string& device_serial, const std::string& package) {
-  std::string response;
-  std::string command = "pm clear " + package;
-  Status status = ExecuteHostShellCommand(device_serial, command, &response);
-  if (!status.IsOk())
-    return status;
-  if (response.find("Success") == std::string::npos)
-    return Status(kUnknownError, "Failed to clear data for " + package +
-                  " on device " + device_serial + ": " + response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) package;
+
   return Status(kOk);
 }
 
 Status AdbImpl::Launch(
     const std::string& device_serial, const std::string& package,
     const std::string& activity) {
-  std::string response;
-  Status status = ExecuteHostShellCommand(
-      device_serial,
-      "am start -W -n " + package + "/" + activity + " -d data:,",
-      &response);
-  if (!status.IsOk())
-    return status;
-  if (response.find("Complete") == std::string::npos)
-    return Status(kUnknownError,
-                  "Failed to start " + package + " on device " + device_serial +
-                  ": " + response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) package;
+  (void) activity;
+
   return Status(kOk);
 }
 
 Status AdbImpl::ForceStop(
     const std::string& device_serial, const std::string& package) {
-  std::string response;
-  return ExecuteHostShellCommand(
-      device_serial, "am force-stop " + package, &response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) package;
+
+  return Status(kOk);
 }
 
 Status AdbImpl::GetPidByName(const std::string& device_serial,
                              const std::string& process_name,
                              int* pid) {
-  std::string response;
-  Status status = ExecuteHostShellCommand(device_serial, "ps", &response);
-  if (!status.IsOk())
-    return status;
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) process_name;
+  (void) pid;
 
-  std::vector<std::string> lines;
-  base::SplitString(response, '\n', &lines);
-  for (size_t i = 0; i < lines.size(); ++i) {
-    std::string line = lines[i];
-    if (line.empty())
-      continue;
-    std::vector<std::string> tokens;
-    base::SplitStringAlongWhitespace(line, &tokens);
-    if (tokens.size() != 9)
-      continue;
-    if (tokens[8].compare(process_name) == 0) {
-      if (base::StringToInt(tokens[1], pid)) {
-        return Status(kOk);
-      } else {
-        break;
-      }
-    }
-  }
-
-  return Status(kUnknownError,
-                "Failed to get PID for the following process: " + process_name);
+  return Status(kOk);
 }
 
 Status AdbImpl::ExecuteCommand(
     const std::string& command, std::string* response) {
-  scoped_refptr<ResponseBuffer> response_buffer = new ResponseBuffer;
-  VLOG(1) << "Sending adb command: " << command;
-  io_task_runner_->PostTask(
-      FROM_HERE,
-      base::Bind(&ExecuteCommandOnIOThread, command, response_buffer, port_));
-  Status status = response_buffer->GetResponse(
-      response, base::TimeDelta::FromSeconds(30));
-  if (status.IsOk()) {
-    VLOG(1) << "Received adb response: " << *response;
-  }
-  return status;
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) command;
+  (void) response;
+
+  return Status(kOk);
 }
 
 Status AdbImpl::ExecuteHostCommand(
     const std::string& device_serial,
     const std::string& host_command, std::string* response) {
-  return ExecuteCommand(
-      "host-serial:" + device_serial + ":" + host_command, response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) host_command;
+  (void) response;
+
+  return Status(kOk);
 }
 
 Status AdbImpl::ExecuteHostShellCommand(
     const std::string& device_serial,
     const std::string& shell_command,
     std::string* response) {
-  return ExecuteCommand(
-      "host:transport:" + device_serial + "|shell:" + shell_command,
-      response);
+  // TODO(Peter Wang): Need to implement for android xwalk.
+  (void) device_serial;
+  (void) shell_command;
+  (void) response;
+
+  return Status(kOk);
 }
 
--- test/chromedriver/chrome/device_manager.cc
+++ test/chromedriver/chrome/device_manager.cc
@@ -32,153 +32,60 @@ Status Device::StartApp(const std::string& package,
                         const std::string& process,
                         const std::string& args,
                         int port) {
-  if (!active_package_.empty())
-    return Status(kUnknownError,
-        active_package_ + " was launched and has not been quit");
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) package;
+  (void) activity;
+  (void) process;
+  (void) args;
+  (void) port;
 
-  Status status = adb_->CheckAppInstalled(serial_, package);
-  if (!status.IsOk())
-    return status;
-
-  status = adb_->ClearAppData(serial_, package);
-  if (!status.IsOk())
-    return status;
-
-  std::string known_activity;
-  std::string device_socket;
-  std::string command_line_file;
-  std::string exec_name;
-  if (package.compare("org.chromium.content_shell_apk") == 0) {
-    known_activity = ".ContentShellActivity";
-    device_socket = "content_shell_devtools_remote";
-    command_line_file = "/data/local/tmp/content-shell-command-line";
-    exec_name = "content_shell";
-  } else if (package.compare("org.chromium.chrome.testshell") == 0) {
-    known_activity = ".ChromiumTestShellActivity";
-    device_socket = "chromium_testshell_devtools_remote";
-    command_line_file = "/data/local/tmp/chromium-testshell-command-line";
-    exec_name = "chromium_testshell";
-  } else if (package.find("chrome") != std::string::npos) {
-    known_activity = "com.google.android.apps.chrome.Main";
-    device_socket = "chrome_devtools_remote";
-    command_line_file = "/data/local/chrome-command-line";
-    exec_name = "chrome";
-  }
-
-  if (!known_activity.empty()) {
-    if (!activity.empty() || !process.empty())
-      return Status(kUnknownError, "known package " + package +
-                    " does not accept activity/process");
-  } else if (activity.empty()) {
-    return Status(kUnknownError, "WebView apps require activity name");
-  }
-
-  if (!command_line_file.empty()) {
-    status = adb_->SetCommandLineFile(serial_, command_line_file, exec_name,
-                                      args);
-    if (!status.IsOk())
-      return status;
-  }
-
-  status = adb_->Launch(serial_, package,
-                        known_activity.empty() ? activity : known_activity);
-  if (!status.IsOk())
-    return status;
-  active_package_ = package;
-
-  if (device_socket.empty()) {
-    // Assume this is a WebView app.
-    int pid;
-    status = adb_->GetPidByName(serial_, process.empty() ? package : process,
-                                &pid);
-    if (!status.IsOk()) {
-      if (process.empty())
-        status.AddDetails(
-            "process name must be specified if not equal to package name");
-      return status;
-    }
-    device_socket = base::StringPrintf("webview_devtools_remote_%d", pid);
-  }
-
-  return adb_->ForwardPort(serial_, port, device_socket);
+  return Status(kOk);
 }
 
 Status Device::StopApp() {
-  if (!active_package_.empty()) {
-    std::string response;
-    Status status = adb_->ForceStop(serial_, active_package_);
-    if (!status.IsOk())
-      return status;
-    active_package_ = "";
-  }
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+
   return Status(kOk);
 }
 
 DeviceManager::DeviceManager(Adb* adb) : adb_(adb) {
-  CHECK(adb_);
+  // TODO(Peter Wang): Need to implement it for android xwalk.
 }
 
 DeviceManager::~DeviceManager() {}
 
 Status DeviceManager::AcquireDevice(scoped_ptr<Device>* device) {
-  std::vector<std::string> devices;
-  Status status = adb_->GetDevices(&devices);
-  if (!status.IsOk())
-    return status;
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) device;
 
-  if (devices.empty())
-    return Status(kUnknownError, "There are no devices online");
-
-  base::AutoLock lock(devices_lock_);
-  status = Status(kUnknownError, "All devices are in use (" +
-                  base::IntToString(devices.size()) + " online)");
-  std::vector<std::string>::iterator iter;
-  for (iter = devices.begin(); iter != devices.end(); iter++) {
-    if (!IsDeviceLocked(*iter)) {
-      device->reset(LockDevice(*iter));
-      status = Status(kOk);
-      break;
-    }
-  }
-  return status;
+  return Status(kOk);
 }
 
 Status DeviceManager::AcquireSpecificDevice(
     const std::string& device_serial, scoped_ptr<Device>* device) {
-  std::vector<std::string> devices;
-  Status status = adb_->GetDevices(&devices);
-  if (!status.IsOk())
-    return status;
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) device_serial;
+  (void) device;
 
-  if (std::find(devices.begin(), devices.end(), device_serial) == devices.end())
-    return Status(kUnknownError,
-        "Device " + device_serial + " is not online");
-
-  base::AutoLock lock(devices_lock_);
-  if (IsDeviceLocked(device_serial)) {
-    status = Status(kUnknownError,
-        "Device " + device_serial + " is already in use");
-  } else {
-    device->reset(LockDevice(device_serial));
-    status = Status(kOk);
-  }
-  return status;
+  return Status(kOk);
 }
 
 void DeviceManager::ReleaseDevice(const std::string& device_serial) {
-  base::AutoLock lock(devices_lock_);
-  active_devices_.remove(device_serial);
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) device_serial;
 }
 
 Device* DeviceManager::LockDevice(const std::string& device_serial) {
-  active_devices_.push_back(device_serial);
-  return new Device(device_serial, adb_,
-      base::Bind(&DeviceManager::ReleaseDevice, base::Unretained(this),
-                 device_serial));
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) device_serial;
+
+  return NULL;
 }
 
 bool DeviceManager::IsDeviceLocked(const std::string& device_serial) {
-  return std::find(active_devices_.begin(), active_devices_.end(),
-                   device_serial) != active_devices_.end();
+  // TODO(Peter Wang): Need to implement it for android xwalk.
+  (void) device_serial;
+
+  return false;
 }
 
