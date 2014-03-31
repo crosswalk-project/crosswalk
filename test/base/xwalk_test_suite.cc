35ba388837cae8d575ae5ab4fb4d8dc8b46ca5c4
Thiago Marcos P. Santos <thiago.santos@intel.com>
2014-03-31 15:14:01 +0300

    M35 Gardening: Do not initialize XWalkRunner twice
    This is all being created already when we create a XWalkMainDelegate
    at XWalkTestLauncherDelegate.
    
    Reflects the simplification landed here:
    https://codereview.chromium.org/190663012

--- test/base/xwalk_test_suite.cc
+++ test/base/xwalk_test_suite.cc
@@ -4,110 +4,21 @@
 
 #include "xwalk/test/base/xwalk_test_suite.h"
 
-#include "base/command_line.h"
-#include "base/file_util.h"
-#include "base/memory/ref_counted.h"
-#include "base/metrics/stats_table.h"
-#include "base/path_service.h"
-#include "base/process/process_handle.h"
-#include "base/strings/stringprintf.h"
-#include "base/strings/utf_string_conversions.h"
-#include "xwalk/runtime/browser/xwalk_runner.h"
 #include "xwalk/runtime/common/xwalk_content_client.h"
-#include "xwalk/runtime/common/xwalk_paths.h"
-#include "content/public/test/test_launcher.h"
-#include "net/base/net_errors.h"
-#include "net/base/net_util.h"
-#include "net/dns/mock_host_resolver.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "ui/base/resource/resource_bundle.h"
-#include "ui/base/resource/resource_handle.h"
-
-#if defined(OS_POSIX)
-#include "base/memory/shared_memory.h"
-#endif
-
-namespace {
-
-void RemoveSharedMemoryFile(const std::string& filename) {
-  // Stats uses SharedMemory under the hood. On posix, this results in a file
-  // on disk.
-#if defined(OS_POSIX)
-  base::SharedMemory memory;
-  memory.Delete(filename);
-#endif
-}
-
-}  // namespace
-
-class XWalkTestSuiteInitializer : public testing::EmptyTestEventListener {
- public:
-  XWalkTestSuiteInitializer() {
-  }
-
-  virtual void OnTestStart(const testing::TestInfo& test_info) OVERRIDE {
-    content_client_.reset(new xwalk::XWalkContentClient);
-    content::SetContentClient(content_client_.get());
-
-    xwalk_runner_.reset(new xwalk::XWalkRunner());
-    SetBrowserClientForTesting(xwalk_runner_->GetContentBrowserClient());
-  }
-
-  virtual void OnTestEnd(const testing::TestInfo& test_info) OVERRIDE {
-    xwalk_runner_.reset();
-    content_client_.reset();
-    content::SetContentClient(NULL);
-  }
-
- private:
-  scoped_ptr<xwalk::XWalkContentClient> content_client_;
-  scoped_ptr<xwalk::XWalkRunner> xwalk_runner_;
-
-  DISALLOW_COPY_AND_ASSIGN(XWalkTestSuiteInitializer);
-};
 
 XWalkTestSuite::XWalkTestSuite(int argc, char** argv)
     : content::ContentTestSuiteBase(argc, argv) {
 }
 
-XWalkTestSuite::~XWalkTestSuite() {
-}
+XWalkTestSuite::~XWalkTestSuite() {}
 
 void XWalkTestSuite::Initialize() {
-  xwalk::RegisterPathProvider();
-
   // Initialize after overriding paths as some content paths depend on correct
   // values for DIR_EXE and DIR_MODULE.
   content::ContentTestSuiteBase::Initialize();
 
-  base::FilePath pak_dir;
-  PathService::Get(base::DIR_MODULE, &pak_dir);
-  DCHECK(!pak_dir.empty());
-  base::FilePath resources_pack_path;
-  resources_pack_path = pak_dir.Append(FILE_PATH_LITERAL("xwalk.pak"));
-  ui::ResourceBundle::InitSharedInstanceWithPakPath(resources_pack_path);
   {
     xwalk::XWalkContentClient client;
     content::ContentTestSuiteBase::RegisterContentSchemes(&client);
   }
-
-  stats_filename_ = base::StringPrintf("unit_tests-%d",
-                                       base::GetCurrentProcId());
-  RemoveSharedMemoryFile(stats_filename_);
-  stats_table_.reset(new base::StatsTable(stats_filename_, 20, 200));
-  base::StatsTable::set_current(stats_table_.get());
-
-  testing::TestEventListeners& listeners =
-      testing::UnitTest::GetInstance()->listeners();
-  listeners.Append(new XWalkTestSuiteInitializer);
-}
-
-void XWalkTestSuite::Shutdown() {
-  ResourceBundle::CleanupSharedInstance();
-
-  base::StatsTable::set_current(NULL);
-  stats_table_.reset();
-  RemoveSharedMemoryFile(stats_filename_);
-
-  base::TestSuite::Shutdown();
 }
