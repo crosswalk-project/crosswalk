148958a482976b92f1aa4de24b8f3b64af4853e0
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2014-04-01 17:20:31 +0300

    M35 gardening.
    Update RuntimeContext::RequestMidiSysExPermission()'s signature.
    
    https://codereview.chromium.org/185863003

--- runtime/browser/runtime_context.h
+++ runtime/browser/runtime_context.h
@@ -75,6 +75,7 @@ class RuntimeContext
       int render_view_id,
       int bridge_id,
       const GURL& requesting_frame,
+      bool user_gesture,
       const MidiSysExPermissionCallback& callback) OVERRIDE { }
   virtual void CancelMidiSysExPermissionRequest(
       int render_process_id,
