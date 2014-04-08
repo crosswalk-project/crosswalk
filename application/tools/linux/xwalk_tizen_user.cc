c52c5427745906e3aaaa6c3fa3f6490cf7c8c917
Baptiste DURAND <baptiste.durand@open.eurogiciel.org>
2014-04-08 18:40:22 +0200

    Adapt user check to multi user architecture.
    BUG:https://crosswalk-project.org/jira/browse/XWALK-1649
    
    Change-Id: I2e38c4582e7b0bf1a024ffdad22d1bd14056bb11
    Signed-off-by: Baptiste DURAND <baptiste.durand@open.eurogiciel.org>

--- application/tools/linux/xwalk_tizen_user.cc
+++ application/tools/linux/xwalk_tizen_user.cc
@@ -10,30 +10,36 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <pwd.h>
+#include <grp.h>
 
-int xwalk_tizen_set_home_for_user_app(void) {
-#if !defined(OS_TIZEN)
-  return 0;
-#endif
-
-  // Tizen doesn't set HOME by default on login for user "app".
-  uid_t uid = getuid();
-  struct passwd* passwd = getpwuid(uid);  // NOLINT
-
-  if (!passwd)
-    return -ENOENT;
-
-  if (strcmp(passwd->pw_name, "app")) {
-    fprintf(stderr,
-            "User is not 'app', launching an application will not work\n");
+int xwalk_tizen_check_user_app(void) {
+  char* buffer = NULL;
+  int err = 0;
+  struct group grp;
+  struct group* current_g;
+  int64_t len = sysconf(_SC_GETGR_R_SIZE_MAX);
+  if (len == -1)
+    len = 1024;
+  buffer = reinterpret_cast<char*>(malloc((size_t)len));
+  if (!buffer)
     return -EINVAL;
-  }
 
-  if (setenv("HOME", passwd->pw_dir, true) != 0) {
-    fprintf(stderr, "Couldn't set 'HOME' env variable to '%s'\n",
-            passwd->pw_dir);
+  err = getgrgid_r(getgid(), &grp, buffer, len, &current_g);
+  if (err) {
+  fprintf(stderr, "group can't be determined");
+    fprintf(stderr, "launching an application will not work\n");
+    free(buffer);
     return -EINVAL;
+  } else {
+    if ( (!current_g) || (
+        strcmp(current_g->gr_name, "users") &&
+        strcmp(current_g->gr_name, "app"))) {
+      fprintf(stderr, "group '%s' is not allowed :",
+            current_g ? current_g->gr_name : "<NULL>");
+      fprintf(stderr, "launching an application will not work\n");
+      free(buffer);
+      return -EINVAL;
+    }
   }
-
   return 0;
 }
