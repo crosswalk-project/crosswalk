356c01ca7d29f14774591416a7e2a498755869aa
Long Xiang <xiang.long@intel.com>
2014-04-11 18:05:48 +0800

    [Application][Tizen] Move extension process logic to a standalone thread
    The previous EP(ExtensionProcess) & launcher merge patch changed the launcher's
    main thread glib message loop to Chromium message loop which is MessagePumpOzone
    in Tizen. This leads to some asynchronous DBus function and signals cannot be
    successfully called.
    
    To divide the old launcher logic(mainly communicate with DBus) and new EP
    logic this patch added a new XWalkLauncherExtensionService running in a separate
    thread.

---
+++ application/tools/linux/xwalk_extension_process_launcher.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef XWALK_APPLICATION_TOOLS_LINUX_XWALK_EXTENSION_PROCESS_LAUNCHER_H_
+#define XWALK_APPLICATION_TOOLS_LINUX_XWALK_EXTENSION_PROCESS_LAUNCHER_H_
+
+#include <string>
+
+#include "base/threading/thread.h"
+
+namespace xwalk {
+namespace extensions {
+class XWalkExtensionProcess;
+}
+}
+
+class XWalkExtensionProcessLauncher: public base::Thread {
+ public:
+  XWalkExtensionProcessLauncher();
+  ~XWalkExtensionProcessLauncher();
+
+  // Will be called in launcher's main thread.
+  void Launch(const std::string& channel_id, int channel_fd);
+
+  bool is_started() const { return is_started_; }
+
+ private:
+  void StartExtensionProcess(const std::string& channel_id, int channel_fd);
+
+  bool is_started_;
+  scoped_ptr<xwalk::extensions::XWalkExtensionProcess> extension_process_;
+};
+
+#endif  // XWALK_APPLICATION_TOOLS_LINUX_XWALK_EXTENSION_PROCESS_LAUNCHER_H_
