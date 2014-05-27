bc939f97d02632d1bbacf5ea107ea159e308decc
hengzhi.wu <hengzhix.wu@intel.com>
2014-05-27 11:25:20 +0800

    [Android] Add test case for stopLoading().
    This patch is to add test cases for stopLoading().
    Open an inexistent url, then execute stopLoading(), the load will be
    stopped, title is empty.

--- test/android/core/javatests/src/org/xwalk/core/xwview/test/XWalkViewTestBase.java
+++ test/android/core/javatests/src/org/xwalk/core/xwview/test/XWalkViewTestBase.java
@@ -544,4 +544,23 @@ public class XWalkViewTestBase
             }
         });
     }
+
+    protected void stopLoadingOnUiThread() throws Exception {
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                mXWalkView.stopLoading();
+            }
+        });
+    }
+
+    protected void stopLoadingSync(final String url) throws Exception {
+        CallbackHelper pageFinishedHelper = mTestHelperBridge.getOnPageFinishedHelper();
+        int currentCallCount = pageFinishedHelper.getCallCount();
+
+        loadUrlAsync(url);
+        stopLoadingOnUiThread();
+        pageFinishedHelper.waitForCallback(currentCallCount, 1, WAIT_TIMEOUT_SECONDS,
+                TimeUnit.SECONDS);
+    }
 }
