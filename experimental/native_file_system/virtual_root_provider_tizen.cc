c8f24e4cd2f4031b16b2d06308e4f6752fb0d3d7
Shawn Gao <shawngao5@gmail.com>
2014-06-11 12:08:01 +0800

    [Tizen] Native File System API
    BUG=XWALK-1875

--- experimental/native_file_system/virtual_root_provider_tizen.cc
+++ experimental/native_file_system/virtual_root_provider_tizen.cc
@@ -4,9 +4,35 @@
 
 #include "xwalk/experimental/native_file_system/virtual_root_provider.h"
 
-#include "base/logging.h"
+#include <tzplatform_config.h>
+
+#include <map>
+#include <string>
 
 VirtualRootProvider::VirtualRootProvider() {
-  // TODO(shawngao5): Tizen support to be added.
-  NOTIMPLEMENTED();
+  const char* names[] = {
+      "CAMERA",
+      "DOCUMENTS",
+      "IMAGES",
+      "SOUNDS",
+      "VIDEOS",
+  };
+
+  tzplatform_variable dirs[] = {
+      TZ_USER_CAMERA,
+      TZ_USER_DOCUMENTS,
+      TZ_USER_IMAGES,
+      TZ_USER_SOUNDS,
+      TZ_USER_VIDEOS
+  };
+
+  for (unsigned int i = 0; i < sizeof(names) / sizeof(names[0]); ++i) {
+    virtual_root_map_[names[i]] =
+        base::FilePath::FromUTF8Unsafe(
+            std::string(tzplatform_getenv(dirs[i])));
+  }
+
+  virtual_root_map_["RINGTONES"] =
+      base::FilePath::FromUTF8Unsafe(
+          std::string(tzplatform_mkpath(TZ_USER_SHARE, "settings/Ringtones")));
 }
