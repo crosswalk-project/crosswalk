c165624f2996de2ae9af437a058d8239962857cc
hengzhi.wu <hengzhix.wu@intel.com>
2014-08-14 16:01:07 +0800

    [Android] Add test case for OnScaleChanged.
    This patch is to add test case for OnScaleChanged().

---
+++ test/android/core/javatests/src/org/xwalk/core/xwview/test/OnScaleChangedTest.java
@@ -0,0 +1,34 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.core.xwview.test;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.chromium.base.test.util.Feature;
+
+/**
+ * Test suite for OnScaleChanged().
+ */
+public class OnScaleChangedTest extends XWalkViewTestBase {
+    private TestHelperBridge.OnScaleChangedHelper mOnScaleChangedHelper;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mOnScaleChangedHelper = mTestHelperBridge.getOnScaleChangedHelper();
+    }
+
+    @SmallTest
+    @Feature({"OnScaleChanged"})
+    public void testOnScaleChanged() throws Throwable {
+        final String name = "scale_changed.html";
+        String fileContent = getFileContent(name);
+        int count = mOnScaleChangedHelper.getCallCount();
+
+        loadDataAsync(null, fileContent, "text/html", false);
+        mOnScaleChangedHelper.waitForCallback(count);
+        assertTrue(Float.compare(mOnScaleChangedHelper.getScale(), 0.0f) > 0);
+    }
+}
