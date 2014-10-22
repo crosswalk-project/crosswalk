cc7f50ed1a9c1d6fea05af4098f61bbdfe32350f
Andrey Malyshev <Andrey.Malyshev@intel.com>
2014-10-22 18:00:04 +0400

    restored functionality of XDK agents (AppPreview and AppPreviewXWalk) broken since xwalk9. The fix consist in checking of uid passed thorough AllowConnectionFromUid and only if uid is not eq to the stored, call chromium corresponded function to check

--- runtime/browser/android/xwalk_dev_tools_server.cc
+++ runtime/browser/android/xwalk_dev_tools_server.cc
@@ -229,7 +229,8 @@ void XWalkDevToolsServer::Start(bool allow_debug_permission) {
   net::UnixDomainServerSocket::AuthCallback auth_callback =
       allow_debug_permission ?
           base::Bind(&AuthorizeSocketAccessWithDebugPermission) :
-          base::Bind(&content::CanUserConnectToDevTools);
+          base::Bind(&XWalkDevToolsServer::CanUserConnectToDevTools,
+              base::Unretained(this));
 
   scoped_ptr<content::DevToolsHttpHandler::ServerSocketFactory> factory(
       new UnixDomainServerSocketFactory(socket_name_));
