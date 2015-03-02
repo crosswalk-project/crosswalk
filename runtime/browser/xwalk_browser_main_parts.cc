fecb166161877f85a30406fede214e4732010a9b
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-03-02 16:18:56 +0200

    Change the way we comment out SIMD.js support.
    Move the comment blocks to separate lines, so that we do not screw up
    `git blame' every time we enable or disable SIMD.js.

--- runtime/browser/xwalk_browser_main_parts.cc
+++ runtime/browser/xwalk_browser_main_parts.cc
@@ -112,13 +112,15 @@ void XWalkBrowserMainParts::PreMainMessageLoopStart() {
   command_line->AppendSwitch(switches::kAllowFileAccessFromFiles);
 
   // Enable SIMD.JS API by default.
-  /*std::string js_flags("--simd_object");
+#if 0
+  std::string js_flags("--simd_object");
   if (command_line->HasSwitch(switches::kJavaScriptFlags)) {
     js_flags += " ";
     js_flags +=
         command_line->GetSwitchValueASCII(switches::kJavaScriptFlags);
   }
-  command_line->AppendSwitchASCII(switches::kJavaScriptFlags, js_flags);*/
+  command_line->AppendSwitchASCII(switches::kJavaScriptFlags, js_flags);
+#endif
 
   startup_url_ = GetURLFromCommandLine(*command_line);
 }
