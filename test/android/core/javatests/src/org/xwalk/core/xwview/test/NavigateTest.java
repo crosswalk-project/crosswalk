1549bb865f6f2ca66a0e87fd51876ad4b5a484b7
hengzhi.wu <hengzhix.wu@intel.com>
2015-03-19 09:56:22 +0800

    [Android] Refine the test case for navigation.
    This patch is to refine the test case for navigation.
    Verify the title after go back or forward.
    
    (cherry picked from commit c62c11bb999ae975d628d4af05a305762affb871)

--- test/android/core/javatests/src/org/xwalk/core/xwview/test/NavigateTest.java
+++ test/android/core/javatests/src/org/xwalk/core/xwview/test/NavigateTest.java
@@ -6,30 +6,61 @@
 package org.xwalk.core.xwview.test;
 
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.chromium.base.test.util.Feature;
+import org.chromium.net.test.util.TestWebServer;
+
+import org.xwalk.core.xwview.test.util.CommonResources;
 
 /**
  * Test suite for Navigate().
  */
 public class NavigateTest extends XWalkViewTestBase {
+    private TestWebServer mWebServer;
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
+        mWebServer = TestWebServer.start();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mWebServer.shutdown();
+        super.tearDown();
+    }
+
+    private String addPageToTestServer(TestWebServer webServer, String httpPath, String html) {
+        List<Pair<String, String>> headers = new ArrayList<Pair<String, String>>();
+        headers.add(Pair.create("Content-Type", "text/html"));
+        headers.add(Pair.create("Cache-Control", "no-store"));
+        return webServer.setResponse(httpPath, html, headers);
     }
 
     @SmallTest
     @Feature({"Navigate"})
     public void testNavigate() throws Throwable {
-        final String url1 = "about:blank";
-        final String url2 = "file:///android_asset/www/index.html";
-        final String title ="Crosswalk Sample Application";
-        loadUrlSync(url1);
-        loadUrlSync(url2);
-        assertEquals(title, getTitleOnUiThread());
+        String path = "/test.html";
+        String pageContent = CommonResources.makeHtmlPageFrom("<title>Test</title>",
+                "<div> The title is: Test </div>");
+        final String firstUrl = addPageToTestServer(mWebServer, path, pageContent);
+        final String firstTitle = "Test";
+        final String secondUrl = "file:///android_asset/www/index.html";
+        final String secondTitle ="Crosswalk Sample Application";
+
+        loadUrlSync(firstUrl);
+        loadUrlSync(secondUrl);
+        assertEquals(secondUrl, getUrlOnUiThread());
+        assertEquals(secondTitle, getTitleOnUiThread());
         goBackSync();
-        assertEquals(url1, getUrlOnUiThread());
+        assertEquals(firstUrl, getUrlOnUiThread());
+        assertEquals(firstTitle, getTitleOnUiThread());
         goForwardSync();
-        assertEquals(url2, getUrlOnUiThread());
+        assertEquals(secondUrl, getUrlOnUiThread());
+        assertEquals(secondTitle, getTitleOnUiThread());
     }
 }
