77ab64f8f7fc2f54403db14477df324de292089f
Alexis Menard <alexis.menard@intel.com>
2015-03-24 14:26:09 -0300

    Make native filesystem extension work on Windows.
    I believe it needs to be extended and completed.

--- experimental/native_file_system/virtual_root_provider_win.cc
+++ experimental/native_file_system/virtual_root_provider_win.cc
@@ -4,9 +4,42 @@
 
 #include "xwalk/experimental/native_file_system/virtual_root_provider.h"
 
-#include "base/logging.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+
+bool VirtualRootProvider::testing_enabled_ = false;
 
 VirtualRootProvider::VirtualRootProvider() {
-  // TODO(darktears): Windows support to be added.
-  NOTIMPLEMENTED();
+  if (!virtual_root_map_.empty())
+    return;
+
+  if (testing_enabled_) {
+    base::GetTempDir(&home_path_);
+  } else {
+    home_path_ = base::GetHomeDir();
+  }
+
+  virtual_root_map_["DESKTOP"] =
+      home_path_.Append(FILE_PATH_LITERAL("Desktop"));
+  virtual_root_map_["DOWNLOADS"] =
+      home_path_.Append(FILE_PATH_LITERAL("Downloads"));
+  virtual_root_map_["DOCUMENTS"] =
+      home_path_.Append(FILE_PATH_LITERAL("Documents"));
+  virtual_root_map_["MUSIC"] =
+      home_path_.Append(FILE_PATH_LITERAL("Music"));
+  virtual_root_map_["PICTURES"] =
+      home_path_.Append(FILE_PATH_LITERAL("Pictures"));
+  virtual_root_map_["VIDEOS"] =
+      home_path_.Append(FILE_PATH_LITERAL("Videos"));
+}
+
+void VirtualRootProvider::SetTesting(bool testing_enabled) {
+  testing_enabled_ = testing_enabled;
+  base::FilePath tmp_path;
+  base::GetTempDir(&tmp_path);
+  base::FilePath doc_path =
+      tmp_path.Append(FILE_PATH_LITERAL("Documents"));
+  if (!DirectoryExists(doc_path)) {
+    CreateDirectory(doc_path);
+  }
 }
