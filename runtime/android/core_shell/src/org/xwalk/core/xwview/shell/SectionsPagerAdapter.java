2e236e131c461da2cf4fa56d2b7987d915780543
Dong Jun <jun.dong@intel.com>
2015-03-31 17:20:41 +0800

    XWalkViewShellActivity upgrade
    Replace FragmentActivity with XWalkActivity.
    XWalkCoreWrapper can not accessed by classes out of org.xwalk.core.
    Instead, these classes must inherit from XWalkActivity to access these
    functionality.
    For LZMA, XWalkViewShellActivity rely on the decompressing function
    provided by XWalkActivity, in order to use XWalkView.
    (cherry picked from commit a90147682a4bbfb962a9c8fdcbb7c7ccf7cd2280)

--- runtime/android/core_shell/src/org/xwalk/core/xwview/shell/SectionsPagerAdapter.java
+++ runtime/android/core_shell/src/org/xwalk/core/xwview/shell/SectionsPagerAdapter.java
@@ -9,11 +9,11 @@ import java.util.ArrayList;
 import org.xwalk.core.XWalkView;
 
 import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentPagerAdapter;
 
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
     private ArrayList<Fragment> mFragmentList;
