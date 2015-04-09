crosswalk-project/crosswalk

commit 2e723a92b798630f32c775ff7cdcb4f10dd1b79b

[Android] make_apk: Simplify TryCodeBaseFromVersionName().
Make the code much simpler and easier to test:
- Rename the function to MakeCodeBaseFromAppVersion() to better indicate
  what it is actually doing.
- Use a simpler regular expression to sanitize the version number, the
  previous one was needlessly bloated and also wrong (it accepted
  version strings such as "1.......2......3", for example).
- Use basic string and integer manipulation to return a formatted string
  instead of needlessly fiddling around with str.zfill(), str.strip()
  and regular expression groups.
- Stop making the function abort the entire program on error, it makes
  unit testing impossible. Instead, just return None and let the calling
  function handle the error.
- Make the error message easier to understand.

BUG=XWALK-3473

app/tools/android/make_apk.py
@@ -146,34 +146,24 @@ def FindExtensionJars(root_path):
   return extension_jars
 
 
-# Try to parse out suitable app_versionCodeBase based on app_version,
-# app_version "xx.xx.xxx" will generate app_versionCodeBase "xxxxxxx"
-# For example,  "1.2.3" will generate app_versionCodeBase "0102003"
-# "1.2" will generate "0102000"
-# If app_version does not match
-# r'(\d{1,2}\.+)(\d{1,2}\.+)(\d{1,3})$|(|\d{1,2}\.+)(\d{1,2})$',
-# notify user the failure
-def TryCodeBaseFromVersionName(app_version):
-  m = re.match(r'(\d{1,2}\.+)(\d{1,2}\.+)(\d{1,3})$|(|\d{1,2}\.+)(\d{1,2})$',
-               app_version)
-  if not m:
-    print('Can not parse out app_versionCodeBase from app_version, '
-          'please specify --app-versionCode or --app-versionCodeBase : '
-          'app_version=%s' % (app_version))
-    sys.exit(12)
+def MakeCodeBaseFromAppVersion(app_version):
+  """
+  Generates a string suitable for an Android versionCode from a version number.
 
-  versionList = []
-  for item in m.groups():
-    if (item and len(item) > 0):
-      versionList.append(item.strip('.'))
-  n = len(versionList)
-  while n < 3:
-    versionList.append('0')
-    n = n + 1
-  versionCodeBase = versionList[0].zfill(2)
-  versionCodeBase = versionCodeBase + versionList[1].zfill(2)
-  versionCodeBase = versionCodeBase + versionList[2].zfill(3)
-  return versionCodeBase
+  The returned string will be appended to the ABI prefix digit to create a
+  version number for the android:versionCode attribute of the Android manifest
+  file.
+
+  |app_version| must be a string with the format "ab.cd.efg", all digits but
+   'a' being optional.
+  If |app_version|'s format is invalid, this function returns None.
+  """
+  version_re = r'\d{1,2}(\.\d{1,2}(\.\d{1,3})?)?$'
+  if not re.match(version_re, app_version):
+    return None
+  version_numbers = map(int, app_version.split('.'))
+  version_numbers.extend([0] * (3 - len(version_numbers))) # Pad to 3 parts.
+  return '%02d%02d%03d' % tuple(version_numbers)
 
 
 # Follows the recommendation from
@@ -206,7 +196,16 @@ def MakeVersionCode(options, app_version):
   # If both --app-versionCode and --app-versionCodeBase not specified,
   # try to parse out versionCodeBase based on app_version
   else:
-    b = TryCodeBaseFromVersionName(app_version)
+    b = MakeCodeBaseFromAppVersion(app_version)
+    if b is None:
+      print('Error: Cannot create a valid android:versionCode from version '
+            'number "%s". Valid version numbers must follow the format '
+            '"ab.cd.efg", where only \'a\' is mandatory. For example, "1", '
+            '"3.45" and "12.3.976" are all valid version numbers. If you use '
+            'a different versioning scheme, please either "--app-versionCode" '
+            'or "--app-versionCodeBase" to manually provide the '
+            'android:versionCode number that your APK will use.' % app_version)
+      sys.exit(12)
   # zero pad to 7 digits, middle digits can be used for other
   # features, according to recommendation in URL
   return '%s%s' % (abi, b.zfill(7))

commit 30b9c3f871863f5a092319b2db1b0af841cf9948

[Android] make_apk: Fix Python 3 regression after 2e723a9.
Commit 2e723a9 ("[Android] make_apk: Simplify
TryCodeBaseFromVersionName()") introduced a regression when run with
Python 3.

In Python 3, map() returns an iterator, so we cannot call extend() on
it. Fix it by using a list comprehension instead of map().

BUG=XWALK-3903

app/tools/android/make_apk.py
@@ -161,7 +161,7 @@ def MakeCodeBaseFromAppVersion(app_version):
   version_re = r'\d{1,2}(\.\d{1,2}(\.\d{1,3})?)?$'
   if not re.match(version_re, app_version):
     return None
-  version_numbers = map(int, app_version.split('.'))
+  version_numbers = [int(i) for i in app_version.split('.')]
   version_numbers.extend([0] * (3 - len(version_numbers))) # Pad to 3 parts.
   return '%02d%02d%03d' % tuple(version_numbers)
 