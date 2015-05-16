c85392ee73ae7751aea5b6f64a0d1c0e5d3183ba
Mrunal Kapade <mrunal.kapade@intel.com>
2015-05-15 19:54:52 -0700

    M44 Rebase: Update the Remote Debugging and DevTools as per upstream changes
    The DevToolsHttpHandler has been componentized upstream
    
    https://codereview.chromium.org/1086623002
    https://codereview.chromium.org/1089413002
    https://codereview.chromium.org/1056343004
    https://codereview.chromium.org/1114503002

--- runtime/browser/devtools/remote_debugging_server.cc
+++ runtime/browser/devtools/remote_debugging_server.cc
@@ -4,16 +4,30 @@
 
 #include "xwalk/runtime/browser/devtools/remote_debugging_server.h"
 
-#include "content/public/browser/devtools_http_handler.h"
+#include "components/devtools_http_handler/devtools_http_handler.h"
+#include "components/devtools_http_handler/devtools_http_handler_delegate.h"
+#include "content/public/browser/devtools_agent_host.h"
+#include "content/public/browser/devtools_frontend_host.h"
+#include "content/public/browser/render_widget_host_view.h"
+#include "content/public/browser/web_contents.h"
+#include "grit/xwalk_resources.h"
 #include "net/base/net_errors.h"
 #include "net/socket/tcp_server_socket.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/snapshot/snapshot.h"
 #include "xwalk/runtime/browser/devtools/xwalk_devtools_delegate.h"
 #include "xwalk/runtime/browser/xwalk_browser_context.h"
+#include "xwalk/runtime/common/xwalk_content_client.h"
+
+using content::DevToolsAgentHost;
+using content::RenderViewHost;
+using content::RenderWidgetHostView;
+using content::WebContents;
 
 namespace xwalk {
 
 class TCPServerSocketFactory
-    : public content::DevToolsHttpHandler::ServerSocketFactory {
+    : public devtools_http_handler::DevToolsHttpHandler::ServerSocketFactory {
  public:
   TCPServerSocketFactory(
     const std::string& address, int port, int backlog)
@@ -23,7 +37,7 @@ class TCPServerSocketFactory
   }
 
  private:
-  // content::DevToolsHttpHandler::ServerSocketFactory.
+  // devtools_http_handler::DevToolsHttpHandler::ServerSocketFactory.
   scoped_ptr<net::ServerSocket> CreateForHttpServer() override {
     scoped_ptr<net::ServerSocket> socket(
         new net::TCPServerSocket(NULL, net::NetLog::Source()));
@@ -37,19 +51,101 @@ class TCPServerSocketFactory
   DISALLOW_COPY_AND_ASSIGN(TCPServerSocketFactory);
 };
 
+class XWalkDevToolsHttpHandlerDelegate :
+    public devtools_http_handler::DevToolsHttpHandlerDelegate {
+ public:
+  XWalkDevToolsHttpHandlerDelegate();
+  ~XWalkDevToolsHttpHandlerDelegate() override;
+
+  // DevToolsHttpHandlerDelegate implementation.
+  std::string GetDiscoveryPageHTML() override;
+  std::string GetFrontendResource(const std::string& path) override;
+  std::string GetPageThumbnailData(const GURL& url) override;
+  void ProcessAndSaveThumbnail(const GURL& url,
+                               scoped_refptr<base::RefCountedBytes> png);
+
+ private:
+  using ThumbnailMap = std::map<GURL, std::string>;
+  ThumbnailMap thumbnail_map_;
+
+  base::WeakPtrFactory<XWalkDevToolsHttpHandlerDelegate> weak_factory_;
+  DISALLOW_COPY_AND_ASSIGN(XWalkDevToolsHttpHandlerDelegate);
+};
+
+XWalkDevToolsHttpHandlerDelegate::XWalkDevToolsHttpHandlerDelegate()
+    : weak_factory_(this) {
+}
+
+XWalkDevToolsHttpHandlerDelegate::~XWalkDevToolsHttpHandlerDelegate() {
+}
+
+std::string XWalkDevToolsHttpHandlerDelegate::GetDiscoveryPageHTML() {
+  return ResourceBundle::GetSharedInstance().GetRawDataResource(
+      IDR_DEVTOOLS_FRONTEND_PAGE_HTML).as_string();
+}
+
+std::string XWalkDevToolsHttpHandlerDelegate::GetFrontendResource(
+    const std::string& path) {
+  // TODO: need to return proper value
+  return content::DevToolsFrontendHost::GetFrontendResource(path).as_string();
+}
+
+std::string XWalkDevToolsHttpHandlerDelegate::GetPageThumbnailData(
+    const GURL& url) {
+  if (thumbnail_map_.find(url) != thumbnail_map_.end())
+    return thumbnail_map_[url];
+  // TODO(YangangHan): Support real time thumbnail.
+  DevToolsAgentHost::List agents =
+      content::DevToolsAgentHost::GetOrCreateAll();
+  for (auto& it : agents) {
+    content::WebContents* web_contents = it.get()->GetWebContents();
+    if (web_contents && web_contents->GetURL() == url) {
+      RenderWidgetHostView* render_widget_host_view =
+          web_contents->GetRenderWidgetHostView();
+      if (!render_widget_host_view)
+        continue;
+      gfx::Rect snapshot_bounds(
+        render_widget_host_view->GetViewBounds().size());
+      ui::GrabViewSnapshotAsync(
+        render_widget_host_view->GetNativeView(),
+        snapshot_bounds,
+        base::ThreadTaskRunnerHandle::Get(),
+        base::Bind(&XWalkDevToolsHttpHandlerDelegate::ProcessAndSaveThumbnail,
+                   weak_factory_.GetWeakPtr(),
+                   url));
+        break;
+    }
+  }
+  return std::string();
+}
+
+void XWalkDevToolsHttpHandlerDelegate::ProcessAndSaveThumbnail(
+    const GURL& url,
+    scoped_refptr<base::RefCountedBytes> png) {
+  if (!png.get())
+    return;
+  const std::vector<unsigned char>& png_data = png->data();
+  std::string png_string_data(reinterpret_cast<const char*>(&png_data[0]),
+                              png_data.size());
+  thumbnail_map_[url] = png_string_data;
+}
+
 RemoteDebuggingServer::RemoteDebuggingServer(
     XWalkBrowserContext* browser_context,
     const std::string& ip,
     int port,
     const std::string& frontend_url) {
   base::FilePath output_dir;
-  scoped_ptr<content::DevToolsHttpHandler::ServerSocketFactory> factory(
-      new TCPServerSocketFactory(ip, port, 1));
-  devtools_http_handler_.reset(content::DevToolsHttpHandler::Start(
-      factory.Pass(),
-      frontend_url,
-      new XWalkDevToolsHttpHandlerDelegate(),
-      output_dir));
+  scoped_ptr<devtools_http_handler::DevToolsHttpHandler::ServerSocketFactory>
+      factory(new TCPServerSocketFactory(ip, port, 1));
+  devtools_http_handler_.reset(new devtools_http_handler::DevToolsHttpHandler(
+          factory.Pass(),
+          frontend_url,
+          new XWalkDevToolsHttpHandlerDelegate(),
+          output_dir,
+          output_dir,
+          std::string(),
+          xwalk::GetUserAgent()));
   port_ = port;
 }
 
--- runtime/browser/devtools/xwalk_devtools_delegate.cc
+++ runtime/browser/devtools/xwalk_devtools_delegate.cc
@@ -10,170 +10,16 @@
 #include "base/memory/ref_counted_memory.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/thread_task_runner_handle.h"
-#include "content/public/browser/devtools_agent_host.h"
-#include "content/public/browser/devtools_http_handler.h"
-#include "content/public/browser/devtools_target.h"
 #include "content/public/browser/favicon_status.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_view_host.h"
-#include "content/public/browser/render_widget_host_view.h"
-#include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
-#include "grit/xwalk_resources.h"
 #include "net/socket/tcp_listen_socket.h"
-#include "ui/base/resource/resource_bundle.h"
-#include "ui/snapshot/snapshot.h"
-
-using content::DevToolsAgentHost;
-using content::RenderViewHost;
-using content::RenderWidgetHostView;
-using content::WebContents;
-
-namespace {
-
-const char kTargetTypePage[] = "page";
-const char kTargetTypeServiceWorker[] = "service_worker";
-const char kTargetTypeOther[] = "other";
-
-const char kWebUIScheme[] = "chrome";
-
-class Target : public content::DevToolsTarget {
- public:
-  explicit Target(scoped_refptr<content::DevToolsAgentHost> agent_host);
-
-  std::string GetId() const override { return agent_host_->GetId(); }
-  std::string GetType() const override {
-      switch (agent_host_->GetType()) {
-        case content::DevToolsAgentHost::TYPE_WEB_CONTENTS:
-           return kTargetTypePage;
-         case content::DevToolsAgentHost::TYPE_SERVICE_WORKER:
-           return kTargetTypeServiceWorker;
-         default:
-           break;
-       }
-       return kTargetTypeOther;
-     }
-  std::string GetTitle() const override {
-    return agent_host_->GetTitle();
-  }
-  std::string GetDescription() const override { return std::string(); }
-  GURL GetURL() const override { return  agent_host_->GetURL(); }
-  GURL GetFaviconURL() const override { return favicon_url_; }
-  base::TimeTicks GetLastActivityTime() const override {
-    return last_activity_time_;
-  }
-  std::string GetParentId() const override { return std::string(); }
-  bool IsAttached() const override {
-    return agent_host_->IsAttached();
-  }
-  scoped_refptr<DevToolsAgentHost> GetAgentHost() const override {
-    return agent_host_;
-  }
-  bool Activate() const override;
-  bool Close() const override;
-
- private:
-  GURL GetFaviconDataURL(WebContents* web_contents) const;
-
-  scoped_refptr<DevToolsAgentHost> agent_host_;
-  std::string id_;
-  std::string title_;
-  GURL favicon_url_;
-  base::TimeTicks last_activity_time_;
-};
-
-Target::Target(scoped_refptr<content::DevToolsAgentHost> agent_host)
-    : agent_host_(agent_host) {
-  if (content::WebContents* web_contents = agent_host_->GetWebContents()) {
-// The front-end (chrome://inspect) for Android WebView doesn't need Favicon.
-#if !defined(OS_ANDROID)
-    content::NavigationController& controller = web_contents->GetController();
-    content::NavigationEntry* entry = controller.GetActiveEntry();
-    if (entry != NULL && entry->GetURL().is_valid())
-      favicon_url_ = entry->GetFavicon().url;
-    if (favicon_url_.is_empty() && !entry->GetURL().SchemeIs(kWebUIScheme))
-      favicon_url_ = GetFaviconDataURL(web_contents);
-#endif
-    last_activity_time_ = web_contents->GetLastActiveTime();
-  }
-}
-
-GURL Target::GetFaviconDataURL(WebContents* web_contents) const {
-  // Convert icon image to "data:" url.
-  xwalk::Runtime* runtime =
-      static_cast<xwalk::Runtime*>(web_contents->GetDelegate());
-  if (!runtime || runtime->app_icon().IsEmpty())
-    return GURL();
-  scoped_refptr<base::RefCountedMemory> icon_bytes =
-      runtime->app_icon().Copy1xPNGBytes();
-  std::string str_url;
-  str_url.append(reinterpret_cast<const char*>(icon_bytes->front()),
-                 icon_bytes->size());
-  base::Base64Encode(str_url, &str_url);
-  str_url.insert(0, "data:image/png;base64,");
-  return GURL(str_url);
-}
-
-bool Target::Activate() const {
-  return agent_host_->Activate();
-}
-
-bool Target::Close() const {
-  return agent_host_->Close();
-}
-
-}  // namespace
 
 namespace xwalk {
 
-namespace {
-Runtime* CreateWithDefaultWindow(
-    XWalkBrowserContext* browser_context, const GURL& url,
-    Runtime::Observer* observer) {
-  Runtime* runtime = Runtime::Create(browser_context);
-  runtime->set_observer(observer);
-  runtime->LoadURL(url);
-#if !defined(OS_ANDROID)
-  runtime->set_ui_delegate(DefaultRuntimeUIDelegate::Create(runtime));
-  runtime->Show();
-#endif
-  return runtime;
-}
-}  // namespace
-
-XWalkDevToolsHttpHandlerDelegate::XWalkDevToolsHttpHandlerDelegate() {
-}
-
-XWalkDevToolsHttpHandlerDelegate::~XWalkDevToolsHttpHandlerDelegate() {
-}
-
-std::string XWalkDevToolsHttpHandlerDelegate::GetDiscoveryPageHTML() {
-  return ResourceBundle::GetSharedInstance().GetRawDataResource(
-      IDR_DEVTOOLS_FRONTEND_PAGE_HTML).as_string();
-}
-
-void XWalkDevToolsDelegate::ProcessAndSaveThumbnail(
-    const GURL& url,
-    scoped_refptr<base::RefCountedBytes> png) {
-  if (!png.get())
-    return;
-  const std::vector<unsigned char>& png_data = png->data();
-  std::string png_string_data(reinterpret_cast<const char*>(&png_data[0]),
-                              png_data.size());
-  thumbnail_map_[url] = png_string_data;
-}
-
-bool XWalkDevToolsHttpHandlerDelegate::BundlesFrontendResources() {
-  return true;
-}
-
-base::FilePath XWalkDevToolsHttpHandlerDelegate::GetDebugFrontendDir() {
-  return base::FilePath();
-}
-
 XWalkDevToolsDelegate::XWalkDevToolsDelegate(XWalkBrowserContext* context)
-    : browser_context_(context),
-      weak_factory_(this) {
+    : browser_context_(context) {
 }
 
 XWalkDevToolsDelegate::~XWalkDevToolsDelegate() {
@@ -183,52 +29,6 @@ base::DictionaryValue* XWalkDevToolsDelegate::HandleCommand(
     content::DevToolsAgentHost* agent_host,
     base::DictionaryValue* command_dict) {
   return NULL;
-}
-
-std::string XWalkDevToolsDelegate::GetPageThumbnailData(const GURL& url) {
-  if (thumbnail_map_.find(url) != thumbnail_map_.end())
-    return thumbnail_map_[url];
-  // TODO(YangangHan): Support real time thumbnail.
-  content::DevToolsAgentHost::List agents =
-      content::DevToolsAgentHost::GetOrCreateAll();
-  for (auto& it : agents) {
-    WebContents* web_contents = it.get()->GetWebContents();
-    if (web_contents && web_contents->GetURL() == url) {
-      RenderWidgetHostView* render_widget_host_view =
-          web_contents->GetRenderWidgetHostView();
-      if (!render_widget_host_view)
-        continue;
-      gfx::Rect snapshot_bounds(
-        render_widget_host_view->GetViewBounds().size());
-      ui::GrabViewSnapshotAsync(
-        render_widget_host_view->GetNativeView(),
-        snapshot_bounds,
-        base::ThreadTaskRunnerHandle::Get(),
-        base::Bind(&XWalkDevToolsDelegate::ProcessAndSaveThumbnail,
-                   weak_factory_.GetWeakPtr(),
-                   url));
-        break;
-    }
-  }
-  return std::string();
-}
-
-scoped_ptr<content::DevToolsTarget>
-XWalkDevToolsDelegate::CreateNewTarget(const GURL& url) {
-  Runtime* runtime = CreateWithDefaultWindow(
-      browser_context_, url, this);
-  return scoped_ptr<content::DevToolsTarget>(
-      new Target(DevToolsAgentHost::GetOrCreateFor(runtime->web_contents())));
-}
-
-void XWalkDevToolsDelegate::EnumerateTargets(TargetCallback callback) {
-  TargetList targets;
-  content::DevToolsAgentHost::List agents =
-      content::DevToolsAgentHost::GetOrCreateAll();
-  for (auto& it : agents) {
-    targets.push_back(new Target(it));
-  }
-  callback.Run(targets);
 }
 
 void XWalkDevToolsDelegate::OnNewRuntimeAdded(Runtime* runtime) {
