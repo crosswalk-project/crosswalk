15f179136a28f36529d831a439ca2d0760d313cd
Olli Raula <olli.raula@intel.com>
2015-07-09 11:56:53 +0300

    Roll chromium 44.0.2403.61
    Some(temporary) changes due this change in upstream:
    https://crrev.com/1141703002
    
    -Rolled older FFMPEG
    -Disabled two unittests because main functions needed are not in
     upstream anymore
    -Changed one function call because change in upstream
    -dropped two ffmpeg related commits from chromium repo(because upstream)

--- sysapps/common/sysapps_manager_unittest.cc
+++ sysapps/common/sysapps_manager_unittest.cc
@@ -1,7 +1,7 @@
 // Copyright (c) 2013 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+/**
 #include "xwalk/sysapps/common/sysapps_manager.h"
 
 #include "base/command_line.h"
@@ -120,3 +120,5 @@ TEST_F(XWalkSysAppsManagerTest, GetCPUProvider) {
   // the same provider.
   EXPECT_EQ(provider, manager.GetCPUInfoProvider());
 }
+*/
+
