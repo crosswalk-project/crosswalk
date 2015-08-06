8a056b2fe650a601f032491a762c83fdc3a2d2b1
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-08-06 10:46:16 +0200

    M45 gardening: adapt to https://codereview.chromium.org/1160403002

--- runtime/browser/xwalk_pref_store.h
+++ runtime/browser/xwalk_pref_store.h
@@ -34,10 +34,10 @@ class XWalkPrefStore : public PersistentPrefStore {
   bool GetMutableValue(const std::string& key, base::Value** result) override;
   void ReportValueChanged(const std::string& key, uint32 flags) override;
   void SetValue(const std::string& key,
-                base::Value* value,
+                scoped_ptr<base::Value> value,
                 uint32 flags) override;
   void SetValueSilently(const std::string& key,
-                        base::Value* value,
+                        scoped_ptr<base::Value> value,
                         uint32 flags) override;
   void RemoveValue(const std::string& key, uint32 flags) override;
   bool ReadOnly() const override;
