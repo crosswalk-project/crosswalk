e8dfc836aa286cda0be5a079b0b86f40b397624d
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-08-17 18:00:03 +0200

    M45 gardening: adapt to https://codereview.chromium.org/1194383003

--- runtime/browser/ui/splash_screen_tizen.cc
+++ runtime/browser/ui/splash_screen_tizen.cc
@@ -112,7 +112,8 @@ void SplashScreenTizen::DidFailLoad(
     content::RenderFrameHost* render_frame_host,
     const GURL& validated_url,
     int error_code,
-    const base::string16& error_description) {
+    const base::string16& error_description,
+    bool was_ignored_by_handler) {
   Stop();
 }
 
--- runtime/browser/ui/splash_screen_tizen.h
+++ runtime/browser/ui/splash_screen_tizen.h
@@ -42,7 +42,8 @@ class SplashScreenTizen : public content::WebContentsObserver,
   void DidFailLoad(content::RenderFrameHost* render_frame_host,
                    const GURL& validated_url,
                    int error_code,
-                   const base::string16& error_description) override;
+                   const base::string16& error_description,
+                   bool was_ignored_by_handler) override;
 
 
   // ui::ImplicitAnimationObserver overrides:
