8dfaee999692e12caa1199f46b84ea30dc03bcdb
Xiaosong Wei <xiaosong.wei@intel.com>
2015-09-23 13:27:00 +0800

    [Android] Expose onTouchEvent to XWalkView
    Expose onTouchEvent to XWalkView so that developer can override
    XWalkView.onTouchEvent to intercept touch event. Usually super.onTouchEvent
    is called within onTouchEvent override method so we keep a default
    onTouchEvent override in XWalkView to connect to the default touch event
    handler within ContentViewCore.
    
    BUG=XWALK-4879

--- runtime/android/core_internal/src/org/xwalk/core/internal/XWalkViewInternal.java
+++ runtime/android/core_internal/src/org/xwalk/core/internal/XWalkViewInternal.java
@@ -24,6 +24,7 @@ import android.view.SurfaceView;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.view.MotionEvent;
 import android.webkit.ValueCallback;
 import android.widget.FrameLayout;
 
@@ -1258,4 +1259,19 @@ public class XWalkViewInternal extends android.widget.FrameLayout {
     public boolean performLongClickDelegate(){
         return false;
     }
+
+    @XWalkAPI(delegate = true,
+              preWrapperLines = {"return onTouchEvent(event);"})
+    public boolean onTouchEventDelegate(MotionEvent event){
+        return false;
+    }
+
+    // Usually super.onTouchEvent is called within XWalkView.onTouchEvent override
+    // This is used as our default touch event handler.
+    @Override
+    @XWalkAPI
+    public boolean onTouchEvent(MotionEvent event) {
+        return mContent.onTouchEvent(event);
+    }
+
 }
