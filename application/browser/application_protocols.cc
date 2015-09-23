0dd704f790030b30f3608ea0a206a5a66cd1c598
Mrunal Kapade <mrunal.kapade@intel.com>
2015-09-23 15:48:37 -0700

    M46 gardening: adapt to https://codereview.chromium.org/1280473002

--- application/browser/application_protocols.cc
+++ application/browser/application_protocols.cc
@@ -241,7 +241,7 @@ void GetUserAgentLocales(const std::string& sys_locale,
   if (sys_locale.empty())
     return;
 
-  std::string locale = base::StringToLowerASCII(sys_locale);
+  std::string locale = base::ToLowerASCII(sys_locale);
   size_t position;
   do {
     ua_locales.push_back(locale);
