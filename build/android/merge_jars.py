fc91dd763d0224ebff1c2cdec82578b7834e0239
fujunwei <junwei.fu@intel.com>
2015-09-30 11:02:37 +0800

    [Android] Don't depend on android support v13 library in Crosswalk Runtime Library
    Exclude the classes of android support when packaging xwalk core library jar.
    The commit of using android support is:
    "On Android devices BackgroundSync needs to launch the browser the next time the device
    goes online if a one-shot sync is registered." in https://codereview.chromium.org/1140813006/#ps340001.
    
    BUG=XWALK-5092

--- build/android/merge_jars.py
+++ build/android/merge_jars.py
@@ -7,6 +7,7 @@
 
 import optparse
 import os
+import shutil
 import sys
 
 GYP_ANDROID_DIR = os.path.join(os.path.dirname(__file__),
@@ -24,12 +25,21 @@ def main():
   parser = optparse.OptionParser()
   parser.add_option('--jars', help='The jars to merge.')
   parser.add_option('--jar-path', help='The output merged jar file.')
+  parser.add_option('--jar-excluded-classes', help='Classes to exclude from the jar.')
 
   options, _ = parser.parse_args()
 
   with build_utils.TempDir() as temp_dir:
     for jar_file in build_utils.ParseGypList(options.jars):
       build_utils.ExtractAll(jar_file, path=temp_dir, pattern='*.class')
+    if options.jar_excluded_classes:
+      jar_excluded_classes = build_utils.ParseGypList(options.jar_excluded_classes)
+      for excluded_class in jar_excluded_classes:
+        for root, _, files in os.walk(temp_dir):
+          relative_path = os.path.relpath(root, temp_dir)
+          if relative_path == excluded_class:
+            shutil.rmtree(root)
+            continue
     jar.JarDirectory(temp_dir, [], options.jar_path)
 
 
