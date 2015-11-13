crosswalk-project/crosswalk

commit e5d3b6c2bc1251c979ab525a92d42e3341a1d25b

[Windows][Extensions]Fix render process crash when sending data from native to JS using the shared memory handle.
This is the second fix for this bug. The previous fix uncovered this
one.

The crash was because the memory handle we send to the extension process
was null. The fact it was null is because on Windows it was failing to
share the handle of the shared memory between the render process and
the extension process. GiveReadOnlyToProcess was failing inside
windows specific code, more exactly in DuplicateHandle, a win API
which was returning an error code of 6. 6 means the handle we're passing
is invalid. Turns out that while we store the renderer process handle
earlier (so we can share the data between the two), the open() call
on the process is too early. It seems like Windows can't work with this
handle unless the process is open right before. Moving the open() call
to the last moment (by storing the PID instead of the handle) doesn't
fully work (we don't fail with error 6 but with 5 which means there
is not enough privileges to open/share with that process). We therefore
open it with extra privileges which means we can share data between
the two. I also added a LOG and an early return when sharing the handle
doesn't work, after all it means it's functionally broken.

While investigating this issue it turns out that the automatic test was
broken because it was always reporting "Pass" even if the actual message
passing with data handle was broken. The fix consists of moving it from
a regular extension test to a true external extension test. The second
part is also making sure we don't report "Pass" until we received all
the messages back from the native extension. If we don't receive one
the test will time out. I've decided to address it in this patch so that
I don't need to fix the test, then disable it to land that patch and
re-enable it again.

It turns out that the fix made the test to fail on Linux (running the test

extensions/common/xwalk_extension_server.cc
@@ -25,7 +25,7 @@ const size_t kInlineMessageMaxSize = 256 * 1024;
 
 XWalkExtensionServer::XWalkExtensionServer()
     : sender_(NULL),
-      renderer_process_handle_(base::kNullProcessHandle),
+      renderer_process_pid_(0),
       permissions_delegate_(NULL) {}
 
 XWalkExtensionServer::~XWalkExtensionServer() {
@@ -54,9 +54,7 @@ bool XWalkExtensionServer::OnMessageReceived(const IPC::Message& message) {
 }
 
 void XWalkExtensionServer::OnChannelConnected(int32 peer_pid) {
-  base::Process process = base::Process::Open(peer_pid);
-  renderer_process_handle_ = process.Handle();
-  CHECK(process.IsValid());
+  renderer_process_pid_ = peer_pid;
 }
 
 void XWalkExtensionServer::OnCreateInstance(int64_t instance_id,
@@ -221,7 +219,13 @@ void XWalkExtensionServer::PostMessageToJSCallback(
   memcpy(shared_memory.memory(), message->data(), message->size());
 
   base::SharedMemoryHandle handle;
-  shared_memory.GiveReadOnlyToProcess(renderer_process_handle_, &handle);
+  base::Process process =
+      base::Process::OpenWithExtraPrivileges(renderer_process_pid_);
+  CHECK(process.IsValid());
+  if (!shared_memory.GiveReadOnlyToProcess(process.Handle(), &handle)) {
+    LOG(WARNING) << "Can't share memory handle to send out of line message";
+    return;
+  }
 
   Send(new XWalkExtensionClientMsg_PostOutOfLineMessageToJS(handle,
                                                             message->size()));

extensions/common/xwalk_extension_server.h
@@ -117,7 +117,7 @@ class XWalkExtensionServer : public IPC::Listener,
   typedef std::set<std::string> ExtensionSymbolsSet;
   ExtensionSymbolsSet extension_symbols_;
 
-  base::ProcessHandle renderer_process_handle_;
+  int32 renderer_process_pid_;
 
   XWalkExtension::PermissionsDelegate* permissions_delegate_;
 };

extensions/test/data/bulk_data_transmission.html
@@ -47,15 +47,6 @@ var readableSize = function(size) {
   return size + suffix;
 }
 
-var requestDataAsync = function(size) {
-  bulkData.requestBulkDataAsync(size, function(msg) {
-      var message = document.createElement('p');
-      message.innerText = 'Requested '
-        + readableSize(msg.length) + ' data chunk asynchronously';
-      divAsync.appendChild(message);
-    });
-}
-
 var startPow = 1;
 var maxPow = 26; // 26 is 64MB, which is the limit of IPC post message
 var repeatTimes = 1;
@@ -64,10 +55,15 @@ var index = startPow * repeatTimes;
 
 function runTestCase() {
   return new Promise(function(resolve) {
-      var size = Math.pow(2, Math.floor(index / repeatTimes));
-      requestDataAsync(size);
+    var size = Math.pow(2, Math.floor(index / repeatTimes));
+    bulkData.requestBulkDataAsync(size, function (msg) {
+      var message = document.createElement('p');
+      message.innerText = 'Received '
+        + readableSize(msg.length) + ' data chunk asynchronously';
+      divAsync.appendChild(message);
       resolve();
-    }).then(function() {
+    });
+  }).then(function () {
       ++index;
       if (index == (maxPow + 1) * repeatTimes) {
         document.title = "Pass";
@@ -76,7 +72,7 @@ function runTestCase() {
       runTestCase();
     }, function(e) {
       console.error(e);
-      document.title = "Fail";
+      document.title = "Fail" + e;
     });
 };
 runTestCase();

extensions/test/external_extension.cc
@@ -24,6 +24,16 @@ class ExternalExtensionTest : public XWalkExtensionsTestBase {
   }
 };
 
+class BulkExtensionTest : public XWalkExtensionsTestBase {
+ public:
+  void SetUp() override {
+    XWalkExtensionService::SetExternalExtensionsPathForTesting(
+      GetExternalExtensionTestPath(
+        FILE_PATH_LITERAL("bulk_data_transmission")));
+    XWalkExtensionsTestBase::SetUp();
+  }
+};
+
 class RuntimeInterfaceTest : public XWalkExtensionsTestBase {
  public:
   void SetUp() override {
@@ -121,4 +131,14 @@ IN_PROC_BROWSER_TEST_F(MultipleEntryPointsExtension, ReplacementObjectIsUsed) {
   title_watcher.AlsoWaitForTitle(kFailString);
   xwalk_test_utils::NavigateToURL(runtime, url);
   EXPECT_EQ(kPassString, title_watcher.WaitAndGetTitle());
+}
+
+IN_PROC_BROWSER_TEST_F(BulkExtensionTest, BulkDataExtension) {
+Runtime* runtime = CreateRuntime();
+GURL url = GetExtensionsTestURL(base::FilePath(),
+base::FilePath().AppendASCII("bulk_data_transmission.html"));
+content::TitleWatcher title_watcher(runtime->web_contents(), kPassString);
+title_watcher.AlsoWaitForTitle(kFailString);
+xwalk_test_utils::NavigateToURL(runtime, url);
+EXPECT_EQ(kPassString, title_watcher.WaitAndGetTitle());
 }

extensions/test/xwalk_extensions_browsertest.cc
@@ -107,45 +107,6 @@ class ExtensionWithInvalidName : public XWalkExtension {
 
 bool ExtensionWithInvalidName::s_instance_was_created = false;
 
-static const char* kBulkDataAPI = "var bulkDataListener = null;"
-""
-"extension.setMessageListener(function(msg) {"
-"  if (bulkDataListener instanceof Function) {"
-"    bulkDataListener(msg);"
-"  };"
-"});"
-""
-"exports.requestBulkDataAsync = function(power, callback) {"
-"  bulkDataListener = callback;"
-"  extension.postMessage(power.toString());"
-"};";
-
-class BulkDataContext : public XWalkExtensionInstance {
- public:
-  BulkDataContext() {
-  }
-  void HandleMessage(scoped_ptr<base::Value> msg) override {
-    std::string message;
-    msg->GetAsString(&message);
-    int size = atoi(message.c_str());
-    std::string data_chunk(size, 'p');
-    scoped_ptr<base::Value> data(new base::StringValue(data_chunk));
-    PostMessageToJS(data.Pass());
-  }
-};
-
-class BulkDataExtension : public XWalkExtension {
- public:
-  BulkDataExtension() : XWalkExtension() {
-    set_name("bulkData");
-    set_javascript_api(kBulkDataAPI);
-  }
-
-  XWalkExtensionInstance* CreateInstance() override {
-    return new BulkDataContext();
-  }
-};
-
 }  // namespace
 
 class XWalkExtensionsTest : public XWalkExtensionsTestBase {
@@ -154,7 +115,6 @@ class XWalkExtensionsTest : public XWalkExtensionsTestBase {
       XWalkExtensionVector* extensions) override {
     extensions->push_back(new EchoExtension);
     extensions->push_back(new ExtensionWithInvalidName);
-    extensions->push_back(new BulkDataExtension);
   }
 };
 
@@ -205,16 +165,6 @@ IN_PROC_BROWSER_TEST_F(XWalkExtensionsDelayedTest, EchoExtensionSync) {
   GURL url = GetExtensionsTestURL(base::FilePath(),
                                   base::FilePath().AppendASCII(
                                       "sync_echo.html"));
-  content::TitleWatcher title_watcher(runtime->web_contents(), kPassString);
-  title_watcher.AlsoWaitForTitle(kFailString);
-  xwalk_test_utils::NavigateToURL(runtime, url);
-  EXPECT_EQ(kPassString, title_watcher.WaitAndGetTitle());
-}
-
-IN_PROC_BROWSER_TEST_F(XWalkExtensionsTest, BulkDataExtension) {
-  Runtime* runtime = CreateRuntime();
-  GURL url = GetExtensionsTestURL(base::FilePath(),
-      base::FilePath().AppendASCII("bulk_data_transmission.html"));
   content::TitleWatcher title_watcher(runtime->web_contents(), kPassString);
   title_watcher.AlsoWaitForTitle(kFailString);
   xwalk_test_utils::NavigateToURL(runtime, url);

commit 2c25a329fe43480a3db2cfc4309279eda107d072

[Android] Fixed the bulk data transmission crash on Android
This issue is introduced by patch e5d3b6c2, the solution only works
for Windows but crashes on Android.

On Android as we reuse existing proxy channel when creating extension
servers, the XWalkExtensionServer::OnChannelConnected() will not be
triggered in this case. So we keep a pointer of the channel proxy
instead of a sender pointer when initialize extension server, from
which we can get the renderer process id when we need to retrieve
renderer process and its handler in PostMessageToJSCallback().

BUG=XWALK-5029

extensions/common/xwalk_extension_server.cc
@@ -24,8 +24,7 @@ namespace extensions {
 const size_t kInlineMessageMaxSize = 256 * 1024;
 
 XWalkExtensionServer::XWalkExtensionServer()
-    : sender_(NULL),
-      renderer_process_pid_(0),
+    : channel_proxy_(NULL),
       permissions_delegate_(NULL) {}
 
 XWalkExtensionServer::~XWalkExtensionServer() {
@@ -51,10 +50,6 @@ bool XWalkExtensionServer::OnMessageReceived(const IPC::Message& message) {
   IPC_END_MESSAGE_MAP()
 
   return handled;
-}
-
-void XWalkExtensionServer::OnChannelConnected(int32 peer_pid) {
-  renderer_process_pid_ = peer_pid;
 }
 
 void XWalkExtensionServer::OnCreateInstance(int64_t instance_id,
@@ -111,17 +106,17 @@ void XWalkExtensionServer::OnPostMessageToNative(int64_t instance_id,
   data.instance->HandleMessage(value.Pass());
 }
 
-void XWalkExtensionServer::Initialize(IPC::Sender* sender) {
-  base::AutoLock l(sender_lock_);
-  DCHECK(!sender_);
-  sender_ = sender;
+void XWalkExtensionServer::Initialize(IPC::ChannelProxy* channelProxy) {
+  base::AutoLock l(channel_proxy_lock_);
+  DCHECK(!channel_proxy_);
+  channel_proxy_ = channelProxy;
 }
 
 bool XWalkExtensionServer::Send(IPC::Message* msg) {
-  base::AutoLock l(sender_lock_);
-  if (!sender_)
+  base::AutoLock l(channel_proxy_lock_);
+  if (!channel_proxy_)
     return false;
-  return sender_->Send(msg);
+  return channel_proxy_->Send(msg);
 }
 
 namespace {
@@ -220,7 +215,7 @@ void XWalkExtensionServer::PostMessageToJSCallback(
 
   base::SharedMemoryHandle handle;
   base::Process process =
-      base::Process::OpenWithExtraPrivileges(renderer_process_pid_);
+      base::Process::OpenWithExtraPrivileges(channel_proxy_->GetPeerPID());
   CHECK(process.IsValid());
   if (!shared_memory.GiveReadOnlyToProcess(process.Handle(), &handle)) {
     LOG(WARNING) << "Can't share memory handle to send out of line message";
@@ -364,8 +359,8 @@ void XWalkExtensionServer::OnGetExtensions(
 }
 
 void XWalkExtensionServer::Invalidate() {
-  base::AutoLock l(sender_lock_);
-  sender_ = NULL;
+  base::AutoLock l(channel_proxy_lock_);
+  channel_proxy_ = NULL;
 }
 
 namespace {

extensions/common/xwalk_extension_server.h
@@ -52,7 +52,6 @@ class XWalkExtensionServer : public IPC::Listener,
 
   // IPC::Listener Implementation.
   bool OnMessageReceived(const IPC::Message& message) override;
-  void OnChannelConnected(int32 peer_pid) override;
 
   // Different types of ExtensionServers are initialized with different
   // permission delegates: For out-of-process extensions the extension
@@ -60,7 +59,7 @@ class XWalkExtensionServer : public IPC::Listener,
   // IPC; For in-process extensions running in extension thread, we will
   // give a delegate that will do an async method call and for UI thread
   // extensions, doing synchronous request is not allowed.
-  void Initialize(IPC::Sender* sender);
+  void Initialize(IPC::ChannelProxy* channelProxy);
   bool Send(IPC::Message* msg);
 
   bool RegisterExtension(scoped_ptr<XWalkExtension> extension);
@@ -104,8 +103,8 @@ class XWalkExtensionServer : public IPC::Listener,
   bool ValidateExtensionEntryPoints(
       const std::vector<std::string>& entry_points);
 
-  base::Lock sender_lock_;
-  IPC::Sender* sender_;
+  base::Lock channel_proxy_lock_;
+  IPC::ChannelProxy* channel_proxy_;
 
   typedef std::map<std::string, XWalkExtension*> ExtensionMap;
   ExtensionMap extensions_;
@@ -116,8 +115,6 @@ class XWalkExtensionServer : public IPC::Listener,
   // The exported symbols for extensions already registered.
   typedef std::set<std::string> ExtensionSymbolsSet;
   ExtensionSymbolsSet extension_symbols_;
-
-  int32 renderer_process_pid_;
 
   XWalkExtension::PermissionsDelegate* permissions_delegate_;
 };