fe68126b379ac89e8a8e0136fdb2df845751f1eb
Mrunal Kapade <mrunal.kapade@intel.com>
2015-12-11 19:01:01 -0800

    M48 gardening: adapt to https://codereview.chromium.org/1411203010

--- runtime/browser/android/xwalk_content.cc
+++ runtime/browser/android/xwalk_content.cc
@@ -113,7 +113,7 @@ bool ManifestGetString(const xwalk::application::Manifest& manifest,
 XWalkContent* XWalkContent::FromID(int render_process_id,
                                    int render_view_id) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  const content::RenderViewHost* rvh =
+  content::RenderViewHost* rvh =
       content::RenderViewHost::FromID(render_process_id, render_view_id);
   if (!rvh) return NULL;
   content::WebContents* web_contents =
