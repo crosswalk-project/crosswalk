47e8461cd3e60410b502aad70e541159d422791d
hengzhi.wu <hengzhix.wu@intel.com>
2016-01-27 12:44:39 +0800

    [Android] Add two parameters for ClientCertRequest.
    Add principals and KeyTypes for ClientCertRequest, provide two
    APIs to get principals and key types.
    Refine the test case for ClientCertRequest.
    
    BUG=XWALK-6025
    (cherry picked from commit eff1f7f4e3b41143f9807f81bfe56dba831cc07a)

--- runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContentsClientBridge.java
+++ runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContentsClientBridge.java
@@ -727,7 +727,7 @@ class XWalkContentsClientBridge extends XWalkContentsClient
             }
 
             final ClientCertRequestInternal handler = new ClientCertRequestHandlerInternal(
-                    this, id, host, port);
+                    this, id, keyTypes, principals, host, port);
             this.onReceivedClientCertRequest(handler);
         }
     }
