crosswalk-project/crosswalk

commit ec0720dab35cf76520432f460d9d97f387426762

[Windows] Fix application origin
Before the application URL origin (which is used in granting API
permissions) was generated from the installation path. Instead the
URL origin must be unique and it must remain the same for the app
wherever it is installed.
Now the application origin is generated from the upgrade id
GUID that remains the same through all the application versions.

application/common/application_data.cc
@@ -41,17 +41,19 @@ namespace application {
 
 // static
 scoped_refptr<ApplicationData> ApplicationData::Create(
-    const base::FilePath& path, const std::string& explicit_id,
+    const base::FilePath& path, const std::string& id,
     SourceType source_type, scoped_ptr<Manifest> manifest,
     std::string* error_message) {
   DCHECK(error_message);
+  DCHECK(IsValidApplicationID(id));
+
   base::string16 error;
   if (!manifest->ValidateManifest(error_message))
     return NULL;
 
   scoped_refptr<ApplicationData> app_data =
-      new ApplicationData(path, source_type, std::move(manifest));
-  if (!app_data->Init(explicit_id, &error)) {
+      new ApplicationData(path, id, source_type, std::move(manifest));
+  if (!app_data->Init(&error)) {
     *error_message = base::UTF16ToUTF8(error);
     return NULL;
   }
@@ -98,9 +100,13 @@ bool ApplicationData::IsHostedApp() const {
   return source_type_ == EXTERNAL_URL;
 }
 
-ApplicationData::ApplicationData(const base::FilePath& path,
-  SourceType source_type, scoped_ptr<Manifest> manifest)
+ApplicationData::ApplicationData(
+    const base::FilePath& path,
+    const std::string& id,
+    SourceType source_type,
+    scoped_ptr<Manifest> manifest)
     : manifest_version_(0),
+      application_id_(id),
       path_(path),
       manifest_(manifest.release()),
       finished_parsing_manifest_(false),
@@ -146,15 +152,11 @@ GURL ApplicationData::GetResourceURL(const std::string& relative_path) const {
   return GetResourceURL(URL(), relative_path);
 }
 
-bool ApplicationData::Init(const std::string& explicit_id,
-                           base::string16* error) {
+bool ApplicationData::Init(base::string16* error) {
   DCHECK(error);
   ManifestHandlerRegistry* registry =
       ManifestHandlerRegistry::GetInstance(manifest_type());
   if (!registry->ParseAppManifest(this, error))
-    return false;
-
-  if (!LoadID(explicit_id, error))
     return false;
   if (!LoadName(error))
     return false;
@@ -168,23 +170,6 @@ bool ApplicationData::Init(const std::string& explicit_id,
   application_url_ = ApplicationData::GetBaseURLFromApplicationId(ID());
 
   finished_parsing_manifest_ = true;
-  return true;
-}
-
-bool ApplicationData::LoadID(const std::string& explicit_id,
-                             base::string16* error) {
-  std::string application_id;
-  if (!explicit_id.empty()) {
-    application_id_ = explicit_id;
-    return true;
-  }
-
-  application_id = GenerateIdForPath(path_);
-  if (application_id.empty()) {
-    NOTREACHED() << "Could not create ID from path.";
-    return false;
-  }
-  application_id_ = application_id;
   return true;
 }
 

application/browser/application_service.cc
@@ -73,9 +73,16 @@ Application* ApplicationService::LaunchFromManifestPath(
 
   base::FilePath app_path = path.DirName();
   LOG(ERROR) << "Loading app from " << app_path.MaybeAsASCII();
-
+  std::string app_id = GenerateIdForPath(app_path);
+#if defined (OS_WIN)
+  std::string update_id;
+  if (manifest->GetString(application_manifest_keys::kXWalkWindowsUpdateID,
+                          &update_id)) {
+    app_id = GenerateId(update_id);
+  }
+#endif
   scoped_refptr<ApplicationData> application_data = ApplicationData::Create(
-      app_path, std::string(), ApplicationData::LOCAL_DIRECTORY,
+      app_path, app_id, ApplicationData::LOCAL_DIRECTORY,
       std::move(manifest), &error);
   if (!application_data.get()) {
     LOG(ERROR) << "Error occurred while trying to load application: "

application/common/application_data.h
@@ -72,7 +72,7 @@ class ApplicationData : public base::RefCountedThreadSafe<ApplicationData> {
   };
 
   static scoped_refptr<ApplicationData> Create(const base::FilePath& app_path,
-      const std::string& explicit_id, SourceType source_type,
+      const std::string& id, SourceType source_type,
           scoped_ptr<Manifest> manifest, std::string* error_message);
 
   // Returns an absolute url to a resource inside of an application. The
@@ -138,16 +138,13 @@ class ApplicationData : public base::RefCountedThreadSafe<ApplicationData> {
   friend class base::RefCountedThreadSafe<ApplicationData>;
   friend class ApplicationStorageImpl;
 
-  ApplicationData(const base::FilePath& path,
+  ApplicationData(const base::FilePath& path, const std::string& id,
       SourceType source_type, scoped_ptr<Manifest> manifest);
   virtual ~ApplicationData();
 
   // Initialize the application from a parsed manifest.
-  bool Init(const std::string& explicit_id, base::string16* error);
+  bool Init(base::string16* error);
 
-  // Chooses the application ID for an application based on a variety of
-  // criteria. The chosen ID will be set in |manifest|.
-  bool LoadID(const std::string& explicit_id, base::string16* error);
   // The following are helpers for InitFromValue to load various features of the
   // application from the manifest.
   bool LoadName(base::string16* error);

application/common/application_file_util_unittest.cc
@@ -12,13 +12,11 @@
 #include "base/strings/utf_string_conversions.h"
 #include "xwalk/application/common/application_data.h"
 #include "xwalk/application/common/application_manifest_constants.h"
-#include "xwalk/application/common/manifest.h"
 #include "xwalk/application/common/constants.h"
+#include "xwalk/application/common/id_util.h"
+#include "xwalk/application/common/manifest.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
-
-using xwalk::application::ApplicationData;
-using xwalk::application::Manifest;
 
 namespace keys = xwalk::application_manifest_keys;
 
@@ -41,7 +39,8 @@ TEST_F(ApplicationFileUtilTest, LoadApplicationWithValidPath) {
 
   std::string error;
   scoped_refptr<ApplicationData> application(LoadApplication(
-          install_dir, std::string(), ApplicationData::LOCAL_DIRECTORY,
+          install_dir, GenerateIdForPath(install_dir),
+          ApplicationData::LOCAL_DIRECTORY,
           Manifest::TYPE_MANIFEST, &error));
   ASSERT_TRUE(application.get() != NULL);
   EXPECT_EQ("The first application that I made.", application->Description());
@@ -82,7 +81,8 @@ TEST_F(ApplicationFileUtilTest,
 
   std::string error;
   scoped_refptr<ApplicationData> application(LoadApplication(
-          install_dir, std::string(), ApplicationData::LOCAL_DIRECTORY,
+          install_dir, GenerateIdForPath(install_dir),
+          ApplicationData::LOCAL_DIRECTORY,
           Manifest::TYPE_MANIFEST, &error));
   ASSERT_TRUE(application.get() == NULL);
   ASSERT_FALSE(error.empty());
@@ -100,7 +100,8 @@ static scoped_refptr<ApplicationData> LoadApplicationManifest(
   scoped_ptr<Manifest> manifest = make_scoped_ptr(
       new Manifest(make_scoped_ptr(values->DeepCopy())));
   scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      manifest_dir, std::string(), location, std::move(manifest), error);
+      manifest_dir, GenerateIdForPath(manifest_dir), location,
+          std::move(manifest), error);
   return application;
 }
 

application/common/application_manifest_constants.cc
@@ -73,6 +73,9 @@ const char kXWalkLaunchScreenPortrait[] =
     "xwalk_launch_screen.portrait";
 const char kXWalkLaunchScreenReadyWhen[] =
     "xwalk_launch_screen.ready_when";
+
+const char kXWalkWindowsUpdateID[] = "xwalk_windows_update_id";
+
 }  // namespace application_manifest_keys
 
 // manifest keys for widget applications.

application/common/application_manifest_constants.h
@@ -66,6 +66,9 @@ namespace application_manifest_keys {
   extern const char kXWalkLaunchScreenLandscape[];
   extern const char kXWalkLaunchScreenPortrait[];
   extern const char kXWalkLaunchScreenReadyWhen[];
+
+  // Windows specific:
+  extern const char kXWalkWindowsUpdateID[];
 }  // namespace application_manifest_keys
 
 namespace application_widget_keys {

application/common/manifest_handler_unittest.cc
@@ -10,6 +10,7 @@
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "xwalk/application/common/application_data.h"
+#include "xwalk/application/common/id_util.h"
 #include "xwalk/application/common/manifest_handler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -219,7 +220,7 @@ TEST_F(ManifestHandlerTest, DependentHandlers) {
   manifest.SetInteger("g", 6);
   std::string error;
   scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
+      base::FilePath(), GenerateId("test"),
       ApplicationData::LOCAL_DIRECTORY,
       make_scoped_ptr(new Manifest(make_scoped_ptr(manifest.DeepCopy()))),
       &error);
@@ -246,7 +247,7 @@ TEST_F(ManifestHandlerTest, FailingHandlers) {
   // Succeeds when "a" is not recognized.
   std::string error;
   scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
+      base::FilePath(), GenerateId("test"),
       ApplicationData::LOCAL_DIRECTORY,
       make_scoped_ptr(new Manifest(make_scoped_ptr(manifest_a.DeepCopy()))),
       &error);
@@ -262,7 +263,7 @@ TEST_F(ManifestHandlerTest, FailingHandlers) {
   registry.reset(new ScopedTestingManifestHandlerRegistry(handlers));
 
   application = ApplicationData::Create(
-      base::FilePath(), std::string(),
+      base::FilePath(), GenerateId("test"),
       ApplicationData::LOCAL_DIRECTORY,
       make_scoped_ptr(new Manifest(make_scoped_ptr(manifest_a.DeepCopy()))),
       &error);
@@ -282,7 +283,7 @@ TEST_F(ManifestHandlerTest, Validate) {
   manifest.SetInteger("b", 2);
   std::string error;
   scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
+      base::FilePath(), GenerateId("test"),
       ApplicationData::LOCAL_DIRECTORY,
       make_scoped_ptr(new Manifest(make_scoped_ptr(manifest.DeepCopy()))),
       &error);

application/common/manifest_handlers/permissions_handler_unittest.cc
@@ -5,6 +5,7 @@
 #include "xwalk/application/common/manifest_handlers/permissions_handler.h"
 
 #include "xwalk/application/common/application_manifest_constants.h"
+#include "xwalk/application/common/manifest_handlers/unittest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace xwalk {
@@ -32,12 +33,8 @@ TEST_F(PermissionsHandlerTest, NonePermission) {
   base::DictionaryValue manifest;
   manifest.SetString(keys::kNameKey, "no name");
   manifest.SetString(keys::kXWalkVersionKey, "0");
-  std::string error;
-  scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
-      ApplicationData::LOCAL_DIRECTORY,
-      make_scoped_ptr(new Manifest(make_scoped_ptr(manifest.DeepCopy()))),
-      &error);
+  scoped_refptr<ApplicationData> application =
+      CreateApplication(Manifest::TYPE_MANIFEST, manifest);
   EXPECT_TRUE(application.get());
   EXPECT_EQ(GetAPIPermissionsInfo(application).size(), 0);
 }
@@ -48,12 +45,8 @@ TEST_F(PermissionsHandlerTest, EmptyPermission) {
   manifest.SetString(keys::kXWalkVersionKey, "0");
   base::ListValue* permissions = new base::ListValue;
   manifest.Set(keys::kPermissionsKey, permissions);
-  std::string error;
-  scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
-      ApplicationData::LOCAL_DIRECTORY,
-      make_scoped_ptr(new Manifest(make_scoped_ptr(manifest.DeepCopy()))),
-      &error);
+  scoped_refptr<ApplicationData> application =
+      CreateApplication(Manifest::TYPE_MANIFEST, manifest);
   EXPECT_TRUE(application.get());
   EXPECT_EQ(GetAPIPermissionsInfo(application).size(), 0);
 }
@@ -65,12 +58,8 @@ TEST_F(PermissionsHandlerTest, DeviceAPIPermission) {
   base::ListValue* permissions = new base::ListValue;
   permissions->AppendString("geolocation");
   manifest.Set(keys::kPermissionsKey, permissions);
-  std::string error;
-  scoped_refptr<ApplicationData> application = ApplicationData::Create(
-      base::FilePath(), std::string(),
-      ApplicationData::LOCAL_DIRECTORY,
-      make_scoped_ptr(new Manifest(make_scoped_ptr(manifest.DeepCopy()))),
-      &error);
+  scoped_refptr<ApplicationData> application =
+      CreateApplication(Manifest::TYPE_MANIFEST, manifest);
   EXPECT_TRUE(application.get());
   const PermissionSet& permission_list =
       GetAPIPermissionsInfo(application);

commit de1acdc1df90b8f43018ec5f4b47110c1a9fd273

Fix a -Wreorder error after ec0720da.
Commit ec0720da ("[Windows] Fix application origin") introduced an
ordering issue when initializing `ApplicationData`'s members which
causes build failures with stricted compiler flags (as it happens by
default in M49):

../../xwalk/application/common/application_data.cc:109:7: error: field 'application_id_' will be initialized after field 'path_' [-Werror,-Wreorder]
      application_id_(id),

application/common/application_data.cc
@@ -106,8 +106,8 @@ ApplicationData::ApplicationData(
     SourceType source_type,
     scoped_ptr<Manifest> manifest)
     : manifest_version_(0),
-      application_id_(id),
       path_(path),
+      application_id_(id),
       manifest_(manifest.release()),
       finished_parsing_manifest_(false),
       source_type_(source_type) {