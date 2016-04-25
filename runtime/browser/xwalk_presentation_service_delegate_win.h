4c7b5b5e66cb170c19063f4bd7de66602ed3a0ff
Kenny Yuan <kaining.yuan@intel.com>
2016-04-25 14:13:21 +0800

    Refactor to remove duplicated code after Presentation API is re-implemented on Android
    Prior to XWALK-4646, there was the impl. of W3C presentation API for Windows platform. It was Windows-only impl at that time.
    
    After some experiment, there was a common subset identified in source code between Android & Windows platform impl., but considering the fact that XWALK-4646 was in high priority (P1 blocker) at that time, and I was unable to obtain a Windows box to test the modified behavior on Windows, I made a decision to copy the common code and modify part of it to Android-specific source code.
    
    After XWALK-4646 is resolved, this XWALK-6262 was opened to track the leftover task, which is to share the common code between Windows & Android platform. It was delayed due to rebase and other resource issues. Now it's finally done.
    
    BUG=XWALK-6262

--- runtime/browser/xwalk_presentation_service_delegate_win.h
+++ runtime/browser/xwalk_presentation_service_delegate_win.h
@@ -6,31 +6,15 @@
 #define XWALK_RUNTIME_BROWSER_XWALK_PRESENTATION_SERVICE_DELEGATE_WIN_H_
 
 #include <string>
-#include <utility>
-
-#include "base/containers/scoped_ptr_hash_map.h"
-#include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
-#include "content/public/browser/presentation_service_delegate.h"
-#include "content/public/browser/web_contents_user_data.h"
-
-namespace content {
-class WebContents;
-}  // namespace content
+#include "xwalk/runtime/browser/xwalk_presentation_service_delegate.h"
 
 namespace xwalk {
 
-class PresentationFrame;
-class PresentationSession;
-class XWalkBrowserContext;
-
 class XWalkPresentationServiceDelegateWin
-    : public content::WebContentsUserData<XWalkPresentationServiceDelegateWin>,
-      public content::PresentationServiceDelegate,
+    : public XWalkPresentationServiceDelegate,
+      public content::WebContentsUserData<XWalkPresentationServiceDelegateWin>,
       public base::SupportsWeakPtr<XWalkPresentationServiceDelegateWin> {
  public:
-  using RenderFrameHostId = std::pair<int, int>;
-
   static content::PresentationServiceDelegate* GetOrCreateForWebContents(
       content::WebContents* web_contents);
 
@@ -39,93 +23,17 @@ class XWalkPresentationServiceDelegateWin
  private:
   explicit XWalkPresentationServiceDelegateWin(
       content::WebContents* web_contents);
-  friend class
-      content::WebContentsUserData<XWalkPresentationServiceDelegateWin>;
 
-  void AddObserver(int render_process_id,
-      int render_frame_id,
-      Observer* observer) override;
+  friend class content::WebContentsUserData<
+      XWalkPresentationServiceDelegateWin>;
 
-  void RemoveObserver(int render_process_id, int render_frame_id) override;
-
-  bool AddScreenAvailabilityListener(
-      int render_process_id,
-      int render_frame_id,
-      content::PresentationScreenAvailabilityListener* listener) override;
-
-  void RemoveScreenAvailabilityListener(
-      int render_process_id,
-      int render_frame_id,
-      content::PresentationScreenAvailabilityListener* listener) override;
-
-  void Reset(
-      int render_process_id,
-      int render_frame_id) override;
-
-  void SetDefaultPresentationUrl(
-      int render_process_id,
-      int render_frame_id,
-      const std::string& default_presentation_url,
-      const content::PresentationSessionStartedCallback& callback) override;
-
+ public:
   void StartSession(
       int render_process_id,
       int render_frame_id,
       const std::string& presentation_url,
       const content::PresentationSessionStartedCallback& success_cb,
       const content::PresentationSessionErrorCallback& error_cb) override;
-
-  void JoinSession(
-      int render_process_id,
-      int render_frame_id,
-      const std::string& presentation_url,
-      const std::string& presentation_id,
-      const content::PresentationSessionStartedCallback& success_cb,
-      const content::PresentationSessionErrorCallback& error_cb) override;
-
-  void CloseConnection(
-      int render_process_id,
-      int render_frame_id,
-      const std::string& presentation_id) override;
-
-  void Terminate(int render_process_id,
-                 int render_frame_id,
-                 const std::string& presentation_id) override;
-
-  void ListenForSessionMessages(
-      int render_process_id,
-      int render_frame_id,
-      const content::PresentationSessionInfo& session,
-      const content::PresentationSessionMessageCallback& message_cb)
-      override {}
-
-  void SendMessage(
-      int render_process_id,
-      int render_frame_id,
-      const content::PresentationSessionInfo& session,
-      scoped_ptr<content::PresentationSessionMessage> message_request,
-      const SendMessageCallback& send_message_cb) override {}
-
-  void ListenForConnectionStateChange(
-      int render_process_id,
-      int render_frame_id,
-      const content::PresentationSessionInfo& connection,
-      const content::PresentationConnectionStateChangedCallback&
-          state_changed_cb) override;
-
- private:
-  PresentationFrame* GetOrAddPresentationFrame(
-      const RenderFrameHostId& render_frame_host_id);
-  void OnSessionStarted(
-      const RenderFrameHostId& id,
-      const content::PresentationSessionStartedCallback& success_cb,
-      const content::PresentationSessionErrorCallback& error_cb,
-      scoped_refptr<PresentationSession> session,
-      const std::string& error);
-
-  content::WebContents* web_contents_;
-  base::ScopedPtrHashMap<RenderFrameHostId, scoped_ptr<PresentationFrame>>
-      presentation_frames_;
 };
 
 }  // namespace xwalk
