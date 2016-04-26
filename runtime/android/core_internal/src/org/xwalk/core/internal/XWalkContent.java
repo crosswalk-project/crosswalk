4f3dd7ed9cb2443a222bc34ad501683013d26e8f
fujunwei <junwei.fu@intel.com>
2016-04-26 14:26:29 +0800

    [Android] Give host application a chance to take over MediaPlayer
    Give the host application a chance to take over the control when a new
    media is about to be played in the current XWalkViewInternal
    
    BUG=XWALK-6770

--- runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
+++ runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
@@ -133,7 +133,7 @@ class XWalkContent implements XWalkPreferencesInternal.KeyValueChangeListener {
         mGeolocationPermissions = new XWalkGeolocationPermissions(sharedPreferences);
 
         MediaPlayerBridge.setResourceLoadingFilter(
-                new XWalkMediaPlayerResourceLoadingFilter());
+                new XWalkMediaPlayerResourceLoadingFilter(mContentsClientBridge));
 
         setNativeContent(nativeInit());
 
