crosswalk-project/crosswalk

commit a2abce58dc14eeb8af049cc191446a6b134764ee

M50: Use NotificationResource instead of SkBitmap(Android)

runtime/browser/android/xwalk_contents_client_bridge.cc
@@ -21,6 +21,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/file_chooser_file_info.h"
 #include "content/public/common/file_chooser_params.h"
+#include "content/public/common/notification_resources.h"
 #include "content/public/common/platform_notification_data.h"
 #include "jni/XWalkContentsClientBridge_jni.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -243,6 +244,7 @@ static void CancelNotification(
 
 void XWalkContentsClientBridge::ShowNotification(
     const content::PlatformNotificationData& notification_data,
+    const content::NotificationResources& notification_resources,
     scoped_ptr<content::DesktopNotificationDelegate> delegate,
     base::Closure* cancel_callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -259,8 +261,7 @@ void XWalkContentsClientBridge::ShowNotification(
   ScopedJavaLocalRef<jstring> jreplace_id(
     ConvertUTF8ToJavaString(env, notification_data.tag));
   ScopedJavaLocalRef<jobject> jicon;
-  if (!icon.empty())
-     jicon = gfx::ConvertToJavaBitmap(&icon);
+  jicon = gfx::ConvertToJavaBitmap(&notification_resources.notification_icon);
 
   int notification_id = g_next_notification_id_++;
   g_notification_map_.set(notification_id, std::move(delegate));

runtime/browser/android/xwalk_contents_client_bridge.h
@@ -70,7 +70,7 @@ class XWalkContentsClientBridge : public XWalkContentsClientBridgeBase ,
       override;
   void ShowNotification(
       const content::PlatformNotificationData& notification_data,
-      const SkBitmap& icon,
+      const content::NotificationResources& notification_resources,
       scoped_ptr<content::DesktopNotificationDelegate> delegate,
       base::Closure* cancel_callback)
       override;

runtime/browser/android/xwalk_contents_client_bridge_base.h
@@ -16,6 +16,7 @@ class SkBitmap;
 
 namespace content {
 class DesktopNotificationDelegate;
+struct NotificationResources;
 struct PlatformNotificationData;
 class RenderFrameHost;
 class WebContents;
@@ -73,7 +74,7 @@ class XWalkContentsClientBridgeBase {
       = 0;
   virtual void ShowNotification(
       const content::PlatformNotificationData& notification_data,
-      const SkBitmap& icon,
+      const content::NotificationResources& notification_resources,
       scoped_ptr<content::DesktopNotificationDelegate> delegate,
       base::Closure* cancel_callback)
       = 0;

runtime/browser/xwalk_platform_notification_service.cc
@@ -106,8 +106,8 @@ void XWalkPlatformNotificationService::DisplayNotification(
       continue;
     XWalkContentsClientBridgeBase* bridge =
         XWalkContentsClientBridgeBase::FromWebContents(web_contents);
-    bridge->ShowNotification(notification_data, icon, std::move(delegate),
-                             cancel_callback);
+    bridge->ShowNotification(notification_data, notification_resources, 
+                             std::move(delegate), cancel_callback);
     return;
   }
 #elif defined(OS_LINUX) && defined(USE_LIBNOTIFY)

commit 10b897c8d7fdecf1a84e69e42805d40285ecaa75

M50: Fix the crash with empty notification resources.
It was caused by
https://github.com/crosswalk-project/crosswalk/commit/a2abce58dc14eeb8af049cc191446a6b134764ee
Notification icon maybe empty if developer did not provide an icon,
or fetching of the icon failed. We need to check the color type to
avoid the crash in java_bitmap.cc.

BUG=XWALK-6857

runtime/browser/android/xwalk_contents_client_bridge.cc
@@ -25,6 +25,7 @@
 #include "content/public/common/platform_notification_data.h"
 #include "jni/XWalkContentsClientBridge_jni.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
 #include "url/gurl.h"
 #include "ui/gfx/android/java_bitmap.h"
 #include "ui/shell_dialogs/selected_file_info.h"
@@ -261,7 +262,11 @@ void XWalkContentsClientBridge::ShowNotification(
   ScopedJavaLocalRef<jstring> jreplace_id(
     ConvertUTF8ToJavaString(env, notification_data.tag));
   ScopedJavaLocalRef<jobject> jicon;
-  jicon = gfx::ConvertToJavaBitmap(&notification_resources.notification_icon);
+  if (notification_resources.notification_icon.colorType() !=
+      SkColorType::kUnknown_SkColorType) {
+    jicon = gfx::ConvertToJavaBitmap(
+        &notification_resources.notification_icon);
+  }
 
   int notification_id = g_next_notification_id_++;
   g_notification_map_.set(notification_id, std::move(delegate));