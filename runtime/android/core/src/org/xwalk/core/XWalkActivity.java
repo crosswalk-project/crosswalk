crosswalk-project/crosswalk

commit 5505ef4d02fea2453426d10e80a479ed35acb10b

[Android] Refactor download mode
- Fix a crash issue of download mode
- Move the process of library extraction from XWalkUpdater to XWalkDecompressor.
- Change the mechanism of updating downloaded runtime
- Use new name of meta-data for download mode

BUG=XWALK-6883

runtime/android/core/src/org/xwalk/core/XWalkActivity.java
@@ -8,81 +8,111 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * <p><code>XWalkActivity</code> helps to execute all procedures to make the Crosswalk runtime
- * workable and displays dialogs to interact with the user if needed. The activities that hold
- * the {@link XWalkView} objects might want to extend <code>XWalkActivity</code> to obtain this
+ * <p><code>XWalkActivity</code> helps to execute all procedures to make Crosswalk Project runtime
+ * workable and displays dialogs to interact with the user. The activities that hold the
+ * {@link XWalkView} objects might want to extend <code>XWalkActivity</code> to obtain this
  * capability. For those activities, there's no need to use {@link XWalkInitializer} and
- * {@link XWalkUpdater}.
+ * {@link XWalkUpdater}.</p>
  *
- * <p><code>XWalkActivity</code> will try to run in embedded mode firstly. If the Crosswalk runtime
- * hasn't been embedded in the app, or the embedded Crosswalk runtime doesn't match the
- * CPU architecture of the device, it will switch to shared mode, i.e., link to the separately
- * installed Crosswalk runtime package which is shared by multiple apps. If the Crosswalk runtime
- * hasn't been installed on the device, or the installed Crosswalk runtime isn't compatible with
- * the app, it will pop up dialogs to prompt the user to download suitable one. Once the user has
- * agreed to download, it will navigate to the Crosswalk runtime's page on the default application
- * store, subsequent process will be up to the user. If the developer specified the download URL of
- * the Crosswalk runtime, it will launch the download manager to fetch the package. To specify the
- * download URL, insert a meta-data element with the name "xwalk_apk_url" inside the application
- * tag in the Android manifest.
+ * <p>By <code>XWalkActivity</code>, your application can support all of embedded mode, shared mode
+ * and download mode with same code. So this is the preferred interface. </p>
+ *
+ * <h3>Sample Code</h3>
+ *
+ * <p>Here is the sample code for all running modes:</p>
  *
  * <pre>
- * &lt;application&gt;
- *     &lt;meta-data android:name="xwalk_apk_url" android:value="http://host/XWalkRuntimeLib.apk" /&gt;
- * &lt;/&gt;
- * </pre>
+ * import android.os.Bundle;
+ * import org.xwalk.core.XWalkActivity;
+ * import org.xwalk.core.XWalkView;
  *
- * <p>In embedded mode, the developer can use the embedding API in <code>onCreate()</code> directly.
- * But in shared mode and lite mode, the Crosswalk runtime isn't loaded yet at the moment the
- * activity is created, so the embedding API won't be usable immediately. To make your code
- * compatible with all modes, it's recommended that all routines using the embedding API should be
- * inside {@link #onXWalkReady} or after {@link #onXWalkReady} is invoked. Please refer to following
- * example for more details.</p>
- *
- * <p>For example:</p>
- *
- * <pre>
- * public class MyActivity extends XWalkActivity {
+ * public class MainActivity extends XWalkActivity {
  *     XWalkView mXWalkView;
  *
  *     &#64;Override
  *     protected void onCreate(Bundle savedInstanceState) {
  *         super.onCreate(savedInstanceState);
  *
- *         // Until onXWalkReady() is invoked, you should do nothing with the embedding API
- *         // except the following:
- *         // 1. Instanciate the XWalkView object
+ *         // Until onXWalkReady() is invoked, you should do nothing with the
+ *         // embedding API except the following:
+ *         // 1. Instantiate the XWalkView object
  *         // 2. Call XWalkPreferences.setValue()
- *         // 3. Call XWalkView.setUIClient()
- *         // 4. Call XWalkView.setResourceClient()
- *
- *         XWalkPreferences.setValue(XWalkPreferences.ANIMATABLE_XWALK_VIEW, true);
- *
- *         setContentView(R.layout.activity_xwalkview);
+ *         // 3. Call mXWalkView.setXXClient(), e.g., setUIClient
+ *         // 4. Call mXWalkView.setXXListener(), e.g., setDownloadListener
+ *         // 5. Call mXWalkView.addJavascriptInterface()
+ *         setContentView(R.layout.activity_main);
  *         mXWalkView = (XWalkView) findViewById(R.id.xwalkview);
- *         mXWalkView.setUIClient(new MyXWalkUIClient(mXWalkView));
- *         mXWalkView.setResourceClient(new MyXWalkResourceClient(mXWalkView));
  *     }
  *
  *     &#64;Override
  *     public void onXWalkReady() {
  *         // Do anyting with the embedding API
- *         mXWalkView.load("http://crosswalk-project.org/", null);
+ *         mXWalkView.load("https://crosswalk-project.org/", null);
  *     }
  * }
  * </pre>
  *
- * <p>To download the Crosswalk runtime, you need to grant following permissions in the
- * Android manifest:</p>
+ * <h3>App Manifest</h3>
+ *
+ * <p>For shared mode and download mode, you might need to edit the Android manifest to set some
+ * properties. </p>
+ *
+ * <h5>Shared Mode</h5>
+ *
+ * <p>If you want the end-user to download Crosswalk Project runtime from specified URL instead of
+ * switching to the application store, add following &lt;meta-data&gt; element inside the
+ * &lt;application&gt; element:</p>
  *
  * <pre>
- * &lt;uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" /&gt;
- * &lt;uses-permission android:name="android.permission.CHANGE_NETWORK_STATE" /&gt;
- * &lt;uses-permission android:name="android.permission.ACCESS_WIFI_STATE" /&gt;
- * &lt;uses-permission android:name="android.permission.CHANGE_WIFI_STATE" /&gt;
- * &lt;uses-permission android:name="android.permission.INTERNET" /&gt;
+ * &lt;application&gt;
+ *     &lt;meta-data android:name="xwalk_apk_url" android:value="http://host/XWalkRuntimeLib.apk" /&gt;
+ * </pre>
+ *
+ * <p>Please note that when the HTTP request is sent to server, the URL will be appended with
+ * "?arch=CPU_API" to indicate that on which CPU architecture it's currently running. The CPU_API
+ * is the same as the value returned from "adb shell getprop ro.product.cpu_abi", e.g. x86 for
+ * IA 32bit, x86_64 for IA 64bit, armeabi-v7a for ARM 32bit and arm64-v8a for ARM 64bit.
+ *
+ * <p>The specified APK will be downloaded to SD card, so you have to grant following permission: </p>
+ *
+ * <pre>
  * &lt;uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /&gt;
  * </pre>
+ *
+ * <h5>Download Mode</h5>
+ *
+ * <p>Firstly, you need to add following &lt;meta-data&gt; element to enable download mode:</p>
+ *
+ * <pre>
+ * &lt;application&gt;
+ *     &lt;meta-data android:name="xwalk_download_mode" android:value="enable"/&gt;
+ * </pre>
+ *
+ * <p>In download mode, the value of <code>xwalk_apk_url</code> is mandatory. However, the
+ * downloaded Apk will be saved into application's private storage, so the permission of writing to
+ * SD card is not needed anymore.</p>
+ *
+ * <p>By default, the application will verify the signature of downloaded Crosswalk Project runtime,
+ * which is required to be the same as your application. But you can disable it by adding following
+ * &lt;meta-data&gt; element:
+ *
+ * <pre>
+ * &lt;application&gt;
+ *     &lt;meta-data android:name="xwalk_verify" android:value="disable"/&gt;
+ * </pre>
+ *
+ * <p> If your application has already downloaded Crosswalk Project runtime but the application got
+ * an update after that, the build version of shared library you used to bundle with your
+ * new application may be newer than the build version of downloaded Crosswalk Project runtime.
+ * In this case, it will download new version of Crosswalk Project runtime from the server again.
+ * If you want to continue using old version of Crosswalk Project runtime, you could add following
+ * &lt;meta-data&gt; element:
+ *
+ * <pre>
+ * &lt;application&gt;
+ *     &lt;meta-data android:name="xwalk_download_mode_update" android:value="disable"/&gt;
+ * </pre>
+ *
  */
 public abstract class XWalkActivity extends Activity {
     private XWalkActivityDelegate mActivityDelegate;

runtime/android/core/src/org/xwalk/core/XWalkActivityDelegate.java
@@ -20,6 +20,7 @@ public class XWalkActivityDelegate
             implements DecompressListener, ActivateListener {
     private static final String TAG = "XWalkActivity";
     private static final String META_XWALK_ENABLE_DOWNLOAD_MODE = "xwalk_enable_download_mode";
+    private static final String META_XWALK_DOWNLOAD_MODE = "xwalk_download_mode";
 
     private Activity mActivity;
     private XWalkDialogManager mDialogManager;
@@ -38,7 +39,13 @@ public class XWalkActivityDelegate
         mActivity = activity;
         mCancelCommand = cancelCommand;
         mCompleteCommand = completeCommand;
-        mIsDownloadMode = isDownloadModeEnabled();
+
+        String enable = getApplicationMetaData(META_XWALK_DOWNLOAD_MODE);
+        if (enable == null) {
+            enable = getApplicationMetaData(META_XWALK_ENABLE_DOWNLOAD_MODE);
+        }
+        mIsDownloadMode = enable != null
+                && (enable.equalsIgnoreCase("enable") || enable.equalsIgnoreCase("true"));
 
         if (mIsDownloadMode) {
             mXWalkUpdater = new XWalkUpdater(
@@ -179,14 +186,14 @@ public class XWalkActivityDelegate
         mCompleteCommand.run();
     }
 
-    private boolean isDownloadModeEnabled() {
+    private String getApplicationMetaData(String name) {
         try {
-            ApplicationInfo appInfo = mActivity.getPackageManager().getApplicationInfo(
+            PackageManager packageManager = mActivity.getPackageManager();
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(
                     mActivity.getPackageName(), PackageManager.GET_META_DATA);
-            String enableStr = appInfo.metaData.getString(META_XWALK_ENABLE_DOWNLOAD_MODE);
-            return enableStr.equalsIgnoreCase("enable");
+            return appInfo.metaData.getString(name);
         } catch (NameNotFoundException | NullPointerException e) {
         }
-        return false;
+        return null;
     }
 }

runtime/android/core/src/org/xwalk/core/XWalkCoreWrapper.java
@@ -38,12 +38,11 @@ class XWalkCoreWrapper {
     private static final String WRAPPER_PACKAGE = "org.xwalk.core";
     private static final String BRIDGE_PACKAGE = "org.xwalk.core.internal";
     private static final String TAG = "XWalkLib";
-    private static final String XWALK_CORE_EXTRACTED_DIR = "extracted_xwalkcore";
     private static final String XWALK_CORE_CLASSES_DEX = "classes.dex";
     private static final String OPTIMIZED_DEX_DIR = "dex";
     private static final String META_XWALK_ENABLE_DOWNLOAD_MODE = "xwalk_enable_download_mode";
-    private static final String XWALK_PREF_FILE = "xwalk_pref";
-    private static final String XWALK_PREF_BUILD_VERSION = "xwalk_build_version";
+    private static final String META_XWALK_DOWNLOAD_MODE = "xwalk_download_mode";
+    private static final String META_XWALK_DOWNLOAD_MODE_UPDATE = "xwalk_download_mode_update";
     private static final String PATTERN_BIT_MISMATCH =
             "dlopen failed: \".+\" is (32|64)-bit instead of (32|64)-bit";
 
@@ -79,6 +78,7 @@ class XWalkCoreWrapper {
     private int mApiVersion;
     private int mMinApiVersion;
     private int mCoreStatus;
+    private boolean mIsDownloadMode;
 
     private Context mWrapperContext;
     private Context mBridgeContext;
@@ -186,12 +186,7 @@ class XWalkCoreWrapper {
             return sProvisionalInstance.mCoreStatus;
         }
 
-        if (sProvisionalInstance.isDownloadMode()) {
-            if (XWalkUpdater.getAutoUpdate() &&
-                    !sProvisionalInstance.checkXWalkRuntimeBuildVersion()) {
-                return sProvisionalInstance.mCoreStatus;
-            }
-
+        if (sProvisionalInstance.mIsDownloadMode) {
             sProvisionalInstance.findDownloadedCore();
             return sProvisionalInstance.mCoreStatus;
         }
@@ -211,9 +206,7 @@ class XWalkCoreWrapper {
             sProvisionalInstance.findSharedCore(XWalkLibraryInterface.XWALK_CORE_IA_PACKAGE);
         } else if (cpuAbi.equalsIgnoreCase("x86_64")) {
             if (!sProvisionalInstance.findSharedCore(XWalkLibraryInterface.XWALK_CORE64_PACKAGE)) {
-                if (!sProvisionalInstance.findSharedCore(XWalkLibraryInterface.XWALK_CORE_IA_PACKAGE)) {
-                    sProvisionalInstance.findSharedCore(XWalkLibraryInterface.XWALK_CORE64_IA_PACKAGE);
-                }
+                sProvisionalInstance.findSharedCore(XWalkLibraryInterface.XWALK_CORE64_IA_PACKAGE);
             }
         }
         return sProvisionalInstance.mCoreStatus;
@@ -255,6 +248,13 @@ class XWalkCoreWrapper {
                 minApiVersion : mApiVersion;
         mCoreStatus = XWalkLibraryInterface.STATUS_PENDING;
         mWrapperContext = context;
+
+        String enable = getApplicationMetaData(META_XWALK_DOWNLOAD_MODE);
+        if (enable == null) {
+            enable = getApplicationMetaData(META_XWALK_ENABLE_DOWNLOAD_MODE);
+        }
+        mIsDownloadMode = enable != null
+                && (enable.equalsIgnoreCase("enable") || enable.equalsIgnoreCase("true"));
     }
 
     private void initCoreBridge() {
@@ -301,8 +301,8 @@ class XWalkCoreWrapper {
     }
 
     private boolean findDownloadedCore() {
-        String libDir = mWrapperContext.getDir(XWALK_CORE_EXTRACTED_DIR, Context.MODE_PRIVATE).
-                getAbsolutePath();
+        String libDir = mWrapperContext.getDir(XWalkLibraryInterface.XWALK_CORE_EXTRACTED_DIR,
+                Context.MODE_PRIVATE).getAbsolutePath();
         String dexPath = libDir + File.separator + XWALK_CORE_CLASSES_DEX;
         String dexOutputPath = mWrapperContext.getDir(OPTIMIZED_DEX_DIR, Context.MODE_PRIVATE).
                 getAbsolutePath();
@@ -314,62 +314,9 @@ class XWalkCoreWrapper {
             return false;
         }
 
-        // store the current XWALK_BUILD_VERSION for auto-update detection.
-        setXWalkRuntimeBuildVersion();
-
         Log.d(TAG, "Running in downloaded mode");
         mCoreStatus = XWalkLibraryInterface.STATUS_MATCH;
         return true;
-    }
-
-    private boolean isDownloadMode() {
-        try {
-            PackageManager packageManager = mWrapperContext.getPackageManager();
-            ApplicationInfo appInfo = packageManager.getApplicationInfo(
-                    mWrapperContext.getPackageName(), PackageManager.GET_META_DATA);
-            String enableStr = appInfo.metaData.getString(META_XWALK_ENABLE_DOWNLOAD_MODE);
-            return enableStr.equalsIgnoreCase("enable");
-        } catch (NameNotFoundException | NullPointerException e) {
-        }
-        return false;
-    }
-
-    private static void storeXWalkRuntimeBuildVersion(Context context, String version) {
-        SharedPreferences sp = context.getSharedPreferences(XWALK_PREF_FILE, Context.MODE_PRIVATE);
-        sp.edit().putString(XWALK_PREF_BUILD_VERSION, version).apply();
-    }
-
-    private void setXWalkRuntimeBuildVersion() {
-        String xwalkBuildVersion = "";
-        try {
-            Class<?> clazz = getBridgeClass("XWalkCoreVersion");
-            xwalkBuildVersion = (String) new ReflectField(clazz, "XWALK_BUILD_VERSION").get();
-        } catch (RuntimeException e) {
-        }
-        storeXWalkRuntimeBuildVersion(mWrapperContext, xwalkBuildVersion);
-    }
-
-    private String getXWalkRuntimeBuildVersion() {
-        SharedPreferences sp = mWrapperContext.getSharedPreferences(XWALK_PREF_FILE,
-                Context.MODE_PRIVATE);
-        return sp.getString(XWALK_PREF_BUILD_VERSION, "");
-    }
-
-    public static void resetXWalkRuntimeBuildVersion(Context context) {
-        storeXWalkRuntimeBuildVersion(context, "");
-    }
-
-    private boolean checkXWalkRuntimeBuildVersion() {
-        String currentVersion = getXWalkRuntimeBuildVersion();
-        Log.d(TAG, "Api build version:" + XWalkAppVersion.XWALK_BUILD_VERSION +
-                ", Runtime build version:" + currentVersion);
-        if (currentVersion.isEmpty() ||
-                currentVersion.equals(XWalkAppVersion.XWALK_BUILD_VERSION)) {
-            return true;
-        }
-        Log.d(TAG, "App doesn't match the downloaded runtime");
-        mCoreStatus = XWalkLibraryInterface.STATUS_RUNTIME_MISMATCH;
-        return false;
     }
 
     private boolean checkCoreVersion() {
@@ -387,6 +334,16 @@ class XWalkCoreWrapper {
             int minLibVersion = (int) new ReflectField(clazz, "MIN_API_VERSION").get();
             Log.d(TAG, "[Lib Version] build:" + buildVersion
                     + ", api:" + libVersion + ", min_api:" + minLibVersion);
+
+            if (mIsDownloadMode && !buildVersion.isEmpty()) {
+                String update = getApplicationMetaData(META_XWALK_DOWNLOAD_MODE_UPDATE);
+                if (update != null
+                        && (update.equalsIgnoreCase("enable") || update.equalsIgnoreCase("true"))
+                        && !buildVersion.equals(XWalkAppVersion.XWALK_BUILD_VERSION)) {
+                    mCoreStatus = XWalkLibraryInterface.STATUS_RUNTIME_MISMATCH;
+                    return false;
+                }
+            }
 
             if (mMinApiVersion > libVersion) {
                 mCoreStatus = XWalkLibraryInterface.STATUS_OLDER_VERSION;
@@ -557,6 +514,17 @@ class XWalkCoreWrapper {
         try {
             return mBridgeLoader.loadClass(BRIDGE_PACKAGE + "." + name);
         } catch (ClassNotFoundException e) {
+        }
+        return null;
+    }
+
+    private String getApplicationMetaData(String name) {
+        try {
+            PackageManager packageManager = mWrapperContext.getPackageManager();
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(
+                    mWrapperContext.getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString(name);
+        } catch (NameNotFoundException | NullPointerException e) {
         }
         return null;
     }

commit f6943eea83a478a167a08053210ac3ffcc41e0c8

[Android] Fix javadoc build with JDK 1.8.
Fix build errors caused by commits 5505ef4 ("[Android] Refactor download
mode") and 1c6ecad ("[Android] Implement the method to set compositing
surface type for single XWalkView"):

```
../xwalk/runtime/android/core/src/org/xwalk/core/XWalkActivity.java:60: error: header used out of sequence: <H5>
 * <h5>Shared Mode</h5>
   ^
../xwalk/runtime/android/core/src/org/xwalk/core/XWalkUpdater.java:172: error: header used out of sequence: <H5>
 * <h5>Shared Mode</h5>
   ^
../out/Release/gen/xwalk_core_reflection_layer/wrapper/org/xwalk/core/XWalkView.java:96: error: text not allowed in <ol> element
 *   </li></ul></li>
                    ^
```

runtime/android/core/src/org/xwalk/core/XWalkActivity.java
@@ -57,7 +57,7 @@ import android.os.Bundle;
  * <p>For shared mode and download mode, you might need to edit the Android manifest to set some
  * properties. </p>
  *
- * <h5>Shared Mode</h5>
+ * <h4>Shared Mode</h4>
  *
  * <p>If you want the end-user to download Crosswalk Project runtime from specified URL instead of
  * switching to the application store, add following &lt;meta-data&gt; element inside the
@@ -79,7 +79,7 @@ import android.os.Bundle;
  * &lt;uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /&gt;
  * </pre>
  *
- * <h5>Download Mode</h5>
+ * <h4>Download Mode</h4>
  *
  * <p>Firstly, you need to add following &lt;meta-data&gt; element to enable download mode:</p>
  *

runtime/android/core/src/org/xwalk/core/XWalkUpdater.java
@@ -169,7 +169,7 @@ import org.xwalk.core.XWalkLibraryLoader.DownloadListener;
  * <p>For shared mode and download mode, you might need to edit the Android manifest to set some
  * properties. </p>
  *
- * <h5>Shared Mode</h5>
+ * <h4>Shared Mode</h4>
  *
  * <p>If you want the end-user to download Crosswalk Project runtime from specified URL instead of
  * switching to the application store, add following &lt;meta-data&gt; element inside the
@@ -191,7 +191,7 @@ import org.xwalk.core.XWalkLibraryLoader.DownloadListener;
  * &lt;uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /&gt;
  * </pre>
  *
- * <h5>Download Mode</h5>
+ * <h4>Download Mode</h4>
  *
  * <p>Firstly, you need to add following &lt;meta-data&gt; element to enable download mode:</p>
  *

runtime/android/core_internal/src/org/xwalk/core/internal/XWalkViewInternal.java
@@ -114,10 +114,10 @@ import org.xwalk.core.internal.extension.BuiltinXWalkExtensions;
  *   </li>
  *   <li>Use XWalkView in MainActivity.java.
  *    <pre>mXWalkView = (XWalkView) findViewById(R.id.xwalkview);</pre>
- *   </li></ul></li>
+ *   </li></ul>
  *   There is debug message on logcat according to your "animatable" values:
  *    "XWalkContent: CompositingSurfaceType is TextureView"
- *   </ol>
+ *   </li></ol>
  *
  * <p>XWalkViewInternal needs hardware acceleration to render web pages. As a result, the
  * AndroidManifest.xml of the caller's app must be appended with the attribute