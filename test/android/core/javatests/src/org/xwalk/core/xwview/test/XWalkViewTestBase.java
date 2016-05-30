crosswalk-project/crosswalk

commit 6761e07c3e7fa54d2766d8d94ffeafa40603b9ae

Move to Chromium 42.0.2311.39.
As usual, this includes a lot of changes to make the code work with a
new Chromium milestone (files got moved, classes were changed etc).

SIMD.js and XDK support are already enabled and working.

Important things to note:
 * ChannelMojo support is being disabled on Android while we investigate
   XWALK-3650.

 * Upstream has introduced a dependency on Google Play Services from the
   Android SDK for the Android build that is a pain to automate, as
   users are expected to accept its EULA first.
   Since Crosswalk does not depend on the Play Services for now, some
   changes were made to chromium-crosswalk to skip this dependency while
   we think of a better way of handling this with upstream.

runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
@@ -42,6 +42,7 @@ import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.NavigationHistory;
 import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.navigation_controller.UserAgentOverrideOption;
 import org.chromium.media.MediaPlayerBridge;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.gfx.DeviceDisplayInfo;
@@ -151,13 +152,12 @@ class XWalkContent extends FrameLayout implements XWalkPreferencesInternal.KeyVa
         // bind all the native->java relationships.
         mCleanupReference = new CleanupReference(this, new DestroyRunnable(mNativeContent));
 
-        mNativeWebContents = nativeGetWebContents(mNativeContent);
-
+        WebContents webContents = nativeGetWebContents(mNativeContent);
 
         // Initialize ContentView.
         mContentViewCore = new ContentViewCore(getContext());
         mContentView = ContentView.newInstance(getContext(), mContentViewCore);
-        mContentViewCore.initialize(mContentView, mContentView, mNativeWebContents, mWindow);
+        mContentViewCore.initialize(mContentView, mContentView, webContents, mWindow);
         mWebContents = mContentViewCore.getWebContents();
         mNavigationController = mWebContents.getNavigationController();
         addView(mContentView, new FrameLayout.LayoutParams(
@@ -177,7 +177,7 @@ class XWalkContent extends FrameLayout implements XWalkPreferencesInternal.KeyVa
         // the members mAllowUniversalAccessFromFileURLs and mAllowFileAccessFromFileURLs
         // won't be changed from false to true at the same time in the constructor of
         // XWalkSettings class.
-        mSettings = new XWalkSettings(getContext(), mNativeWebContents, false);
+        mSettings = new XWalkSettings(getContext(), webContents, false);
         // Enable AllowFileAccessFromFileURLs, so that files under file:// path could be
         // loaded by XMLHttpRequest.
         mSettings.setAllowFileAccessFromFileURLs(true);
@@ -224,7 +224,7 @@ class XWalkContent extends FrameLayout implements XWalkPreferencesInternal.KeyVa
                 params = LoadUrlParams.createLoadDataParamsWithBaseUrl(
                         content, "text/html", false, url, null);
             }
-            params.setOverrideUserAgent(LoadUrlParams.UA_OVERRIDE_TRUE);
+            params.setOverrideUserAgent(UserAgentOverrideOption.TRUE);
             mNavigationController.loadUrl(params);
         }
 
@@ -780,7 +780,7 @@ class XWalkContent extends FrameLayout implements XWalkPreferencesInternal.KeyVa
 
     private native long nativeInit();
     private static native void nativeDestroy(long nativeXWalkContent);
-    private native long nativeGetWebContents(long nativeXWalkContent);
+    private native WebContents nativeGetWebContents(long nativeXWalkContent);
     private native long nativeReleasePopupXWalkContent(long nativeXWalkContent);
     private native void nativeSetJavaPeers(
             long nativeXWalkContent,

runtime/browser/android/xwalk_contents_client_bridge.cc
@@ -290,11 +290,8 @@ void XWalkContentsClientBridge::ExitFullscreen(
     JNIEnv*, jobject, jlong j_web_contents) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   WebContents* web_contents = reinterpret_cast<WebContents*>(j_web_contents);
-  if (web_contents) {
-    RenderViewHost* rvh = web_contents->GetRenderViewHost();
-    if (rvh)
-      rvh->ExitFullscreen();
-  }
+  if (web_contents)
+    web_contents->ExitFullscreen();
 }
 
 void XWalkContentsClientBridge::NotificationDisplayed(

DEPS.xwalk
@@ -17,9 +17,9 @@
 # Edit these when rolling DEPS.xwalk.
 # -----------------------------------
 
-chromium_crosswalk_rev = '6bfb3be277711c369ec45ef77170167a4becadf0'
-v8_crosswalk_rev = '7c7351918cb5a1de3f5c9c0f6bb227b3277a09d9'
-ozone_wayland_rev = 'ebf634371f66955d04234f30e6b52432d16951c6'
+chromium_crosswalk_rev = '4281fabd3a7b4b5b87f5653e51a851045bbd5d55'
+v8_crosswalk_rev = 'e192569693ad7de42287ef9e4a299524a670ce18'
+ozone_wayland_rev = 'eacaa23d129961f94993a30438aa9e4cd7dfefb7'
 
 # |blink_crosswalk_rev| specifies the SHA1 hash of the blink-crosswalk commit
 # we want to point to, very much like the variables above.
@@ -27,8 +27,8 @@ ozone_wayland_rev = 'ebf634371f66955d04234f30e6b52432d16951c6'
 # the blink-crosswalk repository, so that the devtools code can use it to fetch
 # assets from Chromium's servers with a revision that exists there. We need an
 # SVN revision while Blink is still in SVN.
-blink_crosswalk_rev = '275da94cd4eb439bf796751fabe60d4acc2e50a6'
-blink_upstream_rev = '190708'
+blink_crosswalk_rev = '08bab9d8ce72a2e34b0144211895d5760f05dff4'
+blink_upstream_rev = '191638'
 
 crosswalk_git = 'https://github.com/crosswalk-project'
 ozone_wayland_git = 'https://github.com/01org'

VERSION
@@ -1,4 +1,4 @@
 MAJOR=13
-MINOR=41
+MINOR=42
 BUILD=318
 PATCH=0

application/renderer/application_native_module.cc
@@ -5,8 +5,8 @@
 #include "xwalk/application/renderer/application_native_module.h"
 
 #include "base/logging.h"
+#include "content/public/child/v8_value_converter.h"
 #include "content/public/renderer/render_view.h"
-#include "content/public/renderer/v8_value_converter.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebView.h"

extensions/common/xwalk_extension_server.cc
@@ -54,7 +54,9 @@ bool XWalkExtensionServer::OnMessageReceived(const IPC::Message& message) {
 }
 
 void XWalkExtensionServer::OnChannelConnected(int32 peer_pid) {
-  CHECK(base::OpenProcessHandle(peer_pid, &renderer_process_handle_));
+  base::Process process = base::Process::Open(peer_pid);
+  renderer_process_handle_ = process.Handle();
+  CHECK(process.IsValid());
 }
 
 void XWalkExtensionServer::OnCreateInstance(int64_t instance_id,

extensions/renderer/xwalk_extension_module.cc
@@ -8,7 +8,7 @@
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
-#include "content/public/renderer/v8_value_converter.h"
+#include "content/public/child/v8_value_converter.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
 #include "third_party/WebKit/public/web/WebScopedMicrotaskSuppression.h"
 #include "xwalk/extensions/renderer/xwalk_module_system.h"

extensions/renderer/xwalk_extension_renderer_controller.cc
@@ -6,8 +6,8 @@
 
 #include "base/command_line.h"
 #include "base/values.h"
+#include "content/public/child/v8_value_converter.h"
 #include "content/public/renderer/render_thread.h"
-#include "content/public/renderer/v8_value_converter.h"
 #include "grit/xwalk_extensions_resources.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_listener.h"

commit c531e794fae3c72d6cdcf10c5202dac235d45773

[Android] Fix the issue that failed to exit fullscreen.
This issue was introduced by
https://github.com/crosswalk-project/crosswalk/commit/6761e07c3e7fa54d2766d8d94ffeafa40603b9ae
mNativeWebContents was not initialized, so it's value is invalid, can't
get the web contents from it.
Refine test case to catch this issue.

BUG=XWALK-3889

runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContent.java
@@ -90,7 +90,6 @@ class XWalkContent implements XWalkPreferencesInternal.KeyValueChangeListener {
     private ContentBitmapCallback mGetBitmapCallback;
 
     long mNativeContent;
-    long mNativeWebContents;
 
     // TODO(hengzhi.wu): This should be in a global context, not per XWalkView.
     private double mDIPScale;
@@ -194,14 +193,13 @@ class XWalkContent implements XWalkPreferencesInternal.KeyValueChangeListener {
         // bind all the native->java relationships.
         mCleanupReference = new CleanupReference(this, new DestroyRunnable(mNativeContent));
 
-        WebContents webContents = nativeGetWebContents(mNativeContent);
+        mWebContents = nativeGetWebContents(mNativeContent);
 
         // Initialize ContentView.
         mContentViewCore = new ContentViewCore(mViewContext);
         mContentView = XWalkContentView.createContentView(
                 mViewContext, mContentViewCore, mXWalkView);
-        mContentViewCore.initialize(mContentView, mContentView, webContents, mWindow);
-        mWebContents = mContentViewCore.getWebContents();
+        mContentViewCore.initialize(mContentView, mContentView, mWebContents, mWindow);
         mNavigationController = mWebContents.getNavigationController();
         mXWalkView.addView(mContentView, new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT,
@@ -215,7 +213,7 @@ class XWalkContent implements XWalkPreferencesInternal.KeyValueChangeListener {
         // the members mAllowUniversalAccessFromFileURLs and mAllowFileAccessFromFileURLs
         // won't be changed from false to true at the same time in the constructor of
         // XWalkSettings class.
-        mSettings = new XWalkSettingsInternal(mViewContext, webContents, false);
+        mSettings = new XWalkSettingsInternal(mViewContext, mWebContents, false);
         // Enable AllowFileAccessFromFileURLs, so that files under file:// path could be
         // loaded by XMLHttpRequest.
         mSettings.setAllowFileAccessFromFileURLs(true);
@@ -665,7 +663,7 @@ class XWalkContent implements XWalkPreferencesInternal.KeyValueChangeListener {
 
     void exitFullscreen() {
         if (hasEnteredFullscreen()) {
-            mContentsClientBridge.exitFullscreen(mNativeWebContents);
+            mWebContents.exitFullscreen();
         }
     }
 

runtime/android/core_internal/src/org/xwalk/core/internal/XWalkContentsClientBridge.java
@@ -843,11 +843,6 @@ class XWalkContentsClientBridge extends XWalkContentsClient
         nativeCancelJsResult(mNativeContentsClientBridge, id);
     }
 
-    void exitFullscreen(long nativeWebContents) {
-        if (mNativeContentsClientBridge == 0) return;
-        nativeExitFullscreen(mNativeContentsClientBridge, nativeWebContents);
-    }
-
     public void notificationDisplayed(int id) {
         if (mNativeContentsClientBridge == 0) return;
         nativeNotificationDisplayed(mNativeContentsClientBridge, id);
@@ -918,7 +913,6 @@ class XWalkContentsClientBridge extends XWalkContentsClient
     private native void nativeConfirmJsResult(long nativeXWalkContentsClientBridge, int id,
             String prompt);
     private native void nativeCancelJsResult(long nativeXWalkContentsClientBridge, int id);
-    private native void nativeExitFullscreen(long nativeXWalkContentsClientBridge, long nativeWebContents);
     private native void nativeNotificationDisplayed(long nativeXWalkContentsClientBridge, int id);
     private native void nativeNotificationClicked(long nativeXWalkContentsClientBridge, int id);
     private native void nativeNotificationClosed(long nativeXWalkContentsClientBridge, int id, boolean byUser);

runtime/browser/android/xwalk_contents_client_bridge.cc
@@ -329,16 +329,6 @@ void XWalkContentsClientBridge::CancelJsResult(JNIEnv*, jobject, int id) {
   pending_js_dialog_callbacks_.Remove(id);
 }
 
-void XWalkContentsClientBridge::ExitFullscreen(
-    JNIEnv*, jobject, jlong j_web_contents) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  WebContents* web_contents = reinterpret_cast<WebContents*>(j_web_contents);
-  if (web_contents)
-    // TODO(mrunal): Instead of hardcoding the value below we can accept this
-    // as a parameter
-    web_contents->ExitFullscreen(/*will_cause_resize=*/false);
-}
-
 void XWalkContentsClientBridge::NotificationDisplayed(
     JNIEnv*, jobject, jint notification_id) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));

runtime/browser/android/xwalk_contents_client_bridge.h
@@ -90,7 +90,6 @@ class XWalkContentsClientBridge : public XWalkContentsClientBridgeBase ,
   void ProceedSslError(JNIEnv* env, jobject obj, jboolean proceed, jint id);
   void ConfirmJsResult(JNIEnv*, jobject, int id, jstring prompt);
   void CancelJsResult(JNIEnv*, jobject, int id);
-  void ExitFullscreen(JNIEnv*, jobject, jlong web_contents);
   void NotificationDisplayed(JNIEnv*, jobject, jint id);
   void NotificationClicked(JNIEnv*, jobject, jint id);
   void NotificationClosed(JNIEnv*, jobject, jint id, bool by_user);

test/android/core/javatests/src/org/xwalk/core/xwview/test/EnterAndLeaveFullscreenTest.java
@@ -13,6 +13,8 @@ import org.chromium.base.test.util.Feature;
  * Tests for the hasEnteredFullscreen() and leaveFullscreen() method.
  */
 public class EnterAndLeaveFullscreenTest extends XWalkViewTestBase {
+    private TestHelperBridge.OnFullscreenToggledHelper mOnFullscreenToggledHelper =
+            mTestHelperBridge.getOnFullscreenToggledHelper();
 
     @Override
     protected void setUp() throws Exception {
@@ -21,28 +23,40 @@ public class EnterAndLeaveFullscreenTest extends XWalkViewTestBase {
 
     @SmallTest
     @Feature({"XWalkView", "Fullscreen"})
-    public void testEnterAndExitFullscreen() throws Throwable {
+    public void testEnterAndExitFullscreenWithAPI() throws Throwable {
         final String name = "fullscreen_enter_exit.html";
         String fileContent = getFileContent(name);
+        int count = mOnFullscreenToggledHelper.getCallCount();
 
         loadDataSync(name, fileContent, "text/html", false);
+        assertFalse(hasEnteredFullscreen());
 
-        getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    clickOnElementId("enter_fullscreen", null);
-                    assertTrue(getXWalkView().hasEnteredFullscreen());
-                    getXWalkView().leaveFullscreen();
-                    assertFalse(getXWalkView().hasEnteredFullscreen());
+        clickOnElementId("enter_fullscreen", null);
+        mOnFullscreenToggledHelper.waitForCallback(count);
+        assertTrue(hasEnteredFullscreen());
 
-                    clickOnElementId("enter_fullscreen", null);
-                    clickOnElementId("exit_fullscreen", null);
-                    assertFalse(getXWalkView().hasEnteredFullscreen());
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        count = mOnFullscreenToggledHelper.getCallCount();
+        leaveFullscreen();
+        mOnFullscreenToggledHelper.waitForCallback(count);
+        assertFalse(hasEnteredFullscreen());
+    }
+
+    @SmallTest
+    @Feature({"XWalkView", "Fullscreen"})
+    public void testEnterAndExitFullscreenWithJS() throws Throwable {
+        final String name = "fullscreen_enter_exit.html";
+        String fileContent = getFileContent(name);
+        int count = mOnFullscreenToggledHelper.getCallCount();
+
+        loadDataSync(name, fileContent, "text/html", false);
+        assertFalse(hasEnteredFullscreen());
+
+        clickOnElementId("enter_fullscreen", null);
+        mOnFullscreenToggledHelper.waitForCallback(count);
+
+        count = mOnFullscreenToggledHelper.getCallCount();
+        clickOnElementId("exit_fullscreen", null);
+        mOnFullscreenToggledHelper.waitForCallback(count);
+        assertFalse(hasEnteredFullscreen());
     }
 }

test/android/core/javatests/src/org/xwalk/core/xwview/test/XWalkViewTestBase.java
@@ -56,7 +56,7 @@ public class XWalkViewTestBase
     private XWalkView mXWalkView;
     private boolean mAllowSslError = true;
     final TestHelperBridge mTestHelperBridge = new TestHelperBridge();
-    private static final boolean ENABLED = true;		
+    private static final boolean ENABLED = true;
     private static final boolean DISABLED = false;
 
     class TestXWalkUIClientBase extends XWalkUIClient {
@@ -1394,5 +1394,23 @@ public class XWalkViewTestBase
                     + "</head>"
                     + "<body></body></html>";
         }
+    }
+
+    protected boolean hasEnteredFullscreen() throws Exception {
+        return runTestOnUiThreadAndGetResult(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return mXWalkView.hasEnteredFullscreen();
+            }
+        });
+    }
+
+    protected void leaveFullscreen() throws Exception {
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                mXWalkView.leaveFullscreen();
+            }
+        });
     }
 }