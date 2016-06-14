crosswalk-project/crosswalk

commit 20da2b1b22edc9c032919106ea5267533a77ec2b

M51: Use RenderProcessHost instead of BrowserContext
adapt to,
https://codereview.chromium.org/1837873002

runtime/browser/android/net_disk_cache_remover.cc
@@ -7,7 +7,8 @@
 #include "base/bind_helpers.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/web_contents.h"
+#include "content/public/browser/render_process_host.h"
+#include "content/public/browser/storage_partition.h"
 #include "net/disk_cache/disk_cache.h"
 #include "net/http/http_cache.h"
 #include "net/http/http_transaction_factory.h"
@@ -32,24 +33,11 @@ void CallDoomAllEntries(Backend** backend, int rv) {
   (*backend)->DoomAllEntries(base::Bind(&Noop));
 }
 
-void CallDoomEntry(const std::string& key, Backend** backend, int rv) {
-  DCHECK(rv == net::OK);
-  (*backend)->DoomEntry(key, base::Bind(&Noop));
-}
-
-void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter,
-                                 const std::string& key) {
+void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter) {
   typedef Backend* BackendPtr;  // Make line below easier to understand.
   BackendPtr* backend_ptr = new BackendPtr(NULL);
-  CompletionCallback callback;
-  if (!key.empty()) {
-    callback = base::Bind(&CallDoomEntry,
-                          key,
-                          base::Owned(backend_ptr));
-  } else {
-    callback = base::Bind(&CallDoomAllEntries,
-                          base::Owned(backend_ptr));
-  }
+  CompletionCallback callback(base::Bind(&CallDoomAllEntries,
+                                         base::Owned(backend_ptr)));
 
   int rv = context_getter->GetURLRequestContext()->
     http_transaction_factory()->GetCache()->GetBackend(backend_ptr, callback);
@@ -62,31 +50,23 @@ void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter,
 
 void ClearHttpDiskCacheOnIoThread(
     URLRequestContextGetter* main_context_getter,
-    URLRequestContextGetter* media_context_getter,
-    const std::string& key) {
-  ClearHttpDiskCacheOfContext(main_context_getter, key);
-  ClearHttpDiskCacheOfContext(media_context_getter, key);
+    URLRequestContextGetter* media_context_getter) {
+  ClearHttpDiskCacheOfContext(main_context_getter);
+  ClearHttpDiskCacheOfContext(media_context_getter);
 }
 
 }  // namespace
 
 namespace xwalk {
 
-void RemoveHttpDiskCache(content::BrowserContext* browser_context,
-                         int renderer_child_id,
-                         const std::string& key) {
-  URLRequestContextGetter* main_context_getter =
-      browser_context->GetRequestContextForRenderProcess(renderer_child_id);
-  URLRequestContextGetter* media_context_getter =
-      browser_context->GetMediaRequestContextForRenderProcess(
-          renderer_child_id);
-
+void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host) {
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::Bind(&ClearHttpDiskCacheOnIoThread,
-                 base::Unretained(main_context_getter),
-                 base::Unretained(media_context_getter),
-                 key));
+                 base::Unretained(render_process_host->GetStoragePartition()->
+                     GetURLRequestContext()),
+                 base::Unretained(render_process_host->GetStoragePartition()->
+                     GetMediaURLRequestContext())));
 }
 
 }  // namespace xwalk

runtime/browser/android/net_disk_cache_remover.h
@@ -5,11 +5,9 @@
 #ifndef XWALK_RUNTIME_BROWSER_ANDROID_NET_DISK_CACHE_REMOVER_H_
 #define XWALK_RUNTIME_BROWSER_ANDROID_NET_DISK_CACHE_REMOVER_H_
 
-#include <string>
-
 namespace content {
 
-class BrowserContext;
+class RenderProcessHost;
 
 }  // namespace content
 
@@ -17,9 +15,7 @@ namespace xwalk {
 
 // Clear all http disk cache for this renderer. This method is asynchronous and
 // will noop if a previous call has not finished.
-void RemoveHttpDiskCache(content::BrowserContext* browser_context,
-                         int renderer_child_id,
-                         const std::string& key);
+void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host);
 
 }  // namespace xwalk
 

runtime/browser/android/xwalk_content.cc
@@ -237,11 +237,8 @@ void XWalkContent::ClearCache(
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   render_view_host_ext_->ClearCache();
 
-  if (include_disk_files) {
-    RemoveHttpDiskCache(web_contents_->GetBrowserContext(),
-                        web_contents_->GetRoutingID(),
-                        std::string());
-  }
+  if (include_disk_files)
+    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost());
 }
 
 void XWalkContent::ClearCacheForSingleFile(
@@ -251,11 +248,8 @@ void XWalkContent::ClearCacheForSingleFile(
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   std::string key = base::android::ConvertJavaStringToUTF8(env, url);
 
-  if (!key.empty()) {
-    RemoveHttpDiskCache(web_contents_->GetBrowserContext(),
-                        web_contents_->GetRoutingID(),
-                        key);
-  }
+  if (!key.empty())
+    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost());
 }
 
 ScopedJavaLocalRef<jstring> XWalkContent::DevToolsAgentId(JNIEnv* env,

commit a203e7951bee4d3116c7ad4470c3e6f7a71b0b12

M51: Fix failed test case on Android (#3746)
Fix the failed test case of testClearCacheForSingleFile().
This issue was caused by
https://github.com/crosswalk-project/crosswalk/commit/20da2b1b22edc9c032919106ea5267533a77ec2b.
Embedding API of ClearCacheForSingleFile() was implemented in
https://github.com/crosswalk-project/crosswalk/commit/306f2b2d9d19bd4f2f2eb597176da94496a40f1b,
customer could use it to clear cache one by one instead of all.

Fix the failed test case of testSetNetworkAvailableTest().
This issue was caused by 
https://github.com/crosswalk-project/crosswalk/commit/2ad32ffd5384f0cc678d5bed06bb722f0d236ea8.
The variable webkit_initialized_ was not entirely removed.

BUG=XWALK-6998

runtime/browser/android/net_disk_cache_remover.cc
@@ -33,11 +33,24 @@ void CallDoomAllEntries(Backend** backend, int rv) {
   (*backend)->DoomAllEntries(base::Bind(&Noop));
 }
 
-void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter) {
+void CallDoomEntry(Backend** backend, const std::string& key, int rv) {
+  DCHECK(rv == net::OK);
+  (*backend)->DoomEntry(key, base::Bind(&Noop));
+}
+
+void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter,
+                                 const std::string& key) {
   typedef Backend* BackendPtr;  // Make line below easier to understand.
   BackendPtr* backend_ptr = new BackendPtr(NULL);
-  CompletionCallback callback(base::Bind(&CallDoomAllEntries,
-                                         base::Owned(backend_ptr)));
+  CompletionCallback callback;
+  if (!key.empty()) {
+    callback = base::Bind(&CallDoomEntry,
+                          base::Owned(backend_ptr),
+                          key);
+  } else {
+    callback = base::Bind(&CallDoomAllEntries,
+                          base::Owned(backend_ptr));
+  }
 
   int rv = context_getter->GetURLRequestContext()->
     http_transaction_factory()->GetCache()->GetBackend(backend_ptr, callback);
@@ -50,23 +63,26 @@ void ClearHttpDiskCacheOfContext(URLRequestContextGetter* context_getter) {
 
 void ClearHttpDiskCacheOnIoThread(
     URLRequestContextGetter* main_context_getter,
-    URLRequestContextGetter* media_context_getter) {
-  ClearHttpDiskCacheOfContext(main_context_getter);
-  ClearHttpDiskCacheOfContext(media_context_getter);
+    URLRequestContextGetter* media_context_getter,
+    const std::string& key) {
+  ClearHttpDiskCacheOfContext(main_context_getter, key);
+  ClearHttpDiskCacheOfContext(media_context_getter, key);
 }
 
 }  // namespace
 
 namespace xwalk {
 
-void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host) {
+void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host,
+                         const std::string& key) {
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::Bind(&ClearHttpDiskCacheOnIoThread,
                  base::Unretained(render_process_host->GetStoragePartition()->
                      GetURLRequestContext()),
                  base::Unretained(render_process_host->GetStoragePartition()->
-                     GetMediaURLRequestContext())));
+                     GetMediaURLRequestContext()),
+                 key));
 }
 
 }  // namespace xwalk

runtime/browser/android/net_disk_cache_remover.h
@@ -5,6 +5,8 @@
 #ifndef XWALK_RUNTIME_BROWSER_ANDROID_NET_DISK_CACHE_REMOVER_H_
 #define XWALK_RUNTIME_BROWSER_ANDROID_NET_DISK_CACHE_REMOVER_H_
 
+#include <string>
+
 namespace content {
 
 class RenderProcessHost;
@@ -15,7 +17,8 @@ namespace xwalk {
 
 // Clear all http disk cache for this renderer. This method is asynchronous and
 // will noop if a previous call has not finished.
-void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host);
+void RemoveHttpDiskCache(content::RenderProcessHost* render_process_host,
+                         const std::string& key);
 
 }  // namespace xwalk
 

runtime/browser/android/xwalk_content.cc
@@ -238,7 +238,7 @@ void XWalkContent::ClearCache(
   render_view_host_ext_->ClearCache();
 
   if (include_disk_files)
-    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost());
+    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost(), std::string());
 }
 
 void XWalkContent::ClearCacheForSingleFile(
@@ -249,7 +249,7 @@ void XWalkContent::ClearCacheForSingleFile(
   std::string key = base::android::ConvertJavaStringToUTF8(env, url);
 
   if (!key.empty())
-    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost());
+    RemoveHttpDiskCache(web_contents_->GetRenderProcessHost(), key);
 }
 
 ScopedJavaLocalRef<jstring> XWalkContent::DevToolsAgentId(JNIEnv* env,

runtime/renderer/android/xwalk_render_process_observer.cc
@@ -17,8 +17,7 @@
 
 namespace xwalk {
 
-XWalkRenderProcessObserver::XWalkRenderProcessObserver()
-  : webkit_initialized_(false) {
+XWalkRenderProcessObserver::XWalkRenderProcessObserver() {
 }
 
 XWalkRenderProcessObserver::~XWalkRenderProcessObserver() {
@@ -38,13 +37,11 @@ bool XWalkRenderProcessObserver::OnControlMessageReceived(
 }
 
 void XWalkRenderProcessObserver::OnSetJsOnlineProperty(bool network_up) {
-  if (webkit_initialized_)
-    blink::WebNetworkStateNotifier::setOnLine(network_up);
+  blink::WebNetworkStateNotifier::setOnLine(network_up);
 }
 
 void XWalkRenderProcessObserver::OnClearCache() {
-  if (webkit_initialized_)
-    blink::WebCache::clear();
+  blink::WebCache::clear();
 }
 
 void XWalkRenderProcessObserver::OnSetOriginAccessWhitelist(

runtime/renderer/android/xwalk_render_process_observer.h
@@ -28,8 +28,6 @@ class XWalkRenderProcessObserver : public content::RenderProcessObserver {
   void OnClearCache();
   void OnSetOriginAccessWhitelist(std::string base_url,
                                   std::string match_patterns);
-
-  bool webkit_initialized_;
 };
 
 }  // namespace xwalk