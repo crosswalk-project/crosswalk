crosswalk-project/crosswalk

commit 2ad32ffd5384f0cc678d5bed06bb722f0d236ea8

M51: Remove WebKitInitialized()
adapt to,
https://codereview.chromium.org/1821413003

runtime/renderer/android/xwalk_render_process_observer.cc
@@ -37,10 +37,6 @@ bool XWalkRenderProcessObserver::OnControlMessageReceived(
   return handled;
 }
 
-void XWalkRenderProcessObserver::WebKitInitialized() {
-  webkit_initialized_ = true;
-}
-
 void XWalkRenderProcessObserver::OnSetJsOnlineProperty(bool network_up) {
   if (webkit_initialized_)
     blink::WebNetworkStateNotifier::setOnLine(network_up);

runtime/renderer/android/xwalk_render_process_observer.h
@@ -22,7 +22,6 @@ class XWalkRenderProcessObserver : public content::RenderProcessObserver {
 
   // content::RenderProcessObserver implementation.
   bool OnControlMessageReceived(const IPC::Message& message) override;
-  void WebKitInitialized() override;
 
  private:
   void OnSetJsOnlineProperty(bool network_up);

runtime/renderer/xwalk_content_renderer_client.cc
@@ -109,9 +109,6 @@ void XWalkContentRendererClient::RenderThreadStarted() {
   visited_link_slave_.reset(new visitedlink::VisitedLinkSlave);
   thread->AddObserver(visited_link_slave_.get());
 
-  // Using WebString requires blink initialization.
-  thread->EnsureWebKitInitialized();
-
   base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
   if (!cmd_line->HasSwitch(switches::kXWalkDisableExtensions))
     extension_controller_.reset(

runtime/renderer/xwalk_render_process_observer_generic.cc
@@ -70,14 +70,6 @@ bool XWalkRenderProcessObserver::OnControlMessageReceived(
   return handled;
 }
 
-void XWalkRenderProcessObserver::WebKitInitialized() {
-  base::AutoLock lock(lock_);
-  is_blink_initialized_ = true;
-  for (const auto& it : access_whitelist_)
-    AddAccessWhiteListEntry(
-        it->source, it->dest, it->dest_host, it->allow_subdomains);
-}
-
 void XWalkRenderProcessObserver::OnRenderProcessShutdown() {
   is_blink_initialized_ = false;
 }

runtime/renderer/xwalk_render_process_observer_generic.h
@@ -34,7 +34,6 @@ class XWalkRenderProcessObserver : public content::RenderProcessObserver {
 
   // content::RenderProcessObserver implementation.
   bool OnControlMessageReceived(const IPC::Message& message) override;
-  void WebKitInitialized() override;
   void OnRenderProcessShutdown() override;
 
   bool IsWarpMode() const {

commit ad5e8bd98a3bd8591fd1a29bfe843ae2beac712d

M51: Fix the failed test case of testSetNetworkAvailableTest().
This issue was caused by
https://github.com/crosswalk-project/crosswalk/commit/2ad32ffd5384f0cc678d5bed06bb722f0d236ea8.
The variable webkit_initialized_ was not entirely removed.

BUG=XWALK-6998

runtime/renderer/android/xwalk_render_process_observer.cc
@@ -17,8 +17,7 @@
 
 namespace xwalk {
 
-XWalkRenderProcessObserver::XWalkRenderProcessObserver()
-  : webkit_initialized_(false) {
+XWalkRenderProcessObserver::XWalkRenderProcessObserver() {
 }
 
 XWalkRenderProcessObserver::~XWalkRenderProcessObserver() {
@@ -38,13 +37,11 @@ bool XWalkRenderProcessObserver::OnControlMessageReceived(
 }
 
 void XWalkRenderProcessObserver::OnSetJsOnlineProperty(bool network_up) {
-  if (webkit_initialized_)
-    blink::WebNetworkStateNotifier::setOnLine(network_up);
+  blink::WebNetworkStateNotifier::setOnLine(network_up);
 }
 
 void XWalkRenderProcessObserver::OnClearCache() {
-  if (webkit_initialized_)
-    blink::WebCache::clear();
+  blink::WebCache::clear();
 }
 
 void XWalkRenderProcessObserver::OnSetOriginAccessWhitelist(

runtime/renderer/android/xwalk_render_process_observer.h
@@ -28,8 +28,6 @@ class XWalkRenderProcessObserver : public content::RenderProcessObserver {
   void OnClearCache();
   void OnSetOriginAccessWhitelist(std::string base_url,
                                   std::string match_patterns);
-
-  bool webkit_initialized_;
 };
 
 }  // namespace xwalk