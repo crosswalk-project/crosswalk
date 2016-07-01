8adf93ba5ff6d51b60a8cb2e6519168eb5447fb0
Mrunal Kapade <mrunal.kapade@intel.com>
2016-07-01 15:09:24 -0700

    M52: Rename gfx::Display/Screen to display::Display/Screen
    adapt to,
    https://codereview.chromium.org/1924703002

--- runtime/browser/ui/native_app_window_views.cc
+++ runtime/browser/ui/native_app_window_views.cc
@@ -11,7 +11,7 @@
 #include "content/public/browser/web_contents.h"
 #include "grit/xwalk_resources.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/gfx/screen.h"
+#include "ui/display/screen.h"
 #include "ui/views/controls/webview/webview.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/constrained_window/constrained_window_views_client.h"
