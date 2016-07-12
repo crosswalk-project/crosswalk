cab93da8de51e5429d3b64a3f0e7d05a67fcd714
Alexis Menard <alexis.menard@intel.com>
2016-07-12 05:41:45 -0700

    [Windows][Linux] DevTools: Fix Maximum call stack size exceeded error while retrieving CPU profile.
    Do not pass object literals into dispatchMessage API. Always turn it into
    a string.
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=614860
    and
    https://chromium.googlesource.com/chromium/src.git/+/a2b2954002b9f46a902bb23ed6a11ba4c0604096%5E!/
    
    (cherry picked from commit 7096db3b85bf7012362bd32c1e3e766db97ec50a)

--- runtime/browser/devtools/xwalk_devtools_frontend.cc
+++ runtime/browser/devtools/xwalk_devtools_frontend.cc
@@ -8,6 +8,7 @@
 #include "base/command_line.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/json/string_escape.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -267,18 +268,21 @@ void XWalkDevToolsFrontend::DispatchProtocolMessage(
     content::DevToolsAgentHost* agent_host, const std::string& message) {
 
   if (message.length() < kMaxMessageChunkSize) {
-    base::string16 javascript = base::UTF8ToUTF16(
-        "DevToolsAPI.dispatchMessage(" + message + ");");
+    std::string param;
+    base::EscapeJSONString(message, true, &param);
+    std::string code = "DevToolsAPI.dispatchMessage(" + param + ");";
+    base::string16 javascript = base::UTF8ToUTF16(code);
     web_contents()->GetMainFrame()->ExecuteJavaScriptForTests(javascript);
     return;
   }
 
-  base::FundamentalValue total_size(static_cast<int>(message.length()));
+  size_t total_size = message.length();
   for (size_t pos = 0; pos < message.length(); pos += kMaxMessageChunkSize) {
     std::string param;
-    base::JSONWriter::Write(
-        base::StringValue(message.substr(pos, kMaxMessageChunkSize)), &param);
-    std::string code = "DevToolsAPI.dispatchMessageChunk(" + param + ");";
+    base::EscapeJSONString(message.substr(pos, kMaxMessageChunkSize), true,
+        &param);
+    std::string code = "DevToolsAPI.dispatchMessageChunk(" + param + "," +
+        std::to_string(pos ? 0 : total_size) + ");";
     base::string16 javascript = base::UTF8ToUTF16(code);
     web_contents()->GetMainFrame()->ExecuteJavaScriptForTests(javascript);
   }
