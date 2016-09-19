cadbd11405aaee6cfe2342fc2c936b8e5c6c0d18
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2016-09-19 21:32:48 +0200

    [Android] Rewrite generate_app_packaging_tool.py
    Also delegate most decisions about file paths to the build system, and
    only use the script to actually performs the copies and renamings
    necessary to get the template directory looking like we want it to.
    
    This simplifies the script and also paves the way for it to be used by
    GN, which has different paths for certain files consumed by the script.

--- build/android/generate_app_packaging_tool.py
+++ build/android/generate_app_packaging_tool.py
@@ -3,64 +3,66 @@
 # found in the LICENSE file.
 
 import argparse
-import errno
 import os
 import shutil
 import sys
 
+GYP_ANDROID_DIR = os.path.join(os.path.dirname(__file__),
+                               os.pardir, os.pardir, os.pardir,
+                               'build',
+                               'android',
+                               'gyp')
+sys.path.append(GYP_ANDROID_DIR)
 
-def GenerateAppTemplate(build_dir, build_mode, output_dir, source_dir):
-  """
-  Prepares xwalk_app_template/, which contains files used for packaging
-  Crosswalk apps. Its primary consumer is app-tools.
-  """
-  dirs = (
-    (os.path.join(source_dir, 'app/android/app_template'),
-     os.path.join(output_dir, 'template')),
-    (os.path.join(build_dir, 'xwalk_core_library'),
-     os.path.join(output_dir, 'xwalk_core_library')),
-    (os.path.join(build_dir, 'xwalk_shared_library'),
-     os.path.join(output_dir, 'xwalk_shared_library')),
-  )
-  files = (
-    (os.path.join(source_dir, 'API_VERSION'),
-     os.path.join(output_dir, 'API_VERSION')),
-    (os.path.join(source_dir, 'VERSION'),
-     os.path.join(output_dir, 'VERSION')),
-    (os.path.join(build_dir, 'lib.java', 'xwalk_app_runtime_java.jar'),
-     os.path.join(output_dir, 'template', 'libs',
-                  'xwalk_app_runtime_java.jar')),
-  )
-
-  for src, dest in dirs:
-    # Make sure app/android/app_template's BUILD.gn is not included.
-    shutil.copytree(src, dest, ignore=shutil.ignore_patterns('*.gn'))
-  for src, dest in files:
-    try:
-      os.makedirs(os.path.dirname(dest))
-    except OSError, e:
-      if e.errno == errno.EEXIST:
-        pass
-    shutil.copy2(src, dest)
+from util import build_utils
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--build-dir', required=True,
-                      help='Build directory location.')
-  parser.add_argument('--build-mode', required=True,
-                      help='Build mode (Release, Debug etc).')
+  parser.add_argument('--android-template', required=True,
+                      help='Path to the main template directory.')
+  parser.add_argument('--core-library-dir', required=True,
+                      help='Path to xwalk_core_library/.')
+  parser.add_argument('--extra-files',
+                      help='Extra files to add to the template.')
   parser.add_argument('--output-dir', required=True,
                       help='Directory where the app template files will be '
                       'stored.')
-  parser.add_argument('--source-dir', required=True,
-                      help='Top-level source directory location.')
+  parser.add_argument('--shared-library-dir', required=True,
+                      help='Path to xwalk_shared_library/.')
+  parser.add_argument('--stamp', required=True,
+                      help='Path to touch on success.')
+  parser.add_argument('--xwalk-runtime-jar', required=True,
+                      help='Path to the Crosswalk runtime JAR.')
+
   args = parser.parse_args()
 
-  shutil.rmtree(args.output_dir, ignore_errors=True)
-  os.makedirs(args.output_dir)
-  GenerateAppTemplate(args.build_dir, args.build_mode, args.output_dir,
-                      args.source_dir)
+  build_utils.DeleteDirectory(args.output_dir)
+  build_utils.MakeDirectory(args.output_dir)
+
+  shutil.copytree(args.android_template,
+                  os.path.join(args.output_dir, 'template'),
+                  # Make sure app/android/app_template's BUILD.gn is
+                  # not included.
+                  ignore=shutil.ignore_patterns('*.gn'))
+  shutil.copytree(args.core_library_dir,
+                  os.path.join(args.output_dir, 'xwalk_core_library'))
+  shutil.copytree(args.shared_library_dir,
+                  os.path.join(args.output_dir, 'xwalk_shared_library'))
+
+  # For now at least, we have to continue calling the runtime JAR
+  # xwalk_app_runtime_java.jar to avoid breaking app-tools.
+  template_libs_dir = os.path.join(args.output_dir, 'template', 'libs')
+  build_utils.MakeDirectory(template_libs_dir)
+  shutil.copy2(args.xwalk_runtime_jar,
+               os.path.join(template_libs_dir, 'xwalk_app_runtime_java.jar'))
+
+  for extra_file in build_utils.ParseGypList(args.extra_files):
+    # For now we just assume all files are supposed to go to the root of the
+    # output directory.
+    shutil.copy2(extra_file, args.output_dir)
+
+  build_utils.Touch(args.stamp)
 
 
 if __name__ == '__main__':
