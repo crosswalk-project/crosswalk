crosswalk-project/crosswalk

commit 8f3c7d3985a68030ae71debbabacc6152496b65c

[Android] Stop depending on xwalk_core_shell from xwalk_core_library
This makes `xwalk_core_library_android.gypi` stop depending on
`xwalk_android_tests.gypi`; the latter contains just test targets and
thus should not be required by anything not related to tests.

Instead, we now obtain the list of native libraries we want to copy from
the build system itself, which makes a lot more sense than having to
figure out what's in gyp/GN and what's hardcoded in an obscure Python
script.

`gdbserver` is also no longer copied (even in a debug build): the whole
xwalk_core_library thing targets a deprecated IDE (Eclipse) and I
honestly doubt people are debugging their debug Crosswalk builds with
the gdbserver binary from xwalk_core_library.

A new target, `xwalk_core_library_strip_native_libs', had to be added in
order to strip the native libraries. It is not needed by GN.

xwalk_core_library_android.gypi
@@ -294,11 +294,50 @@
       ],
     },
     {
+      'target_name': 'xwalk_core_library_strip_native_libs',
+      'type': 'none',
+      'dependencies': [
+        'libxwalkcore',
+        'libxwalkdummy',
+      ],
+      'variables': {
+        'intermediate_dir': '<(PRODUCT_DIR)/<(_target_name)',
+        'ordered_libraries_file': '<(intermediate_dir)/native_libraries.json',
+      },
+      'direct_dependent_settings': {
+        'variables': {
+          'stripped_native_libraries': [
+            '<(intermediate_dir)/libxwalkcore.so',
+            '<(intermediate_dir)/libxwalkdummy.so',
+          ],
+        },
+      },
+      'actions': [
+        {
+          'variables': {
+            'input_libraries': [
+              '<(SHARED_LIB_DIR)/libxwalkcore.so',
+              '<(SHARED_LIB_DIR)/libxwalkdummy.so',
+            ],
+          },
+          'includes': ['../build/android/write_ordered_libraries.gypi'],
+        },
+        {
+          'action_name': 'strip_native_libraries',
+          'variables': {
+            'stripped_libraries_dir': '<(intermediate_dir)',
+            'stamp': '<(intermediate_dir)/stamp',
+          },
+          'includes': ['../build/android/strip_native_libraries.gypi'],
+        },
+      ],
+    },
+    {
       'target_name': 'xwalk_core_library',
       'type': 'none',
       'dependencies': [
-        'xwalk_core_shell_apk',
         'xwalk_core_library_java',
+        'xwalk_core_library_strip_native_libs',
       ],
       'actions': [
         {
@@ -313,6 +352,8 @@
           ],
           'action': [
             'python', '<(DEPTH)/xwalk/build/android/generate_xwalk_core_library.py',
+            '--abi', '<(android_app_abi)',
+            '--native-libraries', '>(stripped_native_libraries)',
             '-s', '<(DEPTH)',
             '-t', '<(PRODUCT_DIR)'
           ],
@@ -350,6 +391,7 @@
       'type': 'none',
       'dependencies': [
         'xwalk_core_library',
+        'xwalk_core_library_strip_native_libs',
       ],
       'variables': {
          # TODO(wang16): This list is hard coded for now. It might be broken by rebase to
@@ -390,6 +432,8 @@
           ],
           'action': [
             'python', '<(DEPTH)/xwalk/build/android/generate_xwalk_core_library.py',
+            '--abi', '<(android_app_abi)',
+            '--native-libraries', '>(stripped_native_libraries)',
             '-s',  '<(DEPTH)',
             '-t', '<(PRODUCT_DIR)',
             '--src-package',

build/android/generate_xwalk_core_library.py
@@ -11,10 +11,17 @@ import os
 import shutil
 import sys
 import zipfile
-from common_function import RemoveUnusedFilesInReleaseMode
 from xml.dom.minidom import Document
 
-XWALK_CORE_SHELL_APK = 'xwalk_core_shell_apk'
+GYP_ANDROID_DIR = os.path.join(os.path.dirname(__file__),
+                               os.pardir, os.pardir, os.pardir,
+                               'build',
+                               'android',
+                               'gyp')
+sys.path.append(GYP_ANDROID_DIR)
+
+from util import build_utils
+
 
 def AddGeneratorOptions(option_parser):
   option_parser.add_option('-s', dest='source',
@@ -23,6 +30,10 @@ def AddGeneratorOptions(option_parser):
   option_parser.add_option('-t', dest='target',
                            help='Product out target directory.',
                            type='string')
+  option_parser.add_option('--abi',
+                           help='Android ABI being used in the build.')
+  option_parser.add_option('--native-libraries',
+                           help='List of libraries to copy to libs/<abi>.')
   option_parser.add_option('--shared', action='store_true',
                            default=False,
                            help='Generate shared library', )
@@ -147,9 +158,12 @@ def CopyBinaries(out_dir, out_project_dir, src_package, shared):
   pak_list_xml.writexml(pak_list_file, newl='\n', encoding='utf-8')
   pak_list_file.close()
 
-  # Copy native libraries.
-  source_dir = os.path.join(out_dir, XWALK_CORE_SHELL_APK, 'libs')
-  distutils.dir_util.copy_tree(source_dir, libs_dir)
+
+def CopyNativeLibraries(out_project_dir, abi_name, native_libraries):
+  destination_path = os.path.join(out_project_dir, 'libs', abi_name)
+  build_utils.MakeDirectory(destination_path)
+  for native_lib in native_libraries:
+    shutil.copy2(native_lib, destination_path)
 
 
 def CopyDirAndPrefixDuplicates(input_dir, output_dir, prefix, blacklist=None):
@@ -313,12 +327,14 @@ def main(argv):
   # Copy binaries and resuorces.
   CopyResources(options.source, out_dir, out_project_dir, options.shared)
   CopyBinaries(out_dir, out_project_dir, options.src_package, options.shared)
+
+  if options.native_libraries:
+    CopyNativeLibraries(out_project_dir, options.abi,
+                        build_utils.ParseGypList(options.native_libraries))
+
   # Copy JS API binding files.
   CopyJSBindingFiles(options.source, out_project_dir)
-  # Remove unused files.
-  mode = os.path.basename(os.path.normpath(out_dir))
-  RemoveUnusedFilesInReleaseMode(mode,
-      os.path.join(out_project_dir, 'libs'))
+
   # Create empty src directory
   src_dir = os.path.join(out_project_dir, 'src')
   if not os.path.isdir(src_dir):

commit 2b7bb9ea61b097e737dd9193c7ea9f9fb9d708a1

[Android] Directly depend on the .so's we want to strip
This fixes a regression introduced in commit 8f3c7d3 ("[Android] Stop
depending on xwalk_core_shell from xwalk_core_library"), where
`libxwalkdummy.so` and `libxwalkcore.so` were not being stripped and
added to `xwalk_core_library/` (which is then used for the AAR and
`xwalk_app_template`) in incremental builds, leading to outdated,
existing versions being used instead.

The bug was caused by us not explicitly depending on both unstripped
shared libraries in the action that includes
`strip_native_libraries.gypi`: we were previously only depending on
`native_libraries.json` being updated whenever those shared libraries
changed, but `write_ordered_libraries.py` only updates the JSON file if
its contents are actually different (if we started depending on other
shared libraries, for example).

BUG=XWALK-7335

xwalk_core_library_android.gypi
@@ -228,6 +228,10 @@
           'variables': {
             'stripped_libraries_dir': '<(intermediate_dir)',
             'stamp': '<(intermediate_dir)/stamp',
+            'input_paths': [
+              '<(SHARED_LIB_DIR)/libxwalkcore.so',
+              '<(SHARED_LIB_DIR)/libxwalkdummy.so',
+            ],
           },
           'includes': ['../build/android/strip_native_libraries.gypi'],
         },