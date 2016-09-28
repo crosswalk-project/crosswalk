0c04eb396139b442e6f3e991c7ce73071cd04f4f
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2016-09-28 10:13:41 +0200

    [Windows] Add a msbuild.gypi and clean up the MSBuild Python wrapper
    Instead of manually calling `msbuild_dotnet.py` and then renaming
    `xwalk_dotnet_bridge.dll` every time one wants to build a .NET
    extension, do that only once in `msbuild.gypi`: the other targets just
    need to define a few variables and include the new gypi file, which then
    takes care of doing all the rest.
    
    This considerably reduces the size of the other gyp files and avoids a
    lot of duplication.
    
    Clean up `msbuild_dotnet.py` itself a little:
    * Stop invoking `MSBuild` from `//xwalk/extensions/test/win`, which
      creates some needless complications in the WiFi Direct gyp code.
      Instead, just invoke it from the root of the build directory (the
      default).
    * Use `argparse` instead of `optparse` to avoid having to manually
      validate required arguments.
    * Pass `/nologo` and `/verbosity:quiet` to `MSBuild` to make it quieter.
    * Split MSBuild's parameters into a separate variable to make the code
      easier to understand.
    
    BUG=XWALK-7336

--- experimental/wifidirect/wifidirect_extension.gyp
+++ experimental/wifidirect/wifidirect_extension.gyp
@@ -2,49 +2,17 @@
   'targets': [
     {
       'target_name': 'wifidirect_extension',
-      'type': 'none',
-      'actions': [
-        {
-          'action_name': 'build_wifidirect_extension',
-          'inputs': [
-            'XWalkExtension.cs',
-            'XWalkExtensionInstance.cs',
-          ],
-          'outputs': [
-            'wifidirect_extension.dll',
-          ],
-          'action': ['python',
-                     '../../tools/msbuild_dotnet.py',
-                     '--output-dir', '<(PRODUCT_DIR)',
-                     '--project', '../../../experimental/wifidirect/wifidirect_extension.csproj',
-                     '--build-type', 'Debug',
-          ],
-        },
-      ],
-    },
-    {
-      'target_name': 'copy_wifidirect_extension_bridge',
-      'type': 'none',
-      'dependencies': [
-        'wifidirect_extension',
-      ],
-      'actions': [
-        {
-          'action_name': 'copy_and_rename_bridge_wifidirect_extension',
-          'inputs': [
-            '<(PRODUCT_DIR)/xwalk_dotnet_bridge.dll',
-          ],
-          'outputs': [
-            'wifidirect_extension_bridge.dll',
-          ],
-          'action': ['python',
-                     '../../tools/copy_rename.py',
-                     '--source-dir', '<(PRODUCT_DIR)',
-                     '--input-file', 'xwalk_dotnet_bridge.dll',
-                     '--output-file', 'wifidirect_extension_bridge.dll',
-                     '--destination-dir', '<(PRODUCT_DIR)/',
-          ],
-        },
+      'variables': {
+        'output_dir': '<(PRODUCT_DIR)',
+        'output_name': 'wifidirect_extension',
+        'project_path': '<(DEPTH)/xwalk/experimental/wifidirect/wifidirect_extension.csproj',
+        'sources': [
+          'XWalkExtension.cs',
+          'XWalkExtensionInstance.cs',
+        ],
+      },
+      'includes': [
+        '../../build/msbuild.gypi',
       ],
     },
   ],
--- tools/msbuild_dotnet.py
+++ tools/msbuild_dotnet.py
@@ -6,49 +6,42 @@
 msbuild_dotnet.py -- Invoke MSBuild for .NET project.
 """
 
-import logging
-import optparse
+import argparse
 import os
 import subprocess
 import sys
 
 
 def LaunchMSBuild(options):
-  if not options.output_dir:
-    print('You must specify the output-dir')
-    return 1
-  if not options.project:
-    print('You have not specified the project you want to build')
-    return 1
-  if not options.build_type:
-    print('You have not specified the build-type')
-    return 1
+  params = [
+    'Platform=AnyCPU',
+    'Configuration=%s' % options.build_type,
+    'OutDir=%s' % options.output_dir,
+    'BaseIntermediateOutputPath=%s\\' % options.output_dir,
+  ]
+  cmd = [
+    'msbuild',
+    '/nologo',
+    '/p:%s' % ','.join(params),
+    '/verbosity:quiet',
+    options.project,
+  ]
+  subprocess.check_call(cmd)
 
-  output_dir = os.path.abspath(options.output_dir)
-  tools_dir = os.path.dirname(os.path.abspath(__file__))
-  xwalk_dir = os.path.dirname(tools_dir)
-  extensions_dir = os.path.join(xwalk_dir, 'extensions')
-  extensions_test_dir = os.path.join(extensions_dir, 'test')
-  extensions_win_test_dir = os.path.join(extensions_test_dir,
-                                         'win' + os.path.sep)
-
-  output = subprocess.call(['msbuild', extensions_win_test_dir
-      + options.project, '/p:Platform=AnyCPU,Configuration='
-      + options.build_type + ',OutDir=' + output_dir
-      + ',BaseIntermediateOutputPath=' + output_dir + os.path.sep],
-      cwd=extensions_win_test_dir)
-  if output != 0:
-    return output
 
 def main():
-  option_parser = optparse.OptionParser()
-  option_parser.add_option('--output-dir',
-                           help='Set the output dir for MSBuild.')
-  option_parser.add_option('--project',
-                           help='The project to build with MSBuild.')
-  option_parser.add_option('--build-type',
-                           help='The type of build : Release/Debug.')
-  options, _ = option_parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--output-dir', required=True,
+                      help='Set the output dir for MSBuild.')
+  parser.add_argument('--project', required=True,
+                      help='The project to build with MSBuild.')
+  parser.add_argument('--build-type', required=True,
+                      choices=('Debug', 'Release'),
+                      help='The type of build : Release/Debug.')
+  options = parser.parse_args()
+  # We need to call os.path.abspath() because MSBuild seems consider the
+  # project directory the root of a relative path, not the build directory.
+  options.output_dir = os.path.abspath(options.output_dir)
   LaunchMSBuild(options)
 
 
