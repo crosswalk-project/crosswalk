a8b000086a138e4d035ed6b638a90bde52cbc857
Maksim Sisov <maksim.sisov@intel.com>
2016-10-21 13:41:38 +0300

    M54 Rebasing: Fix DisplayPersistentNotification arguments
    adapt to
    https://codereview.chromium.org/2151993002

--- runtime/browser/xwalk_platform_notification_service.h
+++ runtime/browser/xwalk_platform_notification_service.h
@@ -48,6 +48,7 @@ class XWalkPlatformNotificationService
   void DisplayPersistentNotification(
       content::BrowserContext* browser_context,
       int64_t service_worker_registration_id,
+      const GURL& service_worker_origin,
       const GURL& origin,
       const content::PlatformNotificationData& notification_data,
       const content::NotificationResources& notification_resources) override {}
