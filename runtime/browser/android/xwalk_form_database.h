2abd648ef7abf88bcfa9ecf62f2e79e7891c9812
xzhan96 <xiaofeng.zhang@intel.com>
2016-11-10 15:53:05 +0800

    Implement the XWalkViewDatabase
    This class is the equivalent of Android WebViewDatabase, but it only has
    two APIs, hasFormData and clearFormData, they allow developers to determine
    whether there is any saved data for web forms and to clear such data.
    
    BUG=XWALK-7057

---
+++ runtime/browser/android/xwalk_form_database.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2016 The Intel Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef XWALK_RUNTIME_BROWSER_ANDROID_XWALK_FORM_DATABASE_H_
+#define XWALK_RUNTIME_BROWSER_ANDROID_XWALK_FORM_DATABASE_H_
+
+#include <jni.h>
+
+namespace xwalk {
+
+bool RegisterXWalkFormDatabase(JNIEnv* env);
+
+}  // namespace xwalk
+
+#endif  // XWALK_RUNTIME_BROWSER_ANDROID_XWALK_FORM_DATABASE_H_
